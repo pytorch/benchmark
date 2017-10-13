7c405664be5c81686ff22b36ade0df195d81c430
Edward Yang <ezyang@fb.com>
2017-10-13 07:49:14 -0700

    Use CUDA event API
    Signed-off-by: Edward Yang <ezyang@fb.com>

--- baselines/mlstm.cpp
+++ baselines/mlstm.cpp
@@ -1,5 +1,28 @@
 #include <ATen/ATen.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
 #include <iostream>
+#include <chrono>
+
+#include "stdio.h"
+
+#define CUDA_CHECK(result) cudaCheck(result, __FILE__, __LINE__);
+
+static void cudaCheck(cudaError_t result, const char * file, int line) {
+  if(result != cudaSuccess) {
+    std::stringstream ss;
+    ss << file << ":" << line << ": " << cudaGetErrorString(result);
+    throw std::runtime_error(ss.str());
+  }
+}
+
+inline uint64_t getTime() {
+  using namespace std::chrono;
+  using clock = std::conditional<high_resolution_clock::is_steady, high_resolution_clock, steady_clock>::type;
+  return duration_cast<nanoseconds>(clock::now().time_since_epoch()).count();
+}
 
 std::pair<at::Tensor, at::Tensor>
 mlstm(at::Tensor input,
@@ -28,12 +51,17 @@ mlstm(at::Tensor input,
   return {hy, cy};
 }
 
+// Modeled off of Soumith's benchmark at
+// https://github.com/soumith/convnet-benchmarks/blob/d6177f97e61da0d98a528f355086eb2fc05fe7b8/nervana/convnet-benchmarks.py
 int main() {
   constexpr int batch_size = 3;
   constexpr int input_size = 100;
   constexpr int hidden_size = 400;
   constexpr int embed_size = 400;
-  constexpr int n_iters = 700;
+  constexpr int layers = 700;
+  constexpr int loops = 10;
+  constexpr int warmup = 50;
+
   auto input = at::CUDA(at::kFloat).randn({batch_size, input_size});
   auto hx    = at::CUDA(at::kFloat).randn({batch_size, hidden_size});
   auto cx    = at::CUDA(at::kFloat).randn({batch_size, hidden_size});
@@ -41,11 +69,25 @@ int main() {
   auto w_hm  = at::CUDA(at::kFloat).randn({embed_size, hidden_size});
   auto w_ih  = at::CUDA(at::kFloat).randn({4 * hidden_size, input_size});
   auto w_mh  = at::CUDA(at::kFloat).randn({4 * hidden_size, embed_size});
-  // TODO: timing
-  // TODO: warmup
-  for (int i = 0; i < n_iters; i++) {
-    std::tie(hx, cx) = mlstm(input, hx, cx, w_xm, w_hm, w_ih, w_mh);
+
+  cudaEvent_t start, end;
+  CUDA_CHECK(cudaEventCreate(&start));
+  CUDA_CHECK(cudaEventCreate(&end));
+
+  for (int i = 0; i < warmup + loops; i++) {
+    CUDA_CHECK(cudaEventRecord(start, 0));
+    for (int j = 0; j < layers; j++) {
+      std::tie(hx, cx) = mlstm(input, hx, cx, w_xm, w_hm, w_ih, w_mh);
+    }
+    CUDA_CHECK(cudaEventRecord(end, 0));
+    CUDA_CHECK(cudaDeviceSynchronize());
+    float msecs;
+    cudaEventElapsedTime(&msecs, start, end);
+    printf("mlstm(%2d): %8.3f msecs\n", i, msecs);
   }
-  std::cerr << hx << "\n" << cx << "\n";
+
+  CUDA_CHECK(cudaEventDestroy(start));
+  CUDA_CHECK(cudaEventDestroy(end));
+
   return 0;
 }
