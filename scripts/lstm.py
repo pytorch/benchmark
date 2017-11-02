441e45e1bb9b66c7aab1657acdccfb232d661965
Adam Paszke <adam.paszke@gmail.com>
2017-11-02 13:58:30 -0400

    Add an option to profile LSTM script

--- scripts/lstm.py
+++ scripts/lstm.py
@@ -4,6 +4,8 @@ import torch
 from torch.autograd import Variable
 import torch.jit
 from torch._thnn import type2backend
+from torch.nn._functions.rnn import LSTMCell
+from torch.autograd.profiler import profile
 
 import argparse
 import pprint
@@ -16,9 +18,9 @@ import sys
 # If you swap the transpose here, you can test the effect of pre-transposing.
 # In my experiments it didn't account for much
 def t_use(x):
-    return x
-def t_def(x):
     return x.t()
+def t_def(x):
+    return x
 
 
 def fused_lstm(input, hidden, w_ih, w_hh):
@@ -66,9 +68,10 @@ def main():
     parser.add_argument('--batch-size',   type=int, default=1,    help="Batch size")
     parser.add_argument('--input-size',   type=int, default=256,  help="Input size")
     parser.add_argument('--hidden-size',  type=int, default=512,  help="Hidden size")
-    parser.add_argument('--seq-len',      type=int, default=None,  help="Sequence length")
+    parser.add_argument('--seq-len',      type=int, default=None, help="Sequence length")
     parser.add_argument('--warmup',       type=int, default=10,   help="Warmup iterations")
     parser.add_argument('--benchmark',    type=int, default=20,   help="Benchmark iterations")
+    parser.add_argument('--profile',      type=str, default='',   help="Generate chrome trace")
     parser.add_argument('--autograd',     action='store_true',    help="Use autograd")
     parser.add_argument('--variable',     action='store_true',    help="Use Variable, but not autograd (measure baseline overhead)")
     parser.add_argument('--fused',        action='store_true',    help="Use fused cell")
@@ -91,7 +94,6 @@ def main():
         else:
             args.seq_len = 512
 
-    assert not (args.jit and args.fused)
     assert not (args.variable and args.autograd)
 
     pprint.pprint(vars(args))
@@ -105,10 +107,10 @@ def main():
     else:
         V = lambda x, requires_grad=False: x
 
-    if args.fused:
-        lstm = fused_lstm
-    elif args.jit:
-        lstm = torch.jit.compile(nderivs=0)(unfused_lstm)
+    if args.jit:
+        lstm = torch.jit.compile(nderivs=0, optimize=args.fused)(unfused_lstm)
+    elif args.fused:
+        lstm = fused_lstm if not args.autograd else LSTMCell
     else:
         lstm = unfused_lstm
 
@@ -121,22 +123,32 @@ def main():
     start = torch.cuda.Event(enable_timing=True)
     end = torch.cuda.Event(enable_timing=True)
 
-    for i in range(args.warmup + args.benchmark):
+    def run():
         gc.collect()
+        gc.disable()
         start.record()
-        times = []
         start_cpu_secs = time.time()  # high precision only for Linux
         hx, cx = hx0, cx0
         for j in range(args.seq_len):
-            times.append(time.time())
             hx, cx = lstm(input, (hx, cx), w_ih, w_hh)
         if args.backward:
             hx.sum().backward()
         end_cpu_secs = time.time()
         end.record()
         torch.cuda.synchronize()
-        gpu_msecs = start.elapsed_time(end)
-        benchmark_common.print_results_usecs("lstm", i, gpu_msecs*1000, (end_cpu_secs - start_cpu_secs)*1000000, args.seq_len)
+        gpu_usecs = start.elapsed_time(end) * 1000
+        cpu_usecs = (end_cpu_secs - start_cpu_secs) * 1000000
+        gc.enable()
+        return cpu_usecs, gpu_usecs
+
+    for i in range(args.warmup):
+        run()
+    with profile(enabled=bool(args.profile)) as prof:
+        for i in range(args.benchmark):
+            cpu_usecs, gpu_usecs = run()
+            benchmark_common.print_results_usecs("lstm", i, gpu_usecs, cpu_usecs, args.seq_len)
+    if args.profile:
+        prof.export_chrome_trace(args.profile)
 
 if __name__ == "__main__":
     main()
