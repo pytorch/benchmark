af300889b91c005173ab56df5f7877b45149a70b
Christian Puhrsch <cpuhrsch@fb.com>
2018-06-08 23:18:27 -0600

    Checkout PRs for pytorch install

--- timing/python/install_pytorch.sh
+++ timing/python/install_pytorch.sh
@@ -13,8 +13,9 @@ export PATH="$HOME/miniconda3/bin:$PATH"
 
 git clone --quiet https://github.com/pytorch/pytorch
 pushd pytorch
+git fetch --quiet --tags https://github.com/pytorch/pytorch.git +refs/heads/*:refs/remotes/origin/* +refs/pull/*:refs/remotes/origin/pr/* --depth=50
 git checkout "$1"
-git submodule update --init
+git submodule --quiet update --init
 python setup.py install
 popd
 
