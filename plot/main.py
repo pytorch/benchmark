1071b2951f5ed8685706b64283bfde55e4f00297
Adam Paszke <adam.paszke@gmail.com>
2018-10-03 08:46:55 -0700

    Rerun benchmarks multiple times for confidence intervals

--- plot/main.py
+++ plot/main.py
@@ -2,6 +2,7 @@ import os
 import csv
 import time
 import json
+import math
 import subprocess
 from datetime import datetime
 from io import StringIO
@@ -11,7 +12,8 @@ REPO_URL = 'https://github.com/pytorch/pytorch'
 REPO_DIR = 'repo'
 OUTPUT_PATH = 'results.json'
 HERE = os.path.dirname(os.path.abspath(__file__))
-MAX_BENCHES = 30
+MAX_BENCHES = 160
+BENCH_TIMES = 4
 BENCH_EVERY = 10 # th commit
 
 run = partial(subprocess.check_call, cwd=REPO_DIR)
@@ -58,8 +60,8 @@ def build(commit_hash):
            '-v', os.path.join(HERE, '..') + ':/mnt/localdrive',
            '--name', cname,
            '-t', 'pytorch_bench',
-           '/bin/bash', '/mnt/localdrive/timing/python/install_pytorch.sh', commit_hash])
-    run(['docker', 'commit', cname, cname])
+           '/bin/bash', '/mnt/localdrive/timing/python/install_pytorch.sh', commit_hash], **silent)
+    run(['docker', 'commit', cname, cname], **silent)
     end = time.time()
     diff = int(end - start)
     print('    (Build took {} min {} s)'.format(diff // 60, diff % 60))
@@ -67,8 +69,8 @@ def build(commit_hash):
 
 def cleanup(commit_hash):
     # TODO: DON'T REMOVE ALL CONTAINERS!
-    run('docker ps -a -q | xargs docker rm', shell=True)
-    run(['docker', 'image', 'rm', container_name(commit_hash)])
+    run('docker ps -a -q | xargs docker rm', shell=True, **silent)
+    run(['docker', 'image', 'rm', container_name(commit_hash)], **silent)
 
 
 def run_benchmark(commit_hash, args, **kwargs):
@@ -147,6 +149,25 @@ BENCHMARKS = [
     dict(args=['python', '-m', 'rnns.fastrnns.bench', '--print-json']),
 ]
 
+# List[Dict[Dict[Int]]] -> Dict[Dict[List[Int]]]
+def transpose_results(results):
+    def get_keys(result):
+        return sorted([(outer, inner) for outer in result for inner in result[outer]])
+    keys = get_keys(results[0])
+    assert all(get_keys(result) == keys for result in results)
+    any_result = results[0]
+    return {outer: {inner: [result[outer][inner] for result in results] for inner in any_result[outer]} for outer in any_result}
+
+
+def result_stats(result):
+    def mean(l):
+        return sum(l) / len(l)
+    def std(l):
+        m = mean(l)
+        return math.sqrt(sum([(v - m) ** 2 for v in l]))
+    return {outer: {inner: (mean(innerv), std(innerv)) for inner, innerv in outerv.items()} for outer, outerv in result.items()}
+
+
 if __name__ == '__main__':
     fetch_repo()
     new_commits = get_history()
@@ -158,18 +179,29 @@ if __name__ == '__main__':
         for i, commit in enumerate(to_bench):
             if 'times' in commit:
                 continue
+
             try:
                 print('> Building {} ({}/{})...'.format(commit['hash'], i + 1, len(to_bench)))
                 build(commit['hash'])
-                times = {}
-                for args in BENCHMARKS:
-                    output = run_benchmark(commit['hash'], **args)
-                    merge_into(times, json.loads(output))
-                commit['times'] = times
-                cleanup(commit['hash'])
+            except Exception as e:
+                print('Build has failed!')
+                continue
+
+            try:
+                def bench():
+                    times = {}
+                    for args in BENCHMARKS:
+                        output = run_benchmark(commit['hash'], **args)
+                        merge_into(times, json.loads(output))
+                    return times
+
+                bench_result = transpose_results([bench() for _ in range(BENCH_TIMES)])
+                commit['times'] = result_stats(bench_result)
             except Exception as e:
                 print('Interrupted by an exception!')
                 print(e)
+            finally:
+                cleanup(commit['hash'])
     except KeyboardInterrupt:
         print('Received an interrupt. Saving partial results...')
 
