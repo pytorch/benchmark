4bbc2faee46ffff7e8e57212a687981ce23cfa40
Will Constable <whc@fb.com>
2020-08-31 20:26:45 -0700

    Add top level hub benchmark files and CI scripts
    - remove submodule update as no submodules used
    - remove torchhub job
    - update paths in scripts

---
+++ scripts/install_basics.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Install basics
+sudo apt-get install vim
+
+# Install miniconda
+CONDA=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+filename=$(basename "$CONDA")
+wget "$CONDA"
+chmod +x "$filename"
+./"$filename" -b -u
+
+# Force to use python3.7
+. ~/miniconda3/etc/profile.d/conda.sh
+conda activate base
+conda install -y python=3.7
+
+
---
+++ scripts/run_bench_and_upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+. ~/miniconda3/etc/profile.d/conda.sh
+conda activate base
+
+BENCHMARK_DATA=".data"
+mkdir -p ${BENCHMARK_DATA}
+pytest test_bench.py --setup-show --benchmark-sort=Name --benchmark-json=${BENCHMARK_DATA}/hub.json
+
+# Token is only present for certain jobs, only upload if present
+if [ -z "$SCRIBE_GRAPHQL_ACCESS_TOKEN" ]
+then
+    echo "Skipping benchmark upload, token is missing."
+else
+    python scripts/upload_scribe.py --pytest_bench_json ${BENCHMARK_DATA}/hub.json
+fi
