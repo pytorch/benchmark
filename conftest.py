60fec86bc1eed9efbcd80d07793a8cf6ff56ce42
wconstab <whc@fb.com>
2020-09-15 16:26:45 -0700

    Move torch version check to test json and update repo fields (#97)
    - change incorrect/confusing 'pytorch_commit_*' vars to git_* vars
          since the vars are used in repos such as benchmark
        - record torch pkg versions explicitly regardless of repo used
          for benchmark runs

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import torch
 
@@ -21,3 +22,20 @@ def set_fuser(fuser):
 
 def pytest_configure(config):
     set_fuser(config.getoption("fuser"))
+
+def pytest_benchmark_update_machine_info(config, machine_info):
+    machine_info['pytorch_version'] = torch.__version__
+    try:
+        import torchtext
+        machine_info['torchtext_version'] = torchtext.__version__
+    except ImportError:
+        machine_info['torchtext_version'] = '*not-installed*'
+
+    try:
+        import torchvision
+        machine_info['torchvision_version'] = torchvision.__version__
+    except ImportError:
+        machine_info['torchvision_version'] = '*not-installed*'
+
+    machine_info['circle_build_num'] = os.environ.get("CIRCLE_BUILD_NUM")
+    machine_info['circle_project_name'] = os.environ.get("CIRCLE_PROJECT_REPONAME")
