e60668f326d5f18998042fde18badfba1ddf0188
Will Constable <whc@fb.com>
2020-09-16 08:05:22 -0700

    Add logic to push benchmark json to github to archive
    Store data in either 'master' or 'pull_request' subdir of 'project/repo'
    top level dir.  Maintain 'latest' symlink for master data only.
    
    Currently, master sytest mlink is updated on every run, since PR jobs
    are expected to gate regressions.  However, we could change this and
    follow a different protocol for when, and how often, to move the
    pointer.

--- scripts/run_bench_and_upload.sh
+++ scripts/run_bench_and_upload.sh
@@ -3,6 +3,13 @@ set -e
 . ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
 
+# set credentials for git https pushing
+cat > ~/.netrc <<DONE
+machine github.com
+login pytorchbot
+password ${GITHUB_PYTORCHBOT_TOKEN}
+DONE
+
 if [ "$CIRCLE_BRANCH" = "master" ]
 then
     PYTEST_FILTER=""
@@ -10,14 +17,44 @@ else
     PYTEST_FILTER="(not cyclegan) and (not (stargan and train and cpu))"
 fi
 
-BENCHMARK_DATA=".data"
+BENCHMARK_DATA="`pwd`/.data"
 mkdir -p ${BENCHMARK_DATA}
-pytest test_bench.py --setup-show --benchmark-sort=Name --benchmark-json=${BENCHMARK_DATA}/hub.json -k "$PYTEST_FILTER"
+BENCHMARK_FILENAME=${CIRCLE_SHA1}.json
+BENCHMARK_ABS_FILENAME=${BENCHMARK_DATA}/${BENCHMARK_FILENAME}
+pytest test_bench.py --setup-show --benchmark-sort=Name --benchmark-json=${BENCHMARK_ABS_FILENAME} -k "$PYTEST_FILTER"
+
 
 # Token is only present for certain jobs, only upload if present
 if [ -z "$SCRIBE_GRAPHQL_ACCESS_TOKEN" ]
 then
     echo "Skipping benchmark upload, token is missing."
 else
-    python scripts/upload_scribe.py --pytest_bench_json ${BENCHMARK_DATA}/hub.json
+    python scripts/upload_scribe.py --pytest_bench_json ${BENCHMARK_ABS_FILENAME}
 fi
+
+
+git clone https://github.com/wconstab/pytorch_benchmark_data
+pushd pytorch_benchmark_data
+git config --global user.name "pytorchbot"
+git config --global --unset url.ssh://git@github.com.insteadof || true
+
+# Store the files with respect to the repo that their hash refers to
+# separate master and pull_requests just for conveneince in the UI
+if [ "$CIRCLE_BRANCH" = "master" ]
+then
+    SUBDIR="${CIRCLE_PROJECT_REPONAME}/master"
+else
+    SUBDIR="${CIRCLE_PROJECT_REPONAME}/pull_requests"
+fi
+mkdir -p $SUBDIR/
+cp ${BENCHMARK_ABS_FILENAME} $SUBDIR/
+git add ${SUBDIR}/${BENCHMARK_FILENAME}
+if [ "$CIRCLE_BRANCH" = "master" ]
+then
+    ln -s -f ${SUBDIR}/${BENCHMARK_FILENAME} ${SUBDIR}/latest
+    echo ${BENCHMARK_FILENAME} >> ${SUBDIR}/history
+    git add ${SUBDIR}/latest ${SUBDIR}/history
+fi
+git commit -m"Add ${BENCHMARK_FILENAME} - to reproduce results, see pytorch and machine versions in .json"
+git push origin master
+popd
