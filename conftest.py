0e5cad00326d197d5897a0681146997bbd6ac9e5
Will Constable <willconstable@gmail.com>
2020-09-24 00:37:12 -0400

    WIP

--- conftest.py
+++ conftest.py
@@ -4,8 +4,9 @@ import torch
 
 def pytest_addoption(parser):
     parser.addoption("--fuser", help="fuser to use for benchmarks")
+    parser.addoption("--bailout_depth", type=int, help="setting for te fuser")
 
-def set_fuser(fuser):
+def set_fuser(fuser, bailout_depth=20):
     if fuser == "old":
         torch._C._jit_set_profiling_executor(False)
         torch._C._jit_set_profiling_mode(False)
@@ -14,14 +15,15 @@ def set_fuser(fuser):
     elif fuser == "te":
         torch._C._jit_set_profiling_executor(True)
         torch._C._jit_set_profiling_mode(True)
-        torch._C._jit_set_bailout_depth(20)
+        torch._C._jit_set_bailout_depth(bailout_depth)
+        print(f"using bailout_depth {bailout_depth}")
         torch._C._jit_set_num_profiled_runs(2)
         torch._C._jit_override_can_fuse_on_cpu(False)
         torch._C._jit_override_can_fuse_on_gpu(True)
         torch._C._jit_set_texpr_fuser_enabled(True)
 
 def pytest_configure(config):
-    set_fuser(config.getoption("fuser"))
+    set_fuser(config.getoption("fuser"), config.getoption("bailout_depth"))
 
 def pytest_benchmark_update_machine_info(config, machine_info):
     machine_info['pytorch_version'] = torch.__version__
