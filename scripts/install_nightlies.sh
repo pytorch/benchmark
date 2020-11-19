68bb39aee6c290c3e8870f6cff981802c4500c74
wconstab <whc@fb.com>
2020-11-19 09:38:25 -0800

    Update install_nightlies.sh

--- scripts/install_nightlies.sh
+++ scripts/install_nightlies.sh
@@ -3,4 +3,14 @@ set -e
 
 . ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
-conda install -y pytorch torchtext torchvision -c pytorch-nightly
+conda install -y pytorch torchvision -c pytorch-nightly
+
+# separating to debug issue where when installing all 3 this error printed
+# 
+# UnsatisfiableError: The following specifications were found
+# to be incompatible with the existing python installation in your environment:
+#
+# Specifications:
+#
+#   - torchtext -> python[version='>=2.7,<2.8.0a0|>=3.5,<3.6.0a0']
+conda install -y torchtext -c pytorch-nightly
