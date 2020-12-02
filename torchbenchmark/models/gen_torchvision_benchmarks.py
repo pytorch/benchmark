7a94ad890008798b49c9fbdd7e01f4ff7d5b1c32
Chillee <horacehe2007@yahoo.com>
2020-12-01 21:46:46 -0500

    actually fixed the issue this time, by removing inceptoin

--- torchbenchmark/models/gen_torchvision_benchmarks.py
+++ torchbenchmark/models/gen_torchvision_benchmarks.py
@@ -1,14 +1,12 @@
 import os
 from pathlib import Path
 
-class_models = ['alexnet', 'vgg16', 'resnet18', 'resnet50', 'squeezenet1_1', 'densenet121', 'inception_v3', 'mobilenet_v2', 'shufflenet_v2_x1_0', 'resnext50_32x4d', 'mnasnet1_0']
+class_models = ['alexnet', 'vgg16', 'resnet18', 'resnet50', 'squeezenet1_1', 'densenet121', 'mobilenet_v2', 'shufflenet_v2_x1_0', 'resnext50_32x4d', 'mnasnet1_0']
 for class_model in class_models:
     folder = Path(class_model)
     if not os.path.isdir(folder):
         os.makedirs(folder)
     input_shape = (32, 3, 224, 224)
-    if class_model == 'inception_v3': # Exception among imagenet models
-        input_shape = (32, 3, 299, 299)
     example_inputs = f'(torch.randn({input_shape}),)'
     eval_inputs = 'example_inputs[0][0].unsqueeze(0)'
     init_program = f"""
