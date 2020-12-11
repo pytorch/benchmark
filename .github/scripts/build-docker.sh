2bf26fc0a2caefd4a67f9ca109a4dd8cc8258915
xz <xzhao9@fb.com>
2020-12-11 13:44:16 -0500

    Improve the docker build process. (#171)

--- .github/scripts/build-docker.sh
+++ .github/scripts/build-docker.sh
@@ -2,8 +2,12 @@
 
 set -euo pipefail
 
+TODAY=$(date +"%Y%m%d")
+IMAGE_NAME=torchbench/pytorch
+IMAGE_TAG=${TODAY}_gh${GITHUB_RUN_ID}
 PYTORCH_GITGHUB="https://github.com/pytorch/pytorch.git"
 PYTORCH_SRC=${HOME}/pytorch
+DATA_DIR=${HOME}/benchmark-results/gh${GITHUB_RUN_ID}
 
 # Checkout pytorch code
 if [ -d $PYTORCH_SRC ]
@@ -23,5 +27,14 @@ pushd $PYTORCH_SRC
 docker system prune -f
 make -f docker.Makefile PYTHON_VERSION=3.7 \
      CUDA_VERSION=10.2 CUDNN_VERSION=7 \
-     INSTALL_CHANNEL=pytorch-nightly BUILD_TYPE=official devel-image
+     INSTALL_CHANNEL=pytorch-nightly BUILD_TYPE=official \
+     DOCKER_ORG=torchbench \
+     DOCKER_TAG=${IMAGE_TAG} \
+     EXTRA_DOCKER_BUILD_FLAGS=--no-cache devel-image
 popd
+
+# Build Successful. Save the pytorch image tag in benchmarking summary
+mkdir -p $DATA_DIR
+echo "${IMAGE_NAME}:${IMAGE_TAG}" > ${DATA_DIR}/summary.txt
+# Retag the image
+docker image tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_NAME}:latest
