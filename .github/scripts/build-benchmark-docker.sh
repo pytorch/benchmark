831d8a67b9079eec840ac45611a06d0fda2a4af7
xz <xzhao9@fb.com>
2020-12-18 17:46:38 -0500

    Add nightly build pipeline for 0.1 (#175)

---
+++ .github/scripts/build-benchmark-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -euo pipefail
+
+TODAY=$(date +"%Y%m%d")
+IMAGE_NAME=torchbench/pytorch-benchmark
+IMAGE_TAG=${TODAY}_gh${GITHUB_RUN_ID}
+
+docker build -t ${IMAGE_NAME}:${IMAGE_TAG} --no-cache docker
+
+# Retag to pytorch-benchmark:latest
+docker image tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_NAME}:latest
---
+++ .github/scripts/upload-scribe.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# [FB Internal] Upload every json file to Scribe
+
+CONFIG_FILE=/config/${CONFIG_FILE}
+
+set -eo pipefail
+
+if [[ ! -v SCRIBE_GRAPHQL_ACCESS_TOKEN ]]; then
+    echo "SCRIBE_GRAPHQL_ACCESS_TOKEN is not set! Please check your environment variable. Stop."
+    exit 1
+fi
+
+pushd /workspace/benchmark/score
+
+TORCHBENCH_SCORE=$(python compute_score.py --configuration ${CONFIG_FILE} --benchmark_data_dir /output | awk 'NR>2' )
+
+IFS=$'\n'
+for line in $TORCHBENCH_SCORE ; do
+    JSON_NAME=$(echo $line | tr -s " " | cut -d " " -f 1)
+    SCORE=$(echo $line | tr -s " " | cut -d " " -f 2)
+    python3 ../scripts/upload_scribe.py --pytest_bench_json /output/${JSON_NAME} \
+            --torchbench_score $SCORE
+done
+
+popd
