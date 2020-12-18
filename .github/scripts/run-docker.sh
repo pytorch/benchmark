d19df08fcb1c1373c1802ec6cb4425dc738a237c
Xu Zhao <xzhao9@fb.com>
2020-12-18 16:40:59 -0500

    Add score config v0 and nightly testing workflow to version 0.1.

---
+++ .github/scripts/run-docker.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+set -euo pipefail
+
+# Version of the config
+CONFIG_VER=v0
+
+# Run Parameters
+RUN_SCRIPT=$1
+CONFIG_DIR=${PWD}/score/configs/${CONFIG_VER}
+CONFIG_ENV=${CONFIG_DIR}/config-${CONFIG_VER}.env
+# Use the latest pytorch-benchmark image
+TORCH_IMAGE_ID=torchbench/pytorch-benchmark:latest
+NO_TURBO_FILE="/sys/devices/system/cpu/intel_pstate/no_turbo"
+if [ -z "$2" ]; then
+    DATA_DIR=${HOME}/benchmark-results/gh${GITHUB_RUN_ID}
+else
+    DATA_DIR=$2
+fi
+
+# Load environment variables
+set -a;
+source ${CONFIG_ENV}
+set +a;
+
+mkdir -p ${DATA_DIR}
+
+# Set NO_TURBO
+if [[ -e ${NO_TURBO_FILE} ]]; then
+    sh -c "echo 1 > ${NO_TURBO_FILE}"
+fi
+
+export CUDA_VISIBLE_DEVICES=${GPU_LIST}
+# Nvidia won't let this run inside docker
+# Make sure the Nvidia GPU is in persistence mode
+sudo nvidia-smi -pm ENABLED -i ${GPU_LIST}
+# Set the <memory, graphics> clock frequency
+# Need further study on how Nvidia card throttling affect overall performance variance
+sudo nvidia-smi -ac ${GPU_FREQUENCY}
+
+docker run \
+       --env GITHUB_RUN_ID \
+       --env SCRIBE_GRAPHQL_ACCESS_TOKEN \
+       --env-file=${CONFIG_ENV} \
+       --volume="${PWD}:/runner" \
+       --volume="${DATA_DIR}:/output" \
+       --volume="${CONFIG_DIR}:/config" \
+       --gpus device=${GPU_LIST} \
+       $TORCH_IMAGE_ID \
+       bash ${RUN_SCRIPT}
+
+echo "Benchmark finished successfully. Output data dir is benchmark-results/gh${GITHUB_RUN_ID}."
