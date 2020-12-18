a735d365c82946d0340c368c3ca979f70c53f8c6
Xu Zhao <xzhao9@fb.com>
2020-12-18 17:18:11 -0500

    Remove use of current dir.

--- .github/scripts/run-docker.sh
+++ .github/scripts/run-docker.sh
@@ -7,7 +7,7 @@ CONFIG_VER=v0
 
 # Run Parameters
 RUN_SCRIPT=$1
-CONFIG_DIR=${PWD}/score/configs/${CONFIG_VER}
+CONFIG_DIR=/workspace/benchmark/score/configs/${CONFIG_VER}
 CONFIG_ENV=${CONFIG_DIR}/config-${CONFIG_VER}.env
 # Use the latest pytorch-benchmark image
 TORCH_IMAGE_ID=torchbench/pytorch-benchmark:latest
@@ -42,7 +42,6 @@ docker run \
        --env GITHUB_RUN_ID \
        --env SCRIBE_GRAPHQL_ACCESS_TOKEN \
        --env-file=${CONFIG_ENV} \
-       --volume="${PWD}:/runner" \
        --volume="${DATA_DIR}:/output" \
        --volume="${CONFIG_DIR}:/config" \
        --gpus device=${GPU_LIST} \
