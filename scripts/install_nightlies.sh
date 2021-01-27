312064978835f8e59d6b98bf5c2d10581adbf70e
Will Constable <whc@fb.com>
2021-01-26 18:09:23 -0800

    switch 0.1 branch to pip for nightlies

--- scripts/install_nightlies.sh
+++ scripts/install_nightlies.sh
@@ -3,4 +3,10 @@ set -e
 
 . ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
-conda install -y pytorch torchtext torchvision -c pytorch-nightly
+
+# conda install -y pytorch torchtext torchvision -c pytorch-nightly
+
+# Changing to pip to work around https://github.com/pytorch/pytorch/issues/49375
+pip install -q numpy
+pip install -q --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu110/torch_nightly.html
+pip install -q --pre torchtext -f https://download.pytorch.org/whl/nightly/cu110/torch_nightly.html
