2dc08cf71ef167a897446083c10407a060fb9b59
Will Constable <whc@fb.com>
2021-01-27 09:54:18 -0800

    Clean up run_bench script (#197)
    - remove upload steps since timing measurement and score upload are happening on pytorch CI now.
    
    - shorten the runtime of pytest test_bench.py to make this CI faster, and focused on correctness rather than timing.

--- scripts/run_bench_and_upload.sh
+++ scripts/run_bench_and_upload.sh
@@ -3,13 +3,6 @@ set -e
 . ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
 
-# set credentials for git https pushing
-cat > ~/.netrc <<DONE
-machine github.com
-login pytorchbot
-password ${GITHUB_PYTORCHBOT_TOKEN}
-DONE
-
 if [ "$CIRCLE_BRANCH" = "master" ]
 then
     PYTEST_FILTER=""
@@ -21,35 +14,14 @@ BENCHMARK_DATA="`pwd`/.data"
 mkdir -p ${BENCHMARK_DATA}
 BENCHMARK_FILENAME=${CIRCLE_SHA1}_$(date +"%Y%m%d_%H%M%S").json
 BENCHMARK_ABS_FILENAME=${BENCHMARK_DATA}/${BENCHMARK_FILENAME}
-pytest test_bench.py --ignore_machine_config --setup-show --benchmark-sort=Name --benchmark-json=${BENCHMARK_ABS_FILENAME} -k "$PYTEST_FILTER"
 
-# Compute benchmark score
+# configure benchmark invocation to be as fast as possible since this run isn't used for actual timing results,
+# but it's still useful to see (imprecise) timing output for debug/diagnosis of adding a new model
+# 'real' timing and score computation are handled by pytorch CI github actions runner with performance-tuned machine
+# configure to make CI as fast as possible while still producing a json output to flush out bugs in compute_score.
+# need to modify defaults in test_bench.py before possible to override runtime cmd line:
+pytest test_bench.py --ignore_machine_config --setup-show --benchmark-max-time=0.001 --benchmark-min-rounds=1 --benchmark-warmup=off --benchmark-sort=Name --benchmark-json=${BENCHMARK_ABS_FILENAME} -k "$PYTEST_FILTER"
+
+# Compute benchmark score, just to check that the script is not crashing
+# real score computation is handled by pytorch CI using a performance-tuned machine
 TORCHBENCH_SCORE=$(python compute_score.py --configuration torchbenchmark/score/torchbench_0.0.yaml --benchmark_data_file ${BENCHMARK_ABS_FILENAME})
-# Token is only present for certain jobs, only upload if present
-if [ -z "$SCRIBE_GRAPHQL_ACCESS_TOKEN" ]
-then
-    echo "Skipping benchmark upload, token is missing."
-else
-    python scripts/upload_scribe.py --pytest_bench_json ${BENCHMARK_ABS_FILENAME} --torchbench_score ${TORCHBENCH_SCORE}
-fi
-
-# Store the files with respect to the repo that their hash refers to
-# separate master and pull_requests just for conveneince in the UI
-if [ "$CIRCLE_BRANCH" = "master" ]
-then
-    git clone https://github.com/wconstab/pytorch_benchmark_data
-    pushd pytorch_benchmark_data
-    git config --global user.name "pytorchbot"
-    git config --global --unset url.ssh://git@github.com.insteadof || true
-    SUBDIR="${CIRCLE_PROJECT_REPONAME}/master"
-    mkdir -p $SUBDIR/
-    cp ${BENCHMARK_ABS_FILENAME} $SUBDIR/
-    git add ${SUBDIR}/${BENCHMARK_FILENAME}
-    ln -s -f ${SUBDIR}/${BENCHMARK_FILENAME} ${SUBDIR}/latest
-    echo ${BENCHMARK_FILENAME} >> ${SUBDIR}/history
-    git add ${SUBDIR}/latest ${SUBDIR}/history
-    git commit -m"Add ${BENCHMARK_FILENAME} - to reproduce results, see pytorch and machine versions in .json"
-    git push origin master
-    popd
-fi
-
