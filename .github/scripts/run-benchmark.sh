5d37156b1b611df27c4e288c99f35ee5641f4730
Xu Zhao <xzhao9@fb.com>
2021-01-27 17:44:41 -0500

    Restore model to their full names. Remove torchbench_0.0.yaml.

--- .github/scripts/run-benchmark.sh
+++ .github/scripts/run-benchmark.sh
@@ -16,7 +16,7 @@ for c in $(seq 1 $NUM_ITER); do
     taskset -c "${CORE_LIST}" pytest test_bench.py -k "${BENCHMARK_FILTER}" --benchmark-min-rounds "${NUM_ROUNDS}" \
                               --benchmark-json /output/${DATA_JSON_PREFIX}_${c}.json
     # Fill in circle_build_num and circle_project_reponame
-    jq --arg run_id "${GITHUB_RUN_ID}" --arg config_version "githubactions-benchmark-${CONFIG_VER}-metal" \
+    jq --arg run_id "${GITHUB_RUN_ID}" --arg config_version "githubactions-benchmark-${CONFIG_VER}-metal-fullname" \
        '.machine_info.circle_project_name=$config_version | .machine_info.circle_build_num=$run_id' \
        /output/${DATA_JSON_PREFIX}_${c}.json > /output/${DATA_JSON_PREFIX}_${c}.json.tmp
     mv /output/${DATA_JSON_PREFIX}_${c}.json.tmp /output/${DATA_JSON_PREFIX}_${c}.json
