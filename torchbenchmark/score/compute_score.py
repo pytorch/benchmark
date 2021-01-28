fa9c3b67367e2cabd3f45ef767b1f10fa8169e93
Nikitha Malgi <nikithamalgi@fb.com>
2021-01-28 11:05:11 -0800

    Remove score.yml and generate default spec

--- torchbenchmark/score/compute_score.py
+++ torchbenchmark/score/compute_score.py
@@ -9,10 +9,43 @@ import sys
 import os
 import yaml
 
+from collections import defaultdict
 from tabulate import tabulate
+from torchbenchmark import list_models
 
-SPEC_FILE_DEFAULT = "score.yml"
-TARGET_SCORE_DEFAULT = 1000
+#SPEC_FILE_DEFAULT = "score.yml"
+
+def generate_spec():
+    """
+    Helper function which constructs the spec dictionary by iterating
+    over the existing models. This API is used for generating the
+    default spec hierarchy configuration.
+    return type:
+        Returns a dictionary of type collections.defaultdict. Defaultdict
+        handles `None` or missing values and hence, no explicit `None` check
+        is required.
+    Arguments: `None`
+    Note: Only those models with `domain` and `task` class attributes are
+          used to construct the spec hierarchy.
+    """
+    spec = {'hierarchy':{'model':defaultdict(dict)}}
+    # These are the models required to generate the default spec.
+    # Please update this list if any new model needs to be a part of
+    # the default spec configuration.
+    required_models = ['pytorch_mobilenet_v3', 'yolov3', 'attention_is_all_you_need_pytorch', \
+                       'BERT_pytorch', 'fastNLP', 'dlrm', 'LearningToPaint', 'moco', 'demucs', \
+                       'pytorch_struct']
+
+    for model in list_models():
+        if model.name in required_models and hasattr(model, 'domain'):
+            if not spec['hierarchy']['model'][model.domain]:
+                spec['hierarchy']['model'][model.domain] = defaultdict(dict)
+            if not spec['hierarchy']['model'][model.domain][model.task]:
+                spec['hierarchy']['model'][model.domain][model.task] = defaultdict(dict)
+            spec['hierarchy']['model'][model.domain][model.task][model.name] = None
+
+    assert len(spec) == 0, f"Spec is empty. Make sure to use models only in the required list."
+    return spec
 
 def compute_score(config, data, fake_data=None):
     target = config['target']
