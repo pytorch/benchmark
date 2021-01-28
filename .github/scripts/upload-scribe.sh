d6c34ba1b3c577dc93f4ee2466bbba43643a9549
Xu Zhao <xzhao9@fb.com>
2021-01-28 18:58:10 -0500

    Fix a bug in CI script of computing score.

--- .github/scripts/upload-scribe.sh
+++ .github/scripts/upload-scribe.sh
@@ -11,7 +11,7 @@ if [[ ! -v SCRIBE_GRAPHQL_ACCESS_TOKEN ]]; then
     exit 1
 fi
 
-pushd /workspace/benchmark/torchbenchmark/score
+pushd /workspace/benchmark
 
 TORCHBENCH_SCORE=$(python compute_score.py --configuration ${CONFIG_FILE} --benchmark_data_dir /output | awk 'NR>2' )
 
@@ -19,7 +19,7 @@ IFS=$'\n'
 for line in $TORCHBENCH_SCORE ; do
     JSON_NAME=$(echo $line | tr -s " " | cut -d " " -f 1)
     SCORE=$(echo $line | tr -s " " | cut -d " " -f 2)
-    python3 ../scripts/upload_scribe.py --pytest_bench_json /output/${JSON_NAME} \
+    python3 .github/scripts/upload_scribe.py --pytest_bench_json /output/${JSON_NAME} \
             --torchbench_score $SCORE
 done
 
