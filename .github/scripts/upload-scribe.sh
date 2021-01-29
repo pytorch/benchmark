de79b6229a733a0197707947278af4ec11ce19a6
xz <xzhao9@fb.com>
2021-01-28 20:28:45 -0500

    Fix upload_scribe.sh path error. (#217)

--- .github/scripts/upload-scribe.sh
+++ .github/scripts/upload-scribe.sh
@@ -19,7 +19,7 @@ IFS=$'\n'
 for line in $TORCHBENCH_SCORE ; do
     JSON_NAME=$(echo $line | tr -s " " | cut -d " " -f 1)
     SCORE=$(echo $line | tr -s " " | cut -d " " -f 2)
-    python3 .github/scripts/upload_scribe.py --pytest_bench_json /output/${JSON_NAME} \
+    python3 scripts/upload_scribe.py --pytest_bench_json /output/${JSON_NAME} \
             --torchbench_score $SCORE
 done
 
