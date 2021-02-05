1edb9dad68553d745edd43ca0f7898f0f02ef12d
Xu Zhao <xzhao9@fb.com>
2021-02-05 16:41:50 -0500

    Fix another bug in instruction count ci script.

--- .github/scripts/instr-count-benchmark/run-benchmark.sh
+++ .github/scripts/instr-count-benchmark/run-benchmark.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+
+set -xueo pipefail
+
+BASEDIR=$(dirname $0)
+set -a;
+source ${BASEDIR}/config.env
+set +a;
+
+conda activate ${CONDA_ENV_NAME}
+PYTORCH_VERSION=$(python -c "import torch; print(torch.__version__)")
+PYTORCH_GIT_VERSION=$(python -c "import torch; print(torch.version.git_version)" | head -c 7)
+echo "Running instruction benchmark for pytorch-${PYTORCH_VERSION}, commit SHA: ${PYTORCH_GIT_VERSION}"
+
+# Run the instruction count benchmark
