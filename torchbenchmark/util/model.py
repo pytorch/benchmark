f4ee4317c0db94dd3f0e4cb1652ad2c32987157f
Nick Korovaiko <korovaikon@gmail.com>
2021-02-05 12:51:18 -0800

    no grad (#181)

--- torchbenchmark/util/model.py
+++ torchbenchmark/util/model.py
@@ -4,6 +4,20 @@ import pandas as pd
 import typing
 from  collections.abc import Iterable
 import torch
+from contextlib import contextmanager
+
+
+@contextmanager
+def no_grad(val):
+    """Some meta-learning models (e.g. maml) may need to train a target(another) model
+    in inference runs
+    """
+    old_state = torch.is_grad_enabled()
+    try:
+        torch.set_grad_enabled(not val)
+        yield
+    finally:
+        torch.set_grad_enabled(old_state)
 
 class BenchmarkModel():
     """
@@ -24,6 +38,9 @@ class BenchmarkModel():
 
     def set_train(self):
         self._set_mode(True)
+
+    def eval_in_nograd(self):
+        return True
 
     def _set_mode(self, train):
         (model, _) = self.get_module()
