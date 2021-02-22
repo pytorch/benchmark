6bb1ef5bfbbddebf9073880a543ffc47dcfdb5b4
xz <xzhao9@fb.com>
2021-02-22 13:36:36 -0500

    Fix nightly install package on 0.1. (#265)
    * Fix nightly install script to cu102 on 0.1.
    
    * Remove CI score upload on circle ci.

--- scripts/run_bench_and_upload.sh
+++ scripts/run_bench_and_upload.sh
@@ -25,37 +25,3 @@ pytest test_bench.py --ignore_machine_config --setup-show --benchmark-sort=Name 
 
 # Compute benchmark score
 TORCHBENCH_SCORE=$(python compute_score.py --configuration torchbenchmark/score/configs/v0/config-v0.yaml --benchmark_data_file ${BENCHMARK_ABS_FILENAME})
-# Token is only present for certain jobs, only upload if present
-if [ -z "$SCRIBE_GRAPHQL_ACCESS_TOKEN" ]
-then
-    echo "Skipping benchmark upload, token is missing."
-else
-    python scripts/upload_scribe.py --pytest_bench_json ${BENCHMARK_ABS_FILENAME} --torchbench_score ${TORCHBENCH_SCORE}
-fi
-
-
-git clone https://github.com/wconstab/pytorch_benchmark_data
-pushd pytorch_benchmark_data
-git config --global user.name "pytorchbot"
-git config --global --unset url.ssh://git@github.com.insteadof || true
-
-# Store the files with respect to the repo that their hash refers to
-# separate master and pull_requests just for conveneince in the UI
-if [ "$CIRCLE_BRANCH" = "master" ]
-then
-    SUBDIR="${CIRCLE_PROJECT_REPONAME}/master"
-else
-    SUBDIR="${CIRCLE_PROJECT_REPONAME}/pull_requests"
-fi
-mkdir -p $SUBDIR/
-cp ${BENCHMARK_ABS_FILENAME} $SUBDIR/
-git add ${SUBDIR}/${BENCHMARK_FILENAME}
-if [ "$CIRCLE_BRANCH" = "master" ]
-then
-    ln -s -f ${SUBDIR}/${BENCHMARK_FILENAME} ${SUBDIR}/latest
-    echo ${BENCHMARK_FILENAME} >> ${SUBDIR}/history
-    git add ${SUBDIR}/latest ${SUBDIR}/history
-fi
-git commit -m"Add ${BENCHMARK_FILENAME} - to reproduce results, see pytorch and machine versions in .json"
-git push origin master
-popd
