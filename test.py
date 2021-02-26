9f42a865bc6aa7e242bf715727f9e4e196ffc892
Jason Ansel <jansel@fb.com>
2021-02-25 18:38:53 -0800

    Add FX-based profiler (#256)

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ Setup and Run hub models.
 Make sure to enable an https proxy if necessary, or the setup steps may hang.
 """
 # This file shows how to use the benchmark suite from user end.
-import argparse
-import time
-from torchbenchmark import list_models
-from unittest import TestCase
-import re, sys, unittest
-import os.path
-import torch
 import gc
+import unittest
+from unittest import TestCase
+from unittest.mock import patch
+
+import torch
+from torchbenchmark import list_models
+
 
 class TestBenchmark(TestCase):
     def setUp(self):
@@ -27,8 +27,16 @@ class TestBenchmark(TestCase):
             self.assertEqual(self.memory, memory)
             torch.cuda.empty_cache()
 
-def run_model(model_class, model_path, device):
-    m = model_class(device=device)
+    def test_fx_profile(self):
+        try:
+            from torch.fx.interpreter import Interpreter
+        except ImportError:  # older versions of PyTorch
+            raise unittest.SkipTest("Requires torch>=1.8")
+        from fx_profile import main, ProfileAggregate
+        with patch.object(ProfileAggregate, "save") as mock_save:
+            # just run one model to make sure things aren't completely broken
+            main(["--repeat=1", "--filter=pytorch_struct", "--device=cpu"])
+            self.assertGreaterEqual(mock_save.call_count, 1)
 
 
 def _load_test(model_class, device):
@@ -48,18 +56,14 @@ def _load_test(model_class, device):
     def train(self):
         m = model_object(self)
         try:
-            start = time.time()
             m.train()
-            print('Finished training on device: {} in {}s.'.format(device, time.time() - start))
         except NotImplementedError:
             self.skipTest('Method train is not implemented, skipping...')
 
     def eval(self):
         m = model_object(self)
         try:
-            start = time.time()
             m.eval()
-            print('Finished eval on device: {} in {}s.'.format(device, time.time() - start))
         except NotImplementedError:
             self.skipTest('Method eval is not implemented, skipping...')
 
@@ -72,6 +76,7 @@ def _load_tests():
     for Model in list_models():
         for device in ('cpu', 'cuda'):
             _load_test(Model, device)
+
 
 _load_tests()
 if __name__ == '__main__':
