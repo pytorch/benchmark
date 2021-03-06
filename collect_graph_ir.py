3316b60b01fdee4d8ccc6de32111335ee0c176c9
Peng Wu <pengwu@fb.com>
2021-03-06 01:27:47 +0000

    Add options to collect graphs at different stage and w/o profiling executor

--- collect_graph_ir.py
+++ collect_graph_ir.py
@@ -12,45 +12,63 @@ import torch
 NO_JIT = {"demucs", "dlrm", "maml", "yolov3", "moco", "pytorch_CycleGAN_and_pix2pix", "tacotron2"}
 NO_GET_MODULE = {"Background_Matting"}
 
-def get_dump_filename(name, device):
-    return f"{name}.{device}.last_executed_graph_dump.log"
+def get_dump_filename(name, device, args):
+    if args.no_profiling:
+        return f"{name}.{device}.last_executed_graph.noprofile.log"
+    if args.inlined_graph:
+        return f"{name}.{device}.inlined_graph.log"
+    return f"{name}.{device}.last_executed_graph.log"
 
 def iter_models(args):
     device = "cpu"
     for benchmark_cls in list_models():
-        #if (not re.search("|".join(args.filter), benchmark_cls.name, re.I) or
-        #        re.search("|".join(args.exclude), benchmark_cls.name, re.I) or
-        if benchmark_cls.name in NO_GET_MODULE:
-            print(f"{benchmark_cls.name} has no get_module, skipped")
+        bench_name = benchmark_cls.name
+        if args.benchmark and args.benchmark != bench_name:
             continue
-        if benchmark_cls.name in NO_JIT:
-            print(f"{benchmark_cls.name} has no scripted module, skipped")
+        if bench_name in NO_GET_MODULE:
+            print(f"{bench_name} has no get_module, skipped")
+            continue
+        if bench_name in NO_JIT:
+            print(f"{bench_name} has no scripted module, skipped")
             continue
         try:
             # disable profiling mode so that the collected graph does not contain
             # profiling node
-            torch._C._jit_set_profiling_mode(False)
+            if args.no_profiling:
+                torch._C._jit_set_profiling_mode(False)
 
             benchmark = benchmark_cls(device=device, jit=True)
             model, example_inputs = benchmark.get_module()
 
-            if benchmark_cls.name == "BERT_pytorch":
-                # extract ScriptedModule object for BERT model
+            # extract ScriptedModule object for BERT model
+            if bench_name == "BERT_pytorch":
                 model = model.bert
 
-            fname = get_dump_filename(benchmark.name, device)
-            print(f"Dump Graph IR for {benchmark.name} to {fname}")
+            fname = get_dump_filename(bench_name, device, args)
+            print(f"Dump Graph IR for {bench_name} to {fname}")
+            
+            # default mode need to warm up ProfileExecutor
+            if not (args.no_profiling or args.inlined_graph):
+                model.graph_for(*example_inputs)
+
             with open(fname, 'w') as dump_file:
-                print(model.graph_for(*example_inputs), file=dump_file)
+                if args.inlined_graph:
+                    print(model.inlined_graph, file=dump_file)
+                else:
+                    print(model.graph_for(*example_inputs), file=dump_file)
         except NotImplementedError:
-            print(f"Cannot collect graph IR dump for {benchmark.name}")
+            print(f"Cannot collect graph IR dump for {bench_name}")
             pass
 
 def main(args=None):
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument("--filter", "-k", action="append",
-    #                    help="filter benchmarks")
-    #args = parser.parse_args(args)
+    parser = argparse.ArgumentParser(description="dump last_executed graph for all benchmarks with JIT implementation")
+    parser.add_argument("--benchmark", "-b", 
+                        help="dump graph for <BENCHMARK>")
+    parser.add_argument("--no_profiling", action="store_true",
+                        help="dump last_executed graphs w/o profiling executor")
+    parser.add_argument("--inlined_graph", action="store_true",
+                        help="dump graphs dumped by module.inlined_graph")
+    args = parser.parse_args(args)
 
     iter_models(args)
 
