2a0a0652d39153ba192def27371eeb9c1c728444
Richard Zou <zou3519@gmail.com>
2021-04-07 11:34:38 -0700

    [hackathon] Add differentially-private cifar10 training
    Fixes #340.
    
    This PR adds an example of differentially-private (DP) training on
    the cifar10 dataset. In reality this uses resnet18 as the model and
    example inputs with the size of those found in the cifar10 dataset.
    
    The model is not tracable or scriptable. This is because the DP
    training relies on autograd hooks and those cannot be traced through or
    scripted yet.
    
    Test Plan:
    
    All of the following pass, is there anywhere else I should add a test?
    - `python test.py -k test_opacus_cifar10_example_cpu`
    - `python test.py -k test_opacus_cifar10_eval_cpu`
    - `python test.py -k test_opacus_cifar10_train_cpu`

---
+++ torchbenchmark/models/opacus_cifar10/__init__.py
@@ -0,0 +1,83 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torchvision.models as models
+from opacus.utils.module_modification import convert_batchnorm_modules
+from opacus import PrivacyEngine
+
+from ...util.model import BenchmarkModel
+from torchbenchmark.tasks import OTHER
+
+
+class Model(BenchmarkModel):
+    task = OTHER.OTHER_TASKS
+    def __init__(self, device=None, jit=False):
+        super().__init__()
+        self.device = device
+        self.jit = jit
+
+        self.model = models.resnet18(num_classes=10)
+        self.model = convert_batchnorm_modules(self.model)
+        self.model = self.model.to(device)
+
+        self.example_inputs = (
+            torch.randn((64, 3, 32, 32), device=self.device),
+        )
+        self.example_target = torch.randint(0, 10, (64,), device=self.device)
+
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+        self.criterion = nn.CrossEntropyLoss()
+
+        # This is supposed to equal the number of data points.
+        # It is only to compute stats so dwai about the value.
+        sample_size = 64 * 100
+        clipping = {"clip_per_layer": False, "enable_stat": False}
+        self.privacy_engine = PrivacyEngine(
+            self.model,
+            batch_size=64,
+            sample_size=sample_size,
+            alphas=[1 + x / 10.0 for x in range(1, 100)] + list(range(12, 64)),
+            noise_multiplier=1.0,
+            max_grad_norm=1.0,
+            secure_rng=False,
+            **clipping,
+        )
+        self.privacy_engine.attach(self.optimizer)
+
+    def get_module(self):
+        if self.jit:
+            raise NotImplementedError()
+
+        return self.model, self.example_inputs
+
+    def train(self):
+        if self.jit:
+            raise NotImplementedError()
+
+        model, (images,) = self.get_module()
+        model.train()
+        targets = self.example_target
+
+        output = model(images)
+        loss = self.criterion(output, targets)
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+    def eval(self):
+        if self.jit:
+            raise NotImplementedError()
+
+        model, (images,) = self.get_module()
+        model.eval()
+        targets = self.example_target
+        with torch.no_grad():
+            model(images)
+
+
+if __name__ == "__main__":
+    m = Model(device="cuda", jit=False)
+    module, example_inputs = m.get_module()
+    module(*example_inputs)
+    m.train()
+    m.eval()
