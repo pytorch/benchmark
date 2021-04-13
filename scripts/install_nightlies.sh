3bf3d0a509c860fd26af2dd9552bc4505a7f044d
Xu Zhao <xzhao9@fb.com>
2021-04-13 18:22:44 -0400

    Fix Circle CI PR correctness check. (#357)
    - Use gpu.nvidia.medium as the runner because it has Telsa T4 GPU.
    - Fixes the Huggingface model unittests by accepting kwargs in test.py
    - Use CUDA 11.2

--- scripts/install_nightlies.sh
+++ scripts/install_nightlies.sh
@@ -7,16 +7,5 @@ conda activate base
 # conda install -y pytorch torchvision -c pytorch-nightly
 # Changing to pip to work around https://github.com/pytorch/pytorch/issues/49375
 pip install -q numpy
-pip install -q --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html
-
-# separating to debug issue where when installing all 3 this error printed
-# 
-# UnsatisfiableError: The following specifications were found
-# to be incompatible with the existing python installation in your environment:
-#
-# Specifications:
-#
-#   - torchtext -> python[version='>=2.7,<2.8.0a0|>=3.5,<3.6.0a0']
-
-# conda install -y torchtext -c pytorch-nightly
-pip install -q --pre torchtext -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html
+pip install -q --pre torch torchvision torchtext \
+    -f https://download.pytorch.org/whl/nightly/cu112/torch_nightly.html
