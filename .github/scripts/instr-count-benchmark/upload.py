2b970fd183fe4072fb992646e41301db30cc58ec
Taylor Robie <taylorrobie@fb.com>
2021-04-19 10:31:08 -0700

    remove most fields to debug

--- .github/scripts/instr-count-benchmark/upload.py
+++ .github/scripts/instr-count-benchmark/upload.py
@@ -18,38 +18,39 @@ class PytorchMicrobenchmarkUploader(ScribeUploader):
         self.schema = {
             'int': [
                 'time',
-                'start_time',
-                'end_time',
-                'task_number',
+                # 'start_time',
+                # 'end_time',
+                # 'task_number',
 
-                # This is the version of the instruction count microbenchmark.
-                'benchmark_version',
+                # # This is the version of the instruction count microbenchmark.
+                # 'benchmark_version',
 
                 # We don't need to record as many summary statistics for counts
                 # as we do for times, because we retain them all in
                 # `counts_json`.
-                'count_min', 'count_max',
-                'count_p25', 'count_median', 'count_p75',
+                'count_min',
+                # 'count_max',
+                # 'count_p25', 'count_median', 'count_p75',
             ],
             'normal': [
-                # Unique ID for the run.
-                'run_id',
+                # # Unique ID for the run.
+                # 'run_id',
 
-                # Metadata for ad-hoc analysis.
-                'task_key',
+                # # Metadata for ad-hoc analysis.
+                # 'task_key',
                 # 'counts_json',
 
-                # TODO: More environment info.
-                'pytorch_version',
-                'pytorch_git_version',
-                'cuda_version',
-                'python_version',
-                'benchmark_name',
+                # # TODO: More environment info.
+                # 'pytorch_version',
+                # 'pytorch_git_version',
+                # 'cuda_version',
+                # 'python_version',
+                # 'benchmark_name',
             ],
             'float': [
-                't_min', 't_max', 't_mean',
-                't_p01', 't_p25', 't_median', 't_p75', 't_p99',
-                't_stddev',
+                # 't_min', 't_max', 't_mean',
+                # 't_p01', 't_p25', 't_median', 't_p75', 't_p99',
+                # 't_stddev',
             ]
         }
 
@@ -62,15 +63,15 @@ class PytorchMicrobenchmarkUploader(ScribeUploader):
 
         base_msg = {
             'time': int(time.time()),
-            'run_id': os.getenv('GITHUB_RUN_ID'),
-            'start_time': result_json['start_time'],
-            'end_time': result_json['end_time'],
-            'benchmark_version': result_json['version'],
-            'pytorch_version': torch.__version__,
-            'pytorch_git_version': torch.version.git_version,
-            'cuda_version': torch.version.cuda,
-            'python_version': '.'.join([str(i) for i in sys.version_info[:3]]),
-            'benchmark_name': 'instruction_count',
+            # 'run_id': os.getenv('GITHUB_RUN_ID'),
+            # 'start_time': result_json['start_time'],
+            # 'end_time': result_json['end_time'],
+            # 'benchmark_version': result_json['version'],
+            # 'pytorch_version': torch.__version__,
+            # 'pytorch_git_version': torch.version.git_version,
+            # 'cuda_version': torch.version.cuda,
+            # 'python_version': '.'.join([str(i) for i in sys.version_info[:3]]),
+            # 'benchmark_name': 'instruction_count',
         }
 
         messages = []
@@ -78,30 +79,31 @@ class PytorchMicrobenchmarkUploader(ScribeUploader):
             times = measurements["times"]
             counts = measurements["counts"]
             stats_msg = {
-                'task_number': task_number,
-                'task_key': key,
+                # 'task_number': task_number,
+                # 'task_key': key,
 
                 'count_min': min(counts),
-                'count_max': max(counts),
-                'count_p25': int(np.percentile(counts, 25)),
-                'count_median': int(np.median(counts)),
-                'count_p75': int(np.percentile(counts, 75)),
+                # 'count_max': max(counts),
+                # 'count_p25': int(np.percentile(counts, 25)),
+                # 'count_median': int(np.median(counts)),
+                # 'count_p75': int(np.percentile(counts, 75)),
                 # 'counts_json': json.dumps(counts),
 
-                't_min': min(times),
-                't_max': max(times),
-                't_mean': float(np.mean(times)),
-                't_p01': float(np.percentile(times, 1)),
-                't_p25': float(np.percentile(times, 25)),
-                't_median': float(np.median(times)),
-                't_p75': float(np.percentile(times, 75)),
-                't_p99': float(np.percentile(times, 99)),
-                't_stddev': float(np.std(times)),
+                # 't_min': min(times),
+                # 't_max': max(times),
+                # 't_mean': float(np.mean(times)),
+                # 't_p01': float(np.percentile(times, 1)),
+                # 't_p25': float(np.percentile(times, 25)),
+                # 't_median': float(np.median(times)),
+                # 't_p75': float(np.percentile(times, 75)),
+                # 't_p99': float(np.percentile(times, 99)),
+                # 't_stddev': float(np.std(times)),
             }
 
             stats_msg.update(base_msg)
             messages.append(stats_msg)
-        self.upload(messages)
+            print(stats_msg)
+        self.upload(messages[:2])
 
 
 if __name__ == '__main__':
