92e2e63bc0ef2ea151bff3295c3e5a6e81b1b228
Taylor Robie <taylorrobie@fb.com>
2021-05-14 10:49:39 -0700

    check torch install

--- .github/scripts/instr-count-benchmark/run-benchmark.sh
+++ .github/scripts/instr-count-benchmark/run-benchmark.sh
@@ -10,7 +10,10 @@ set +a;
 . activate ${CONDA_ENV_NAME}
 PYTORCH_VERSION=$(python -c "import torch; print(torch.__version__)")
 PYTORCH_GIT_VERSION=$(python -c "import torch; print(torch.version.git_version)" | head -c 7)
-echo "Running instruction benchmark for pytorch-${PYTORCH_VERSION}, commit SHA: ${PYTORCH_GIT_VERSION}"
+PYTORCH_FILE=$(python -c "import torch; print(torch.__file__)")
+echo "Running instruction benchmark for pytorch-${PYTORCH_VERSION}, commit SHA: ${PYTORCH_GIT_VERSION}, PATH: ${PYTORCH_FILE}"
+
+python -c "import os; import torch; env_path = os.getenv('CONDA_PREFIX'); assert torch.__file__.startswith(env_path), f'{torch.__file__} not in {env_path}'"
 
 # Run the instruction count benchmark
 pushd ${BENCHMARK_ROOT}
