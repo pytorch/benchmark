9c44562f22c0f5dc740af9a954c5a402e501b5f4
Xu Zhao <xzhao9@fb.com>
2021-05-18 16:30:36 -0400

    Add speech transformer files.

--- torchbenchmark/models/speech_transformer/__init__.py
+++ torchbenchmark/models/speech_transformer/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import torch
 
-from .data import AudioDataLoader, AudioDataset
 from .decoder import Decoder
 from .encoder import Encoder
 from .transformer import Transformer
@@ -14,77 +13,39 @@ from torchbenchmark.tasks import SPEECH
 class Model(BenchmarkModel, device = "cuda", jit = False):
     task = SPEECH.RECOGNITION
     def __init__(self, device=None, jit=False):
-        self.train_json = None
-        self.valid_json = None
-        self.dict = None
-        self.cfg = SpeechTransformerConfig()
+        if jit:
+            raise NotImplementedError()
+        if not device == "cuda":
+            raise NotImplementedError()
+        self.jit = jit
+        self.device = device
+        self.traincfg = SpeechTransformerTrainConfig()
         self.evalcfg = SpeechTransformerEvalConfig()
-        self.tr_dataset = AudioDataset(self.train_json, self.cfg.batch_size,
-                                       self.cfg.maxlen_in, self.cfg.maxlen_out,
-                                       batch_frames=self.batch_frames)
-        self.cv_dataset = AudioDataset(self.valid_json, self.cfg.batch_size,
-                                       self.cfg.maxlen_in, self.cfg.maxlen_out,
-                                       batch_frames=self.cfg.batch_frames)
-        self.tr_loader = AudioDataLoader(self.tr_dataset, batch_size=1,
-                                         num_workers=self.cfg.num_workers,
-                                         shuffle=self.cfg.shuffle,
-                                         LFR_m=self.cfg.LFR_m,
-                                         LFR_n=self.cfg.LFR_n)
-        self.cv_loader = AudioDataLoader(self.cv_dataset, batch_size=1,
-                                         num_workers=self.cfg.num_workers,
-                                         LFR_m=self.cfg.LFR_m,
-                                         LFR_n=self.cfg.LFR_n)
-        self.char_list, self.sos_id, self.eos_id = process_dict(self.dict)
-        self.vocab_size = len(self.char_list)
-        self.encoder = Encoder(self.cfg.d_input * self.cfg.LFR_m,
-                               self.cfg.n_layers_enc,
-                               self.cfg.n_head,
-                               self.cfg.d_k, self.cfg.d_v,
-                               self.cfg.d_model, self.cfg.d_inner,
-                               dropout=self.cfg.dropout, pe_maxlen=self.cfg.pe_maxlen)
-        self.decoder = Decoder(self.sos_id, self.eos_id, self.vocab_size,
-                               self.cfg.d_word_vec, self.cfg.n_layers_dec, self.cfg.n_head,
-                               self.cfg.d_k, self.cfg.d_v, self.cfg.d_model, self.cfg.d_inner,
-                               dropout=self.cfg.dropout,
-                               tgt_emb_prj_weight_sharing=self.cfg.tgt_emb_prj_weight_sharing,
-                               pe_maxlen=self.cfg.pe_maxlen)
-        model = Transformer(encoder, decoder)
-        if self.device == "cuda":
-            model.cuda()
-        self.optimizer = TransformerOptimizer(torch.optim.Adam(model.parameters(), betas=(0.9, 0.98), eps=1e-09),
-                                              self.cfg.k, self.cfg.d_model, self.cfg.warmup_steps)
-        self.solver = Solver(data, model, optimizier, self.cfg)
-        # Read eval json data
-        with open(self.recog_json, 'rb') as f:
-            self.js = json.load(self.recog_json)['utts']
+        self.traincfg.model.cuda()
+        self.evalcfg.model.cuda()
 
     def get_module(self):
-        if self.device == "cpu":
+        if not self.device == "cuda":
             raise NotImplementedError()
         if self.jit:
             raise NotImplementedError()
+        return self.traincfg.model, self.traincfg.tr_loader
 
     def train(self, niter=1):
-        if self.device == "cpu":
+        if not self.device == "cuda":
             raise NotImplementedError()
         if self.jit:
             raise NotImplementedError()
         for _ in range(niter):
-            self.solver.train()
+            self.traincfg.train()
 
     def eval(self, niter=1):
-        if self.device == "cpu":
+        if not self.device == "cuda":
             raise NotImplementedError()
         if self.jit:
             raise NotImplementedError()
-        self.model.eval()
-        with torch.no_grad():
-            for _ in range(niter):
-                for idx, name in enumerate(self.js.keys(), 1):
-                    nbest_hyps = model.recognize(self.evalcfg.input,
-                                                 self.evalcfg.input_length,
-                                                 self.evalcfg.char_list)
-                    new_js[name] = add_results_to_json(js[name], nbest_hyps, self.char_list)
+        for _ in range(niter):
+            self.evalcfg.eval()
 
 if __name__ == '__main__':
     for device in ['cuda']:
--- torchbenchmark/models/speech_transformer/config.py
+++ torchbenchmark/models/speech_transformer/config.py
@@ -1,8 +1,15 @@
+import json
+import torch
+import kaldi_io
 import dataclasses
 
+from .transformer import Transformer
+from .utils import add_results_to_json, process_dict
+from .data import build_LFR_features
+from .data import AudioDataLoader, AudioDataset
 
 @dataclasses.dataclass
-class SpeechTransformerConfig:
+class SpeechTransformerTrainConfig:
     # Low Frame Rate
     LFR_m = 4
     LFR_n = 3
@@ -21,20 +28,76 @@ class SpeechTransformerConfig:
     tgt_emb_prj_weight_sharing = 1
     label_smoothing = 0.1
     # minibatch
-    shuffle = 0
-    batch_size = 32
-    batch_frames = 0
+    shuffle = 1
+    batch_size = 16
+    batch_frames = 15000
     maxlen_in = 800
     maxlen_out = 150
     num_workers = 4
     # optimizer
-    k = 1.0
+    k = 0.2
     warmup_steps = 4000
+    # input files
+    train_json = "input_data/train/data.json"
+    valid_json = "input_data/dev/data.json"
+    dict_txt = "input_data/lang_1char/train_chars.txt"
+    def __init__(self):
+        self.tr_dataset = AudioDataset(self.train_json, self.cfg.batch_size,
+                                       self.cfg.maxlen_in, self.cfg.maxlen_out,
+                                       batch_frames=self.batch_frames)
+        self.cv_dataset = AudioDataset(self.valid_json, self.cfg.batch_size,
+                                       self.cfg.maxlen_in, self.cfg.maxlen_out,
+                                       batch_frames=self.cfg.batch_frames)
+        self.tr_loader = AudioDataLoader(self.tr_dataset, batch_size=1,
+                                         num_workers=self.cfg.num_workers,
+                                         shuffle=self.cfg.shuffle,
+                                         LFR_m=self.cfg.LFR_m,
+                                         LFR_n=self.cfg.LFR_n)
+        self.cv_loader = AudioDataLoader(self.cv_dataset, batch_size=1,
+                                         num_workers=self.cfg.num_workers,
+                                         LFR_m=self.cfg.LFR_m,
+                                         LFR_n=self.cfg.LFR_n)
+        self.encoder = Encoder(self.cfg.d_input * self.cfg.LFR_m,
+                               self.cfg.n_layers_enc,
+                               self.cfg.n_head,
+                               self.cfg.d_k, self.cfg.d_v,
+                               self.cfg.d_model, self.cfg.d_inner,
+                               dropout=self.cfg.dropout, pe_maxlen=self.cfg.pe_maxlen)
+        self.decoder = Decoder(self.sos_id, self.eos_id, self.vocab_size,
+                               self.cfg.d_word_vec, self.cfg.n_layers_dec, self.cfg.n_head,
+                               self.cfg.d_k, self.cfg.d_v, self.cfg.d_model, self.cfg.d_inner,
+                               dropout=self.cfg.dropout,
+                               tgt_emb_prj_weight_sharing=self.cfg.tgt_emb_prj_weight_sharing,
+                               pe_maxlen=self.cfg.pe_maxlen)
+        self.model = Transformer(self.encoder, self.decoder)
+        self.optimizer = TransformerOptimizer(torch.optim.Adam(model.parameters(), betas=(0.9, 0.98), eps=1e-09),
+                                              self.k, self.d_model, self.warmup_steps)
+        self.solver = Solver(self.data, self.model, self.optimizer, self)
+    def train(self):
+        self.solver.train()
 
 @dataclasses.dataclass
 class SpeechTransformerEvalConfig:
-    beam_size = 1
+    beam_size = 5
     nbest = 1
-    decode_max_len = 0
+    decode_max_len = 100
+    recog_json = "input_data/test/data.json"
+    result_label = "input_data/decode/data.json"
+    dict_txt = "input_data/data/lang_1char/train_chars.txt"
+    model_path = "input_data/model/final.pth.tar"
     def __init__(self):
-        # Construct self.input
+        # Load model
+        self.model, self.LFR_m, self.LFR_n = Transformer.load_model(self.model_path)
+        # Read json data
+        with open(self.recog_json, "rb") as f:
+            self.js = json.load(f)['utts']
+    def eval(self):
+        with torch.no_grad():
+            for idx, name in enumerate(self.js.keys(), 1):
+                input = kaldi_io.read_mat(self.js[name]['input'][0]['feat'])
+                input = build_LFR_features(input, LFR_m, LFR_n)
+                input = torch.from_numpy(input).float()
+                input_length = torch.tensor([input.size(0)], dtype=torch.int)
+                input = input.cuda()
+                input_length = input_length.cuda()
+                nbest_hyps = self.model.recognize(input, input_length, char_list, args)
