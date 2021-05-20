f70576edf3c40afd0f2240f9c042cc3380c0dd01
Xu Zhao <xzhao9@fb.com>
2021-05-19 22:50:27 -0400

    Fix init function.

--- torchbenchmark/models/speech_transformer/__init__.py
+++ torchbenchmark/models/speech_transformer/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
 # The SpeechTransformer model copied from https://github.com/kaituoxu/Speech-Transformer, commit e684777.
+# The model only supports CUDA and eager mode.
 # The input data files in the input_data/ directory are generated with a minimized aishell data
 # containing the following files in the original dataset:
 # S0002.tar.gz, S0757.tar.gz, S0915.tar.gz
@@ -22,12 +23,12 @@ from torchbenchmark.tasks import SPEECH
 class Model(BenchmarkModel):
     task = SPEECH.RECOGNITION
     def __init__(self, device=None, jit=False):
-        if jit:
-            raise NotImplementedError()
-        if not device == "cuda":
-            raise NotImplementedError()
         self.jit = jit
         self.device = device
+        if jit:
+            return
+        if not device == "cuda":
+            return
         self.traincfg = SpeechTransformerTrainConfig()
         self.evalcfg = SpeechTransformerEvalConfig(self.traincfg)
         self.traincfg.model.cuda()
@@ -35,18 +36,18 @@ class Model(BenchmarkModel):
 
     def get_module(self):
         if not self.device == "cuda":
-            raise NotImplementedError()
+            raise NotImplementedError("CPU is not supported by this model")
         if self.jit:
-            raise NotImplementedError()
+            raise NotImplementedError("JIT is not supported by this model")
         for data in self.traincfg.tr_loader:
             padded_input, input_lengths, padded_target = data
             return self.traincfg.model, (padded_input.cuda(), input_lengths.cuda(), padded_target.cuda())
 
     def train(self, niter=1):
         if not self.device == "cuda":
-            raise NotImplementedError()
+            raise NotImplementedError("CPU is not supported by this model")
         if self.jit:
-            raise NotImplementedError()
+            raise NotImplementedError("JIT is not supported by this model")
         for i in range(niter):
             self.traincfg.train(epoch = i)
 
