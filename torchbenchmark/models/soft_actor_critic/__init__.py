69d8395a37cf03724b2b5f5812592884c601f9ec
Xu Zhao <xzhao9@fb.com>
2021-05-20 14:47:42 -0400

    Add the framework of rlkit models.

---
+++ torchbenchmark/models/soft_actor_critic/__init__.py
@@ -0,0 +1,9 @@
+import torch
+
+from ...util.model import BenchmarkModel
+from torchbenchmark.tasks import REINFORCEMENT_LEARNING
+
+class Model(BenchmarkModel):
+    task = REINFORCEMENT_LEARNING.OTHER_RL
+    def __init__():
+        pass
