23ded14c372cae0c03fb9eb6a84df8242b8ff068
Xu Zhao <xzhao9@fb.com>
2021-05-25 16:28:39 -0400

    Show the pytorch version to install.

--- scripts/install_nightlies.sh
+++ scripts/install_nightlies.sh
@@ -7,5 +7,5 @@ conda activate base
 # conda install -y pytorch torchvision -c pytorch-nightly
 # Changing to pip to work around https://github.com/pytorch/pytorch/issues/49375
 pip install -q numpy
-pip install -q --pre torch torchvision torchtext \
+pip install --pre torch torchvision torchtext \
     -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html
