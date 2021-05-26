195efd504730101833427cebe861eafcbedd9e62
Nick Korovaiko <korovaikon@gmail.com>
2021-05-26 10:00:55 -0700

    update template file; add optimize_for_inference (#378)
    * update template file; add optimize_for_inference
    
    * address Xu's feedback
    
    * try freezing
    
    * switch back to inference
    
    * print torch version
    
    * remove print version

--- torchbenchmark/models/mobilenet_v2/__init__.py
+++ torchbenchmark/models/mobilenet_v2/__init__.py
@@ -4,19 +4,40 @@ import torch
 import torch.optim as optim
 import torchvision.models as models
 from ...util.model import BenchmarkModel
+from torchbenchmark.tasks import COMPUTER_VISION
 
+#######################################################
+#
+#       DO NOT MODIFY THESE FILES DIRECTLY!!!
+#       USE `gen_torchvision_benchmarks.py`
+#
+#######################################################
 class Model(BenchmarkModel):
+    task = COMPUTER_VISION.CLASSIFICATION
     def __init__(self, device=None, jit=False):
         super().__init__()
         self.device = device
         self.jit = jit
         self.model = models.mobilenet_v2().to(self.device)
+        self.eval_model = models.mobilenet_v2().to(self.device)
+
         if self.jit:
             self.model = torch.jit.script(self.model)
-        self.example_inputs = (torch.randn((96, 3, 224, 224)).to(self.device),)
+            self.eval_model = torch.jit.script(self.eval_model)
+            # model needs to in `eval`
+            # in order to be optimized for inference
+            self.eval_model.eval()
+            self.eval_model = torch.jit.optimize_for_inference(self.eval_model)
+        self.example_inputs = (torch.randn((32, 3, 224, 224)).to(self.device),)
 
     def get_module(self):
         return self.model, self.example_inputs
+
+    # vision models have another model
+    # instance for inference that has
+    # already been optimized for inference
+    def set_eval(self):
+        pass
 
     def train(self, niter=3):
         optimizer = optim.Adam(self.model.parameters())
@@ -29,7 +50,8 @@ class Model(BenchmarkModel):
             optimizer.step()
 
     def eval(self, niter=1):
-        model, example_inputs = self.get_module()
+        model = self.eval_model
+        example_inputs = self.example_inputs
         example_inputs = example_inputs[0][0].unsqueeze(0)
         for i in range(niter):
             model(example_inputs)
@@ -41,4 +63,3 @@ if __name__ == "__main__":
     module(*example_inputs)
     m.train(niter=1)
     m.eval(niter=1)
-
--- torchbenchmark/models/squeezenet1_1/__init__.py
+++ torchbenchmark/models/squeezenet1_1/__init__.py
@@ -6,6 +6,12 @@ import torchvision.models as models
 from ...util.model import BenchmarkModel
 from torchbenchmark.tasks import COMPUTER_VISION
 
+#######################################################
+#
+#       DO NOT MODIFY THESE FILES DIRECTLY!!!
+#       USE `gen_torchvision_benchmarks.py`
+#
+#######################################################
 class Model(BenchmarkModel):
     task = COMPUTER_VISION.CLASSIFICATION
     def __init__(self, device=None, jit=False):
@@ -13,12 +19,25 @@ class Model(BenchmarkModel):
         self.device = device
         self.jit = jit
         self.model = models.squeezenet1_1().to(self.device)
+        self.eval_model = models.squeezenet1_1().to(self.device)
+
         if self.jit:
             self.model = torch.jit.script(self.model)
+            self.eval_model = torch.jit.script(self.eval_model)
+            # model needs to in `eval`
+            # in order to be optimized for inference
+            self.eval_model.eval()
+            self.eval_model = torch.jit.optimize_for_inference(self.eval_model)
         self.example_inputs = (torch.randn((32, 3, 224, 224)).to(self.device),)
 
     def get_module(self):
         return self.model, self.example_inputs
+
+    # vision models have another model
+    # instance for inference that has
+    # already been optimized for inference
+    def set_eval(self):
+        pass
 
     def train(self, niter=3):
         optimizer = optim.Adam(self.model.parameters())
@@ -31,7 +50,8 @@ class Model(BenchmarkModel):
             optimizer.step()
 
     def eval(self, niter=1):
-        model, example_inputs = self.get_module()
+        model = self.eval_model
+        example_inputs = self.example_inputs
         example_inputs = example_inputs[0][0].unsqueeze(0)
         for i in range(niter):
             model(example_inputs)
