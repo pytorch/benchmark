19f3333d79813cc984e739365bc582f767641ffc
Nikolay Korovaiko <korovaikon@gmail.com>
2021-06-02 15:02:34 -0700

    freeze learningtopaint

--- torchbenchmark/models/LearningToPaint/__init__.py
+++ torchbenchmark/models/LearningToPaint/__init__.py
@@ -45,11 +45,18 @@ class Model(BenchmarkModel):
         self.ground_truth = torch.tensor(self.ground_truth).float()
 
         net = net.to(self.device)
+        self.eval_model = net.to(self.device)
+        # model needs to in `eval`
+        # in order to be optimized for inference
+        self.eval_model.eval()
         train_batch = train_batch.to(self.device)
         self.ground_truth = self.ground_truth.to(self.device)
 
         if self.jit:
             net = torch.jit._script_pdt(net, example_inputs = [(train_batch, ), ])
+            self.eval_model = torch.jit._script_pdt(self.eval_model)
+            self.eval_model = torch.jit.optimize_for_inference(self.eval_model)
+
 
         self.module = net
         self.example_inputs = train_batch
@@ -57,6 +64,12 @@ class Model(BenchmarkModel):
 
     def get_module(self):
         return self.module,[self.example_inputs]
+
+    # LearningToPaint has another model
+    # instance for inference that has
+    # already been optimized for inference
+    def set_eval(self):
+        pass
 
     def train(self, niter=1):
         for _ in range(niter):
@@ -68,7 +81,7 @@ class Model(BenchmarkModel):
 
     def eval(self, niter=1):
         for _ in range(niter):
-            self.module(self.example_inputs)
+            self.eval_model(self.example_inputs)
 
 if __name__ == '__main__':
     m = Model(device='cpu', jit=False)
