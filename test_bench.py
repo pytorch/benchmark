6b377b81852b5333f64e743c4d3de19787d63f41
Taylor Robie <taylorrobie@fb.com>
2021-08-21 21:34:50 -0700

    add 20 minute timeout to benchmark runner (#427)

--- test_bench.py
+++ test_bench.py
@@ -21,6 +21,15 @@ from torchbenchmark import _list_model_paths, ModelTask
 from torchbenchmark.util.machine_config import get_machine_state
 
 
+# Some of the models have very heavyweight setup, so we have to set a very
+# generous limit. That said, we don't want the entire test suite to hang if
+# a single test encounters an extreme failure, so we give up after a test
+# is unresponsive for 20 minutes. (Note: this does not require that the entire
+# test case completes in 20 minutes. It requires that if the worker is
+# unresponsive for 20 minutes the parent will presume it dead / incapacitated.)
+TIMEOUT = 1200  # Seconds
+
+
 def pytest_generate_tests(metafunc):
     # This is where the list of models to test can be configured
     # e.g. by using info in metafunc.config
@@ -50,7 +59,7 @@ class TestBenchNetwork:
 
     def test_train(self, model_path, device, compiler, benchmark):
         try:
-            task = ModelTask(model_path)
+            task = ModelTask(model_path, timeout=TIMEOUT)
             if not task.model_details.exists:
                 return  # Model is not supported.
 
@@ -64,7 +73,7 @@ class TestBenchNetwork:
 
     def test_eval(self, model_path, device, compiler, benchmark, pytestconfig):
         try:
-            task = ModelTask(model_path)
+            task = ModelTask(model_path, timeout=TIMEOUT)
             if not task.model_details.exists:
                 return  # Model is not supported.
 
@@ -92,14 +101,14 @@ class TestWorker:
     """Benchmark SubprocessWorker to make sure we aren't skewing results."""
 
     def test_worker_noop(self, benchmark):
-        worker = subprocess_worker.SubprocessWorker()
+        worker = subprocess_worker.SubprocessWorker(timeout=TIMEOUT)
         benchmark(lambda: worker.run("pass"))
 
     def test_worker_store(self, benchmark):
-        worker = subprocess_worker.SubprocessWorker()
+        worker = subprocess_worker.SubprocessWorker(timeout=TIMEOUT)
         benchmark(lambda: worker.store("x", 1))
 
     def test_worker_load(self, benchmark):
-        worker = subprocess_worker.SubprocessWorker()
+        worker = subprocess_worker.SubprocessWorker(timeout=TIMEOUT)
         worker.store("x", 1)
         benchmark(lambda: worker.load("x"))
