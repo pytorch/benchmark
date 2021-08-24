2b183623e117fdcf7c2e66f15bf44e7ab0aca936
Taylor Robie <taylorrobie@fb.com>
2021-08-24 14:46:35 -0700

    use taskset when GOMP_CPU_AFFINITY is set

--- torchbenchmark/__init__.py
+++ torchbenchmark/__init__.py
@@ -118,6 +118,28 @@ class ModelDetails:
         return os.path.basename(self.path)
 
 
+class Worker(subprocess_worker.SubprocessWorker):
+    """Run subprocess using taskset if CPU affinity is set.
+
+    When GOMP_CPU_AFFINITY is set, importing `torch` in the main process has
+    the very surprising effect of changing the threading behavior in the
+    subprocess. (See https://github.com/pytorch/pytorch/issues/49971 for
+    details.) This is a problem, because it means that the worker is not
+    hermetic and also tends to force the subprocess torch to run in single
+    threaded mode which drastically skews results.
+
+    This can be ameliorated by calling the subprocess using `taskset`, which
+    allows the subprocess PyTorch to properly bind threads.
+    """
+
+    @property
+    def args(self) -> List[str]:
+        affinity = os.environ.get("GOMP_CPU_AFFINITY", "")
+        return (
+            ["taskset", "--cpu-list", affinity] if affinity else []
+        ) + super().args
+
+
 class ModelTask(base_task.TaskBase):
 
     # The worker may (and often does) consume significant system resources.
@@ -134,7 +156,7 @@ class ModelTask(base_task.TaskBase):
         assert self._lock.acquire(blocking=False), "Failed to acquire lock."
 
         self._model_path = model_path
-        self._worker = subprocess_worker.SubprocessWorker(timeout=timeout)
+        self._worker = Worker(timeout=timeout)
         self.worker.run("import torch")
 
         self._details: ModelDetails = ModelDetails(
