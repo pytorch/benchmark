25e4b4b844edd3ffdbe028b071ce7fb0f9a57b9d
eircfb <82548916+eircfb@users.noreply.github.com>
2021-09-20 12:35:46 -0700

    Maml update: synthesize data and parameterize

--- torchbenchmark/models/maml/__init__.py
+++ torchbenchmark/models/maml/__init__.py
@@ -19,6 +19,11 @@ class Model(BenchmarkModel):
     task = OTHER.OTHER_TASKS
     def __init__(self, device=None, jit=False):
         super().__init__()
+
+        # load from disk or synthesize data
+        use_data_file = False
+        debug_print = False
+
         self.device = device
         self.jit = jit
         root = str(Path(__file__).parent)
@@ -35,7 +40,7 @@ class Model(BenchmarkModel):
             'update_step_test': 10
         })
         config = [
-            ('conv2d', [64, 1, 3, 3, 2, 0]),
+            ('conv2d', [64, args.imgc, 3, 3, 2, 0]),
             ('relu', [True]),
             ('bn', [64]),
             ('conv2d', [64, 64, 3, 3, 2, 0]),
@@ -50,9 +55,24 @@ class Model(BenchmarkModel):
             ('flatten', []),
             ('linear', [args.n_way, 64])
         ]
+
         self.module = Meta(args, config).to(device)
-        self.example_inputs = torch.load(f'{root}/batch.pt')
-        self.example_inputs = tuple([torch.from_numpy(i).to(self.device) for i in self.example_inputs])
+
+        if use_data_file:
+            self.example_inputs = torch.load(f'{root}/batch.pt')
+            self.example_inputs = tuple([torch.from_numpy(i).to(self.device) for i in self.example_inputs])
+        else:
+            # synthesize data parameterized by arg values
+            self.example_inputs = (
+                torch.randn(args.task_num, args.n_way, args.imgc, args.imgsz, args.imgsz).to(device),
+                torch.randint(0, args.n_way, [args.task_num, args.n_way], dtype=torch.long).to(device),
+                torch.randn(args.task_num, args.n_way * args.k_qry, args.imgc, args.imgsz, args.imgsz).to(device),
+                torch.randint(0, args.n_way, [args.task_num, args.n_way * args.k_qry], dtype=torch.long).to(device))
+
+        # print input shapes
+        if debug_print:
+            for i in range(len(self.example_inputs)):
+                print(self.example_inputs[i].shape)
 
     def get_module(self):
         if self.jit:
