5ebd96cb771ed26ff5498e7986ad3cc346f8e3fa
Xu Zhao <xzhao9@fb.com>
2021-09-24 13:45:33 -0400

    Fixed sweep plotting script with TorchBench score v1.

--- plot_sweep.py
+++ plot_sweep.py
@@ -21,7 +21,9 @@ from bokeh.plotting import figure, output_file, show
 from collections import defaultdict
 from datetime import datetime as dt
 from torchbenchmark.util.data import load_data_dir, load_data_files
-from torchbenchmark.score.compute_score import TorchBenchScore, SPEC_FILE_DEFAULT
+from torchbenchmark.score.compute_score import TorchBenchScore
+
+TORCHBENCH_SCORE_VERSION = "v1"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
@@ -32,9 +34,7 @@ if __name__ == "__main__":
                              " to generate a score configuration with a target of 1000,"
                              " and everything else will be relative to that.")
     parser.add_argument("--output_html", default='plot.html', help="html file to write")
-    parser.add_argument("--score_heirarchy", default=SPEC_FILE_DEFAULT,
-                        help="file defining score heirarchy")
-    parser.add_argument("--plot_all", choices=['True', 'False'], default='False',
+    parser.add_argument("--plot_all", action='store_true',
                         help="Plots the scores for each configuration")
     parser.add_argument("--reference_json", required=True,
                         help="file defining score norm values, usually first json in first data_dir")
@@ -48,9 +48,8 @@ if __name__ == "__main__":
 
     with open(args.reference_json) as f:
         ref_data = json.load(f)
-    score_heirarchy = args.score_heirarchy
-    plot_all = True if args.plot_all == 'True' else False
-    score_config = TorchBenchScore(ref_data, score_heirarchy, 1000)
+    plot_all = args.plot_all
+    score_config = TorchBenchScore(ref_data=ref_data, version=TORCHBENCH_SCORE_VERSION)
 
     p = figure(plot_width=plot_width, plot_height=plot_height,
                x_axis_type='datetime')
@@ -59,38 +58,44 @@ if __name__ == "__main__":
     ys = []
     zs = []
     for d in compare_datasets:
-        scores = []
-        dates = []
+        scores = {}
         scores_db = defaultdict(list)
         for i in range(len(d._json_raw)):
             data = d._json_raw[i]
-            score = score_config.compute_score(data)
-            scores.append(score)
-            if plot_all:
-                score_per_config = score_config.get_score_per_config(data, True)
-                for config, score in score_per_config.items():
-                    scores_db[config].append(score)
             pytorch_ver = data['machine_info']['pytorch_version']
+            # Slice the portion after '+'
+            pytorch_ver_cuda_loc = pytorch_ver.rfind('+')
+            pytorch_ver = pytorch_ver[:pytorch_ver_cuda_loc]
             date = dt.strptime(pytorch_ver[pytorch_ver.index("dev") + len("dev"):], "%Y%m%d")
+            score = score_config.compute_score(data)
+            scores[date] = score
+
+        dates = []
+        total_scores = []
+        all_scores = []
+        for date in sorted(scores.keys()):
             dates.append(date)
+            total_scores.append(scores[date]["total"])
+            all_scores.append(scores[date])
         xs.append(dates)
-        ys.append(scores)
+        ys.append(total_scores)
         if plot_all:
-            zs.append(scores_db)
+            zs.append(all_scores)
 
     colors = itertools.cycle(Category10[10])
     basenames = map(os.path.basename, args.data_dir)
 
     if plot_all:
-        for x, y, z in zip(xs, ys, zs):
+        for x, z in zip(xs, zs):
             basename = next(basenames)
             color = next(colors)
-            p.line(x, y, color=color, line_width=2, legend_label=basename + '-total-score')
-
-            for config, scores in z.items():
-                test_config = str(config[0] + '-' + config[1] + '-' + config[2])
+            configs = z[0].keys()
+            for config in configs:
                 color = next(colors)
-                p.line(x, scores, color=color, line_width=2, legend_label=basename + '-' + test_config)
+                scores = []
+                for s in z:
+                    scores.append(s[config])
+                p.line(x, scores, color=color, line_width=2, legend_label=basename + '-' + config)
 
         p.legend.click_policy = "hide"
     else:
