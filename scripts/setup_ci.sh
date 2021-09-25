c85fe15cdbdced14f685b297c2f046eadbc19556
Xu Zhao <xzhao9@fb.com>
2021-09-24 20:18:11 -0400

    Use an older version of git lfs to avoid this issue.

--- scripts/setup_ci.sh
+++ scripts/setup_ci.sh
@@ -26,7 +26,7 @@ sudo apt-get -y install \
 
 # install git lfs and checkout the blob files
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
+sudo apt-get install git-lfs=2.13.2
 git lfs install
 git lfs fetch
 git lfs checkout .
