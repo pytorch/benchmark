7548c5ac0876b8da0190f81aa4d69b646f2536eb
Xu Zhao <xzhao9@fb.com>
2021-10-07 19:34:30 -0400

    Fix a few bugs in bisection code.

--- bisection.py
+++ bisection.py
@@ -146,11 +146,12 @@ class TorchSource:
 
     # Update pytorch, torchtext, and torchvision repo
     def update_repos(self):
-        repos = [self.srcpath]
-        repos.extend(TORCHBENCH_DEPS.values())
-        for repo in repos:
+        repos = [(self.srcpath, "master")]
+        for value in TORCHBENCH_DEPS.values():
+            repos.append((value, "main"))
+        for (repo, branch) in repos:
             gitutils.clean_git_repo(repo)
-            assert gitutils.update_git_repo(repo), f"Failed to update master branch of {repo}."
+            assert gitutils.update_git_repo(repo, branch), f"Failed to update {branch} branch of {repo}."
 
     # Get all commits between start and end, save them in self.commits
     def init_commits(self, start: str, end: str, abtest: bool) -> bool:
@@ -238,7 +239,6 @@ class TorchSource:
 
 class TorchBench:
     srcpath: str # path to pytorch/benchmark source code
-    branch: str
     timelimit: int # timeout limit in minutes
     workdir: str
     devbig: str
@@ -249,14 +249,12 @@ class TorchBench:
                  torch_src: TorchSource,
                  timelimit: int,
                  workdir: str,
-                 devbig: str,
-                 branch: str = "master"):
+                 devbig: str):
         self.srcpath = srcpath
         self.torch_src = torch_src
         self.timelimit = timelimit
         self.workdir = workdir
         self.devbig = devbig
-        self.branch = branch
         self.models = list()
 
     def prep(self) -> bool:
@@ -460,7 +458,6 @@ class TorchBenchBisection:
         json_obj["end"] = self.end
         json_obj["threshold"] = self.threshold
         json_obj["timeout"] = self.bench.timelimit
-        json_obj["torchbench_branch"] = self.bench.branch
         json_obj["result"] = []
         for res in self.result:
             r = dict()
