8fe507a65e904e9452c9ab03c8696d3a8f9b4c81
Xu Zhao <xzhao9@fb.com>
2021-11-05 12:49:52 -0400

    [WIP] adding maskrcnn model.

--- torchbenchmark/models/maskrcnn/__init__.py
+++ torchbenchmark/models/maskrcnn/__init__.py
@@ -1,9 +1,10 @@
 """
-maskrcnn model implementation, rewritten from MLPerf v1.0
+Maskrcnn model from torchvision
 """
 
 import torch
 import torch.optim as optim
+import torchvision.models.detection as models
 from ...util.model import BenchmarkModel
 from torchbenchmark.tasks import COMPUTER_VISION
 
@@ -11,13 +12,23 @@ class Model(BenchmarkModel):
     task = COMPUTER_VISION.DETECTION
 
     def __init__(self, device=None, jit=False):
-        pass
+        self.device = device
+        self.jit = jit
+        self.model = models.maskrcnn_resnet50_fpn(pretrained=True)
+        self.bs = 1
+        self.C = 2
+        self.H = 3
+        self.W = 4
+        self.example_inputs = self._gen_inputs()
+
+    def _gen_inputs(self, bs, C, H, W):
+        return torch.rand(bs, C, H, W, device=self.device)
 
     def train(self, niter=1):
         pass
 
     def eval(self, niter=1):
-        raise NotImplementedError("MLPerf Maskrcnn does not support inference.")
+        predictions = self.model(self.infer_example_inputs)
 
 if __name__ == "__main__":
     pass
