2fac99d2d9be5c21a6b68be47b8b3c3b263ac018
Xu Zhao <xzhao9@fb.com>
2021-11-05 18:54:25 -0400

    Working on maskrcnn

--- torchbenchmark/models/maskrcnn/__init__.py
+++ torchbenchmark/models/maskrcnn/__init__.py
@@ -3,32 +3,71 @@ Maskrcnn model from torchvision
 """
 
 import torch
-import torch.optim as optim
-import torchvision.models.detection as models
+import os
+import yaml
+import random
 from ...util.model import BenchmarkModel
 from torchbenchmark.tasks import COMPUTER_VISION
+
+from .prefetcher import Prefetcher
+from .maskrcnn_benchmark.config import cfg
+from maskrcnn_benchmark.data import make_data_loader
+from maskrcnn_benchmark.solver import make_lr_scheduler
+from maskrcnn_benchmark.solver import make_optimizer
+from maskrcnn_benchmark.modeling.detector import build_detection_model
+
+MASTER_SEED = 1337
+torch.manual_seed(MASTER_SEED)
+random.seed(MASTER_SEED)
+np.random.seed(MASTER_SEED)
+torch.backends.cudnn.deterministic = False
+torch.backends.cudnn.benchmark = False
 
 class Model(BenchmarkModel):
     task = COMPUTER_VISION.DETECTION
 
-    def __init__(self, device=None, jit=False):
+    def __init__(self, device=None, jit=False, train_bs=1, eval_bs=1, cfg="e2e_mask_rcnn_R_50_FPN_1x.yaml"):
         self.device = device
         self.jit = jit
-        self.model = models.maskrcnn_resnet50_fpn(pretrained=True)
-        self.bs = 1
-        self.C = 2
-        self.H = 3
-        self.W = 4
-        self.example_inputs = self._gen_inputs()
-
-    def _gen_inputs(self, bs, C, H, W):
-        return torch.rand(bs, C, H, W, device=self.device)
+        cfg_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "configs", cfg)
+        with open(cfg_path, "r") as cf:
+            self.cfg = yaml.safe_load(cf)
+        self.model = build_detection_model(self.cfg).to(self.device)
+        random_number_generator = random.Random(MASTER_SEED)
+        is_fp16 = (cfg.DTYPE == "float16")
+        if is_fp16:
+            # convert model to fp16
+            self.model.half()
+        # skip cuda graph support for now
+        self.optimizer = make_optimizer(cfg, self.model)
+        self.scheduler = make_lr_scheduler(cfg, self.optimizer)
+        # train data loader
+        self.train_data_loader, self.iters_per_epoch = make_data_loader(cfg, is_train=True,
+                                                        is_distributed=False,
+                                                        seed=seed, shapes=shapes)
+        self.per_iter_callback_fn = None
+        self.train_prefetcher = Prefetcher(self.train_data_loader, self.device)
 
     def train(self, niter=1):
-        pass
+        self.model.train()
+        self.optimizer.zero_grad()
+        for iter in niter:
+            for images, targets in self.train_prefetcher:
+                # Images: input images represented as tensors
+                # Targets: the ground truth of boxes
+                loss_dict = self.model(images, targets)
+                losses = sum(loss for loss in loss_dict.values())
+                self.optimizer.backward(losses)
+                # At this point we are waiting for kernels launched to finish
+                # Take advantage of this by loading next input batch before calling step
+                self.train_prefetcher.prefetch()
+                self.optimizer.step()  # This will sync
+                # post-processing
+                self.optimizer.zero_grad()
+                self.scheduler.step()
 
     def eval(self, niter=1):
-        predictions = self.model(self.infer_example_inputs)
+        return NotImplemented("Inference is not implemented for maskrcnn")
 
 if __name__ == "__main__":
     pass
---
+++ torchbenchmark/models/maskrcnn/maskrcnn_benchmark/engine/trainer.py
@@ -0,0 +1,184 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+import datetime
+import logging
+import os
+import time
+
+import torch
+import torch.distributed as dist
+from tqdm import tqdm
+
+from maskrcnn_benchmark.data import make_data_loader
+from maskrcnn_benchmark.utils.comm import get_world_size, synchronize
+from maskrcnn_benchmark.utils.metric_logger import MetricLogger
+from maskrcnn_benchmark.engine.inference import inference
+
+from apex import amp
+
+def reduce_loss_dict(loss_dict):
+    """
+    Reduce the loss dictionary from all processes so that process with rank
+    0 has the averaged results. Returns a dict with the same fields as
+    loss_dict, after reduction.
+    """
+    world_size = get_world_size()
+    if world_size < 2:
+        return loss_dict
+    with torch.no_grad():
+        loss_names = []
+        all_losses = []
+        for k in sorted(loss_dict.keys()):
+            loss_names.append(k)
+            all_losses.append(loss_dict[k])
+        all_losses = torch.stack(all_losses, dim=0)
+        dist.reduce(all_losses, dst=0)
+        if dist.get_rank() == 0:
+            # only main process gets accumulated, so only divide by
+            # world_size in this case
+            all_losses /= world_size
+        reduced_losses = {k: v for k, v in zip(loss_names, all_losses)}
+    return reduced_losses
+
+
+def do_train(
+    cfg,
+    model,
+    data_loader,
+    data_loader_val,
+    optimizer,
+    scheduler,
+    checkpointer,
+    device,
+    checkpoint_period,
+    test_period,
+    arguments,
+):
+    logger = logging.getLogger("maskrcnn_benchmark.trainer")
+    logger.info("Start training")
+    meters = MetricLogger(delimiter="  ")
+    max_iter = len(data_loader)
+    start_iter = arguments["iteration"]
+    model.train()
+    start_training_time = time.time()
+    end = time.time()
+
+    iou_types = ("bbox",)
+    if cfg.MODEL.MASK_ON:
+        iou_types = iou_types + ("segm",)
+    if cfg.MODEL.KEYPOINT_ON:
+        iou_types = iou_types + ("keypoints",)
+    dataset_names = cfg.DATASETS.TEST
+
+    for iteration, (images, targets, _) in enumerate(data_loader, start_iter):
+        
+        if any(len(target) < 1 for target in targets):
+            logger.error(f"Iteration={iteration + 1} || Image Ids used for training {_} || targets Length={[len(target) for target in targets]}" )
+            continue
+        data_time = time.time() - end
+        iteration = iteration + 1
+        arguments["iteration"] = iteration
+
+        images = images.to(device)
+        targets = [target.to(device) for target in targets]
+
+        loss_dict = model(images, targets)
+
+        losses = sum(loss for loss in loss_dict.values())
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = reduce_loss_dict(loss_dict)
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+        meters.update(loss=losses_reduced, **loss_dict_reduced)
+
+        optimizer.zero_grad()
+        # Note: If mixed precision is not used, this ends up doing nothing
+        # Otherwise apply loss scaling for mixed-precision recipe
+        with amp.scale_loss(losses, optimizer) as scaled_losses:
+            scaled_losses.backward()
+        optimizer.step()
+        scheduler.step()
+
+        batch_time = time.time() - end
+        end = time.time()
+        meters.update(time=batch_time, data=data_time)
+
+        eta_seconds = meters.time.global_avg * (max_iter - iteration)
+        eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+
+        if iteration % 20 == 0 or iteration == max_iter:
+            logger.info(
+                meters.delimiter.join(
+                    [
+                        "eta: {eta}",
+                        "iter: {iter}",
+                        "{meters}",
+                        "lr: {lr:.6f}",
+                        "max mem: {memory:.0f}",
+                    ]
+                ).format(
+                    eta=eta_string,
+                    iter=iteration,
+                    meters=str(meters),
+                    lr=optimizer.param_groups[0]["lr"],
+                    memory=torch.cuda.max_memory_allocated() / 1024.0 / 1024.0,
+                )
+            )
+        if iteration % checkpoint_period == 0:
+            checkpointer.save("model_{:07d}".format(iteration), **arguments)
+        if data_loader_val is not None and test_period > 0 and iteration % test_period == 0:
+            meters_val = MetricLogger(delimiter="  ")
+            synchronize()
+            _ = inference(  # The result can be used for additional logging, e. g. for TensorBoard
+                model,
+                # The method changes the segmentation mask format in a data loader,
+                # so every time a new data loader is created:
+                make_data_loader(cfg, is_train=False, is_distributed=(get_world_size() > 1), is_for_period=True),
+                dataset_name="[Validation]",
+                iou_types=iou_types,
+                box_only=False if cfg.MODEL.RETINANET_ON else cfg.MODEL.RPN_ONLY,
+                device=cfg.MODEL.DEVICE,
+                expected_results=cfg.TEST.EXPECTED_RESULTS,
+                expected_results_sigma_tol=cfg.TEST.EXPECTED_RESULTS_SIGMA_TOL,
+                output_folder=None,
+            )
+            synchronize()
+            model.train()
+            with torch.no_grad():
+                # Should be one image for each GPU:
+                for iteration_val, (images_val, targets_val, _) in enumerate(tqdm(data_loader_val)):
+                    images_val = images_val.to(device)
+                    targets_val = [target.to(device) for target in targets_val]
+                    loss_dict = model(images_val, targets_val)
+                    losses = sum(loss for loss in loss_dict.values())
+                    loss_dict_reduced = reduce_loss_dict(loss_dict)
+                    losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+                    meters_val.update(loss=losses_reduced, **loss_dict_reduced)
+            synchronize()
+            logger.info(
+                meters_val.delimiter.join(
+                    [
+                        "[Validation]: ",
+                        "eta: {eta}",
+                        "iter: {iter}",
+                        "{meters}",
+                        "lr: {lr:.6f}",
+                        "max mem: {memory:.0f}",
+                    ]
+                ).format(
+                    eta=eta_string,
+                    iter=iteration,
+                    meters=str(meters_val),
+                    lr=optimizer.param_groups[0]["lr"],
+                    memory=torch.cuda.max_memory_allocated() / 1024.0 / 1024.0,
+                )
+            )
+        if iteration == max_iter:
+            checkpointer.save("model_final", **arguments)
+
+    total_training_time = time.time() - start_training_time
+    total_time_str = str(datetime.timedelta(seconds=total_training_time))
+    logger.info(
+        "Total training time: {} ({:.4f} s / it)".format(
+            total_time_str, total_training_time / (max_iter)
+        )
+    )
