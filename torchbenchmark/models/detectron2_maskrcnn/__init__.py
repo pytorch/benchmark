0116c5d6244779ab086f1263d1fc0088829a8e64
Xu Zhao <xzhao9@fb.com>
2021-11-09 15:47:49 -0500

    Working on detectron2

--- torchbenchmark/models/detectron2_maskrcnn/__init__.py
+++ torchbenchmark/models/detectron2_maskrcnn/__init__.py
@@ -1,4 +1,5 @@
 import torch
+import os
 import itertools
 
 from detectron2.config import instantiate
@@ -10,3 +11,52 @@ from detectron2.utils.events import EventStorage
 class Model(BenchmarkModel):
     task = COMPUTER_VISION.DETECTION
 
+    def __init__(self, device=None, jit=False):
+       super().__init__()
+       self.device = device
+       self.jit = jit
+       model_cfg = model_zoo.get_config("commons/models/mask_rcnn_fpn.py").model
+       model = instantiate(model_cfg).to(self.device)
+
+       # setup environment variable
+       current_dir = os.
+       data_dir = os.path.join(current_dir, ".data", "detectron2_maskrcnn_benchmark_data")
+       os.environ['DETECTRON2_DATASETS'] = data_dir
+       data_cfg = model_zoo.get_config("commons/data/coco.py").dataloader
+
+       # use a mini dataset
+       data_cfg.train.dataset.names = "coco_2017_val_100"
+       data_cfg.train.total_batch_size = 4
+       data_cfg.test.dataset.names = "coco_2017_val_100"
+
+       train_loader = instantiate(data_cfg.train)
+       self.train_loader 
+       test_loader = instantiate(data_cfg.test)
+       self.test_iterator = itertools.cycle(itertools.islice(test_loader, 100))
+
+       self.optimizer = torch.optim.SGD(model.parameters(), 0.)
+
+    def get_module(self):
+        return self.module, (self.example_inputs, )
+
+    def train(self, niter=1):
+        self.model.train()
+        for _ in range(niter):
+            for idx, data in enumerate(self.train_iterator):
+                if idx >= 100:
+                    break
+                losses = model(data)
+                loss = sum(losses.values())
+                loss.backward()
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+
+    def eval(self, niter=1):
+        self.model.eval()
+        with torch.no_grad():
+            for _ in range(niter):
+                for idx, data in enumerate(self.test_iterator):
+                    if idx >= 100:
+                        break
+                    model(data)
+        
