ab288f9429c07ff774768b9834645409039b17f4
Xu Zhao <xzhao9@fb.com>
2021-11-10 18:45:46 -0500

    Use coco min data for maskrcnn.

--- torchbenchmark/models/maskrcnn/__init__.py
+++ torchbenchmark/models/maskrcnn/__init__.py
@@ -12,8 +12,11 @@ from torchbenchmark.tasks import COMPUTER_VISION
 
 # Model specific imports
 import torchvision
-from torchvision.models.detection import MaskRCNN
-from torchvision.models.detection.anchor_utils import AnchorGenerator
+from torch.utils.data import DataLoader
+from .transforms import Compose
+from .coco_utils import ConvertCocoPolysToMask
+from torchvision.datasets.coco import CocoDetection
+from pycocotools.coco import COCO
 
 MASTER_SEED = 1337
 torch.manual_seed(MASTER_SEED)
@@ -22,83 +25,73 @@ np.random.seed(MASTER_SEED)
 torch.backends.cudnn.deterministic = False
 torch.backends.cudnn.benchmark = False
 
-# Input tensors:
-# Tensor [N, C, H, W]
-# N: Number of pictures
-# C: Channels of color
-# H: Picture Height
-# W: Picture Weight
-# Targets:
-# Boxes: FloatTensor[N, 4]
-# Labels: Int64Tensor[N]
-# Masks: UInt8Tensor[N, H, W]
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(CURRENT_DIR, ".data", "coco2017-minimal")
+COCO_DATA_KEY = "coco_2017_val_100"
+COCO_DATA = {
+    "coco_2017_val_100": ("coco/val2017", "coco/annotations/instances_val2017_100.json")
+}
+
+def _collate_fn(batch):
+    return tuple(zip(*batch))
 
 class Model(BenchmarkModel):
     task = COMPUTER_VISION.DETECTION
 
-    def __init__(self, device=None, jit=False, train_bs=1, eval_bs=1, config="coco2017_config.yaml"):
+    def __init__(self, device=None, jit=False, train_bs=1, eval_bs=1):
         self.device = device
         self.jit = jit
-        self.model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-        # Generate inputs
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-        with open(os.path.join(current_dir, config), "r") as cc2017:
-            self.cfg = yaml.safe_load(cc2017)
-        self.example_inputs = self._gen_inputs(train_bs)
-        self.example_targets = self._gen_targets(train_bs)
-        self.infer_example_inputs = self._gen_inputs(eval_bs)
+        self.model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True).to(self.device)
 
-    def _gen_inputs(self, batch_size):
-        inputs = torch.rand(batch_size, self.cfg['C'], self.cfg['H'], self.cfg['W'], device=self.device)
-        return inputs
+        # setup optimizer
+        lr = 0.02
+        momentum = 0.9
+        weight_decay = 1e-4
+        params = [p for p in self.model.parameters() if p.requires_grad]
+        self.optimizer = torch.optim.SGD(params, lr=lr, momentum=momentum, weight_decay=weight_decay)
+        # lr_steps = 16
+        # lr_gamma = 0.1
+        # self.lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=lr_steps, gamma=lr_gamma)
 
-    # Generate three target boxes
-    def _gen_boxes(self):
-        return torch.tensor([
-            # Big box: 200x100
-            [0, 0, 200, 100],
-            # Medium box: 100x50
-            [230, 50, 330, 100],
-            # Small box: 50x25
-            [220, 120, 270, 145] ], dtype=torch.float64, device=self.device)
+        self.train_bs = train_bs
+        self.eval_bs = eval_bs
+        t = [ConvertCocoPolysToMask()]
+        transforms = Compose(t)
+        dataset = CocoDetection(root=os.path.join(DATA_DIR, COCO_DATA[COCO_DATA_KEY][0]),
+                                annFile=os.path.join(DATA_DIR, COCO_DATA[COCO_DATA_KEY][1]),
+                                transforms=transforms)
+        sampler = torch.utils.data.SequentialSampler(dataset)
 
-    def _gen_labels(self, box_cnt):
-        return torch.tensor([1]*box_cnt, dtype=torch.int64, device=self.device)
+        self.eval_data_loader = torch.utils.data.DataLoader(dataset, batch_size=eval_bs,
+                                                            sampler=sampler,
+                                                            collate_fn=_collate_fn)
+        self.train_data_loader = torch.utils.data.DataLoader(dataset, batch_size=train_bs,
+                                                            sampler=sampler,
+                                                            collate_fn=_collate_fn)
 
-    def _gen_scores(self, box_cnt):
-        return torch.rand(box_cnt, dtype=torch.float64, device=self.device)
-
-    def _gen_masks(self, box_cnt):
-        return torch.rand(box_cnt, 1, self.cfg['H'], self.cfg['W'], dtype=torch.float64, device=self.device)
-
-    def _gen_targets(self, batch_size, box_cnt=3):
-        targets = []
-        for _ in range(batch_size):
-            target = {}
-            target["boxes"] = self._gen_boxes()
-            target["labels"] = self._gen_labels(box_cnt)
-            target["scores"] = self._gen_scores(box_cnt)
-            target["masks"] = self._gen_masks(box_cnt)
-            targets.append(target)
-        return targets
 
     def get_module(self):
-        return self.model, (self.example_inputs, self.example_targets, )
-        
+        for (example_inputs, example_targets) in self.train_loader:
+            return self.model, (example_inputs, example_targets, )
+
     def train(self, niter=1):
         if self.jit:
             return NotImplementedError("JIT is not supported by this model")
         self.model.train()
-        for iter in range(niter):
-            self.model(self.example_inputs, self.example_targets)
+        for _, (images, targets) in zip(range(niter), self.train_data_loader):
+            loss_dict = self.model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
+            self.optimizer.zero_grad()
+            losses.backward()
+            self.optimizer.step()
 
     def eval(self, niter=1):
         if self.jit:
             return NotImplementedError("JIT is not supported by this model")
         self.model.eval()
         with torch.no_grad():
-            for iter in range(niter):
-                out = self.model(self.infer_example_inputs)
+            for _, (images, targets) in zip(range(niter), self.eval_data_loader):
+                self.model(images)
 
 if __name__ == "__main__":
     pass
