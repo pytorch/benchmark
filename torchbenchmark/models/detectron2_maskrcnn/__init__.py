822adaaebf0866d75db80652a62d26775f9102cf
Xu Zhao <xzhao9@fb.com>
2021-11-10 16:06:04 -0500

    Use train bs = 4 as default.

--- torchbenchmark/models/detectron2_maskrcnn/__init__.py
+++ torchbenchmark/models/detectron2_maskrcnn/__init__.py
@@ -29,7 +29,7 @@ torch.backends.cudnn.benchmark = False
 class Model(BenchmarkModel):
     task = COMPUTER_VISION.DETECTION
 
-    def __init__(self, device=None, jit=False, train_bs=1):
+    def __init__(self, device=None, jit=False, train_bs=4):
        super().__init__()
        self.device = device
        self.jit = jit
