12442efc1032be1229a22e095fb068cc02f739b0
Xu Zhao <xzhao9@fb.com>
2021-11-15 10:18:39 -0800

    Add detectron2 maskrcnn model (#556)
    Summary:
    This PR fixes https://github.com/pytorch/benchmark/issues/148
    It also creates an input data directory that contains a sample input data used by the model. And correctly sets up the environment.
    
    # Eval
    
    ## Batch size analysis
    <google-sheets-html-origin>
    
    Batch Size | GPU Time | CPU Dispatch Time | Walltime | GPU Delta
    -- | -- | -- | -- | --
    1 | 139.581 | 139.511 | 139.589 | -
    2 | 236.31 | 235.533 | 236.297 | 0.692995465
    4 | 607.77 | 606.458 | 607.751 | 1.571918243
    8 | 1194.014 | 1193.224 | 1193.985 | 0.9645819965
    16 | 2418.878 | 2416.83 | 2418.817 | 1.025837218
    
    best bs=2
    
    ## Non-idleness analysis (bs=2)
    ![image](https://user-images.githubusercontent.com/502017/141039113-5322cce9-a32c-4073-8b47-139e1055bf4a.png)
    
    # Train
    
    ## Batch size analysis
    <google-sheets-html-origin>
    
    Batch Size | GPU Time | CPU Dispatch Time | Walltime | GPU Delta
    -- | -- | -- | -- | --
    1 | 1792.45 | 642.931 | 1792.432 | -
    2 | 3714.625 | 2457.879 | 3714.549 | 1.072373009
    4 | 7254.945 | 6196.129 | 7254.758 | 0.9530760171
    8 | 15508.744 | 14079.343 | 15508.328 | 1.137679059
    
    best bs=1
    
    ## Non-idleness analysis (bs=1)
    ![image](https://user-images.githubusercontent.com/502017/141039173-42928922-458a-4886-91f9-9b3d596c32a7.png)
    
    STABLE_TEST_MODEL: detectron2_maskrcnn
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/556
    
    Reviewed By: aaronenyeshi
    
    Differential Revision: D32410509
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: d178fa2a2b69d2fbb0993b23e140e732f7cec035

--- scripts/install_nightlies.sh
+++ scripts/install_nightlies.sh
@@ -4,6 +4,9 @@ set -e
 . ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
 
+# pycocotools requires numpy 1.17 https://github.com/cocodataset/cocoapi/issues/356
+conda install -y numpy=1.17 requests=2.22 ninja pyyaml setuptools gitpython
+
 # conda install -y pytorch torchvision -c pytorch-nightly
 # Changing to pip to work around https://github.com/pytorch/pytorch/issues/49375
 
@@ -11,7 +14,6 @@ pip install --pre torch torchvision torchtext \
     --progress-bar off \
     -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html
 
-pip install -q numpy
 conda install -y expecttest -c conda-forge
 
 # Log final configuration
