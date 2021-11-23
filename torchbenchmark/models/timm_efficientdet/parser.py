a9b738f6c5db339abd66d631e69eb022c37390f2
Xu Zhao <xzhao9@fb.com>
2021-11-23 16:42:40 -0500

    Only parse known args.

--- torchbenchmark/models/timm_efficientdet/parser.py
+++ torchbenchmark/models/timm_efficientdet/parser.py
@@ -11,14 +11,14 @@ def get_args(config_file=None):
 
         # The main arg parser parses the rest of the args, the usual
         # defaults will have been overridden if config file specified.
-        args = parser.parse_args()
+        args = parser.parse_known_args()
 
         # Cache the args as a text string to save them in the output dir later
         args_text = yaml.safe_dump(args.__dict__, default_flow_style=False)
         return args, args_text
     # The first arg parser parses out only the --config argument, this argument is used to
     # load a yaml file containing key-values that override the defaults for the main parser below
-    config_parser = parser = argparse.ArgumentParser(description='Training Config', add_help=False)
+    parser = argparse.ArgumentParser(description='Training Config', add_help=False)
     parser.add_argument('-c', '--config', default='', type=str, metavar='FILE',
                         help='YAML config file specifying default arguments')
 
