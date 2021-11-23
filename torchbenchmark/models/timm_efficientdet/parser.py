960957a3bc881061bafeb521188181ce76cb56c3
Xu Zhao <xzhao9@fb.com>
2021-11-23 16:46:20 -0500

    Fixed config parsing.

--- torchbenchmark/models/timm_efficientdet/parser.py
+++ torchbenchmark/models/timm_efficientdet/parser.py
@@ -9,9 +9,10 @@ def get_args(config_file=None):
                 cfg = yaml.safe_load(f)
                 parser.set_defaults(**cfg)
 
+        # There may be remaining unrecognized options
         # The main arg parser parses the rest of the args, the usual
         # defaults will have been overridden if config file specified.
-        args = parser.parse_known_args()
+        args, _ = parser.parse_known_args()
 
         # Cache the args as a text string to save them in the output dir later
         args_text = yaml.safe_dump(args.__dict__, default_flow_style=False)
