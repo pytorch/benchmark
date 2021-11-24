77d1db397f49a7752d449fa0fbde94df316c8e9d
Xu Zhao <xzhao9@fb.com>
2021-11-23 21:30:30 -0500

    Fixed eval loader.

--- torchbenchmark/models/timm_efficientdet/parser.py
+++ torchbenchmark/models/timm_efficientdet/parser.py
@@ -141,8 +141,8 @@ def get_args(config_file=None):
                         help='how many batches to wait before logging training status')
     parser.add_argument('--recovery-interval', type=int, default=0, metavar='N',
                         help='how many batches to wait before writing recovery checkpoint')
-    parser.add_argument('-j', '--workers', type=int, default=4, metavar='N',
-                        help='how many training processes to use (default: 1)')
+    parser.add_argument('-j', '--workers', type=int, default=0, metavar='N',
+                        help='how many training processes to use (default: 0)')
     parser.add_argument('--save-images', action='store_true', default=False,
                         help='save images of input bathes every log interval for debugging')
     parser.add_argument('--amp', action='store_true', default=False,
@@ -170,18 +170,21 @@ def get_args(config_file=None):
     parser.add_argument("--local_rank", default=0, type=int)
 
     # Evaluation parameters
+    parser.add_argument('-b', '--eval-batch-size', default=128, type=int,
+                metavar='N', help='eval mini-batch size (default: 128)')
     parser.add_argument('--img-size', default=None, type=int,
                     metavar='N', help='Input image dimension, uses model default if empty')
     parser.add_argument('--checkpoint', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
     parser.add_argument('--use-ema', dest='use_ema', action='store_true',
                     help='use ema version of weights if present')
-    parser.add_argument('-b', '--eval-batch-size', default=128, type=int,
-                    metavar='N', help='eval mini-batch size (default: 128)')
     parser.add_argument('--interpolation', default='bilinear', type=str, metavar='NAME',
                     help='Image resize interpolation type (overrides model)')
     parser.add_argument('--fill-color', default=None, type=str, metavar='NAME',
                     help='Image augmentation fill (background) color ("mean" or int)')
-
+    parser.add_argument('--mean', type=float, nargs='+', default=None, metavar='MEAN',
+                    help='Override mean pixel value of dataset')
+    parser.add_argument('--std', type=float,  nargs='+', default=None,  metavar='STD',
+                    help='Override std deviation of of dataset')
     args, _ = _parse_args()
     return args
