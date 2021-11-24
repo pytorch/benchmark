e1900f0b1dc8b1cfc0346bbd04231bd41a1d3ff3
Xu Zhao <xzhao9@fb.com>
2021-11-23 21:00:55 -0500

    Addec train and eval function.

--- torchbenchmark/models/timm_efficientdet/__init__.py
+++ torchbenchmark/models/timm_efficientdet/__init__.py
@@ -25,7 +25,7 @@ from .parser import get_args
 from .train import train_epoch, validate
 from .loader import create_datasets_and_loaders
 
-# setup environment variable
+# setup coco2017 input path
 CURRENT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 DATA_DIR = os.path.join(CURRENT_DIR.parent.parent, "data", ".data", "coco2017-minimal", "coco")
 
@@ -45,7 +45,7 @@ class Model(BenchmarkModel):
         # generate arguments
         args = get_args()
         # Use native amp if possible
-        args.native_amp = True and check_native_amp()
+        args.native_amp = check_native_amp()
         # Disable distributed
         args.distributed = False
         args.device = device
@@ -84,21 +84,24 @@ class Model(BenchmarkModel):
         self.amp_autocast = suppress
         if args.native_amp:
             self.amp_autocast = torch.cuda.amp.autocast
-            loss_scaler = NativeScaler()
+            self.loss_scaler = NativeScaler()
         self.model_ema = None
         if args.model_ema:
             # Important to create EMA model after cuda(), DP wrapper, and AMP but before SyncBN and DDP wrapper
             self.model_ema = ModelEmaV2(model, decay=args.model_ema_decay)
         self.lr_scheduler, self.num_epochs = create_scheduler(args, self.optimizer)
 
-        loader_train, loader_eval, evaluator = create_datasets_and_loaders(args, model_config)
-        if model_config.num_classes < loader_train.dataset.parser.max_label:
+        self.loader_train, self.loader_eval, self.evaluator = create_datasets_and_loaders(args, model_config)
+        if model_config.num_classes < self.loader_train.dataset.parser.max_label:
             logging.error(
-                f'Model {model_config.num_classes} has fewer classes than dataset {loader_train.dataset.parser.max_label}.')
+                f'Model {model_config.num_classes} has fewer classes than dataset {self.loader_train.dataset.parser.max_label}.')
             exit(1)
-        if model_config.num_classes > loader_train.dataset.parser.max_label:
+        if model_config.num_classes > self.loader_train.dataset.parser.max_label:
             logging.warning(
-                f'Model {model_config.num_classes} has more classes than dataset {loader_train.dataset.parser.max_label}.')
+                f'Model {model_config.num_classes} has more classes than dataset {self.loader_train.dataset.parser.max_label}.')
+        self.train_num_batch = 1
+        self.eval_num_batch = 1
+        self.args = args
 
     def get_module(self):
         self.model.eval()
@@ -108,25 +111,32 @@ class Model(BenchmarkModel):
             raise NotImplementedError("Only CUDA is supported by this model")
         if self.jit:
             raise NotImplementedError("JIT is not supported by this model")
+        eval_metric = self.args.eval_metric
         for epoch in range(niter):
             train_metrics = train_epoch(
                 epoch, self.model, self.loader_train,
                 self.optimizer, self.args,
                 lr_scheduler=self.lr_scheduler, amp_autocast=self.amp_autocast,
-                loss_scaler=self.loss_scaler, model_ema=self.model_ema
+                loss_scaler=self.loss_scaler, model_ema=self.model_ema,
+                num_batch=self.train_num_batch,
             )
             # the overhead of evaluating with coco style datasets is fairly high, so just ema or non, not both
             if self.model_ema is not None:
-                eval_metrics = validate(self.model_ema.module, self.loader_eval, args, evaluator, log_suffix=' (EMA)')
+                eval_metrics = validate(self.model_ema.module, self.loader_eval, self.args, self.evaluator, log_suffix=' (EMA)')
             else:
-                eval_metrics = validate(self.model, self.loader_eval, self.args, evaluator)
+                eval_metrics = validate(self.model, self.loader_eval, self.args, self.evaluator)
             if self.lr_scheduler is not None:
                 # step LR for next epoch
                 self.lr_scheduler.step(epoch + 1, eval_metrics[eval_metric])
 
-    def eval(self, niter=2):
+    def eval(self, niter=1):
         if not self.device == "cuda":
             raise NotImplementedError("Only CUDA is supported by this model")
         if self.jit:
             raise NotImplementedError("JIT is not supported by this model")
-
+        for _ in range(niter):
+            with torch.no_grad():
+                for _, (input, target) in zip(self.eval_num_batch, self.eval_loader):
+                    with self.amp_autocast():
+                        output = self.eval_model(input, img_info=target)
+                    self.evaluator.add_predictions(output, target)
