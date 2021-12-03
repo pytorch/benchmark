48f8cb27846c91ce09c7834fdc66603e965a334f
Aaron Enye Shi <enye.shi@gmail.com>
2021-12-03 22:14:49 +0000

    Fix the train hparams of Tacotron2

--- torchbenchmark/models/tacotron2/__init__.py
+++ torchbenchmark/models/tacotron2/__init__.py
@@ -1,4 +1,4 @@
-from .train_tacotron2 import train, load_model, prepare_dataloaders
+from .train_tacotron2 import load_model, prepare_dataloaders
 import torch
 from .loss_function import Tacotron2Loss
 from argparse import Namespace
@@ -10,6 +10,7 @@ from torchbenchmark.tasks import SPEECH
 
 class Model(BenchmarkModel):
     task = SPEECH.SYNTHESIS
+
     def __init__(self, device=None, jit=False):
         super().__init__()
         """ Required """
@@ -22,12 +23,13 @@ class Model(BenchmarkModel):
         self.hparams = self.create_hparams()
         self.model = load_model(self.hparams).to(device=device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams.learning_rate,
-                                 weight_decay=self.hparams.weight_decay)
+                                          weight_decay=self.hparams.weight_decay)
         self.criterion = Tacotron2Loss().to(device=device)
         train_loader, valset, collate_fn = prepare_dataloaders(self.hparams)
         self.example_input, self.target = self.model.parse_batch(list(train_loader)[0], device=self.device)
 
-
+    # Parameters were obtained from the source code.
+    # Source: https://github.com/NVIDIA/tacotron2/blob/bb6761349354ee914909a42208e4820929612069/hparams.py#L5
     def create_hparams(hparams_string=None, verbose=False):
         """Create model hyperparameters. Parse nondefault from given string."""
         root = str(Path(__file__).parent)
@@ -35,83 +37,82 @@ class Model(BenchmarkModel):
             ################################
             # Experiment Parameters        #
             ################################
-            'epochs':2,
-            'iters_per_checkpoint':1000,
-            'seed':1234,
-            'dynamic_loss_scaling':True,
-            'fp16_run':False,
-            'distributed_run':False,
-            'dist_backend':"nccl",
-            'dist_url':"tcp://localhost:54321",
-            'cudnn_enabled':True,
-            'cudnn_benchmark':False,
-            'ignore_layers':['embedding.weight'],
+            'epochs': 2,  # Reduced in TorchBench to shorten number of train iterations.
+            'iters_per_checkpoint': 1000,
+            'seed': 1234,
+            'dynamic_loss_scaling': True,
+            'fp16_run': False,
+            'distributed_run': False,
+            'dist_backend': "nccl",
+            'dist_url': "tcp://localhost:54321",
+            'cudnn_enabled': True,
+            'cudnn_benchmark': False,
+            'ignore_layers': ['embedding.weight'],
 
             ################################
             # Data Parameters             #
             ################################
-            'load_mel_from_disk':False,
-            'training_files':f'{root}/filelists/ljs_audio_text_train_filelist.txt',
-            'validation_files':f'{root}/filelists/ljs_audio_text_val_filelist.txt',
-            'text_cleaners':['english_cleaners'],
+            'load_mel_from_disk': False,
+            'training_files': f'{root}/filelists/ljs_audio_text_train_filelist.txt',
+            'validation_files': f'{root}/filelists/ljs_audio_text_val_filelist.txt',
+            'text_cleaners': ['english_cleaners'],
 
             ################################
             # Audio Parameters             #
             ################################
-            'max_wav_value':32768.0,
-            'sampling_rate':22050,
-            'filter_length':1024,
-            'hop_length':256,
-            'win_length':1024,
-            'n_mel_channels':80,
-            'mel_fmin':0.0,
-            'mel_fmax':8000.0,
+            'max_wav_value': 32768.0,
+            'sampling_rate': 22050,
+            'filter_length': 1024,
+            'hop_length': 256,
+            'win_length': 1024,
+            'n_mel_channels': 80,
+            'mel_fmin': 0.0,
+            'mel_fmax': 8000.0,
 
             ################################
             # Model Parameters             #
             ################################
-            'n_symbols':len(symbols),
-            'symbols_embedding_dim':512,
+            'n_symbols': len(symbols),
+            'symbols_embedding_dim': 512,
 
             # Encoder parameters
-            'encoder_kernel_size':5,
-            'encoder_n_convolutions':3,
-            'encoder_embedding_dim':512,
+            'encoder_kernel_size': 5,
+            'encoder_n_convolutions': 3,
+            'encoder_embedding_dim': 512,
 
             # Decoder parameters
-            'n_frames_per_step':1,  # currently only 1 is supported
-            'decoder_rnn_dim':1024,
-            'prenet_dim':256,
-            'max_decoder_steps':1000,
-            'gate_threshold':0.5,
-            'p_attention_dropout':0.1,
-            'p_decoder_dropout':0.1,
+            'n_frames_per_step': 1,  # currently only 1 is supported
+            'decoder_rnn_dim': 1024,
+            'prenet_dim': 256,
+            'max_decoder_steps': 1000,
+            'gate_threshold': 0.5,
+            'p_attention_dropout': 0.1,
+            'p_decoder_dropout': 0.1,
 
             # Attention parameters
-            'attention_rnn_dim':1024,
-            'attention_dim':128,
+            'attention_rnn_dim': 1024,
+            'attention_dim': 128,
 
             # Location Layer parameters
-            'attention_location_n_filters':32,
-            'attention_location_kernel_size':31,
+            'attention_location_n_filters': 32,
+            'attention_location_kernel_size': 31,
 
             # Mel-post processing network parameters
-            'postnet_embedding_dim':512,
-            'postnet_kernel_size':5,
-            'postnet_n_convolutions':5,
+            'postnet_embedding_dim': 512,
+            'postnet_kernel_size': 5,
+            'postnet_n_convolutions': 5,
 
             ################################
             # Optimization Hyperparameters #
             ################################
-            'use_saved_learning_rate':False,
-            'learning_rate':1e-3,
-            'weight_decay':1e-6,
-            'grad_clip_thresh':1.0,
-            'batch_size':1,
-            'mask_padding':True  # set model's padded outputs to padded values
+            'use_saved_learning_rate': False,
+            'learning_rate': 1e-3,
+            'weight_decay': 1e-6,
+            'grad_clip_thresh': 1.0,
+            'batch_size': 64,
+            'mask_padding': True  # set model's padded outputs to padded values
         })
         return hparams
-
 
     def get_module(self):
         if self.device == 'cpu':
@@ -133,7 +134,6 @@ class Model(BenchmarkModel):
             loss = self.criterion(y_pred, self.target)
             loss.backward()
             self.optimizer.step()
-
 
     def eval(self, niterations=1):
         if self.device == 'cpu':
