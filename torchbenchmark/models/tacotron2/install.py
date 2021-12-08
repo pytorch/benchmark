9703e96f7a8ed721fa2e95db4e21fc41fb5b902f
Aaron Enye Shi <enye.shi@gmail.com>
2021-12-08 22:42:07 +0000

    Compress tacotron2 data and move to data dir

--- torchbenchmark/models/tacotron2/install.py
+++ torchbenchmark/models/tacotron2/install.py
@@ -1,5 +1,13 @@
+import os
+from pathlib import Path
 import subprocess
 import sys
+
+
+def check_data_dir():
+    current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+    tacotron2_data_dir = os.path.join(current_dir.parent.parent, "data", ".data", "tacotron2-minimal")
+    assert os.path.exists(tacotron2_data_dir), "Couldn't find tacotron2 minimal data dir, please run install.py again."
 
 
 def pip_install_requirements():
@@ -7,4 +15,3 @@ def pip_install_requirements():
 
 if __name__ == '__main__':
     pip_install_requirements()
-
