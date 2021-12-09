9ade72658eb6cd7091ce6c25a86af0ea8421b0d3
Aaron Enye Shi <enye.shi@gmail.com>
2021-12-09 14:21:05 -0800

    Fix training hparams in Tacotron2 (#610)
    Summary:
    To match the correct batch size of 64 mentioned in the source code and paper.
    Source: https://arxiv.org/pdf/1712.05884.pdf
    Code: https://github.com/NVIDIA/tacotron2/blob/bb6761349354ee914909a42208e4820929612069/hparams.py#L84
    
    Also, due to the larger batch size, I needed to add more .wav files (186), since the old dataloader only had 2 .wav files.
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/610
    
    Reviewed By: xuzhao9
    
    Differential Revision: D32964566
    
    Pulled By: aaronenyeshi
    
    fbshipit-source-id: b5d06553d9a70d38fe8ea2197858fe7933109f04

--- torchbenchmark/models/tacotron2/__init__.py
+++ torchbenchmark/models/tacotron2/__init__.py
@@ -1,4 +1,4 @@
-from .train_tacotron2 import train, load_model, prepare_dataloaders
+from .train_tacotron2 import load_model, prepare_dataloaders
 import torch
 from .loss_function import Tacotron2Loss
 from argparse import Namespace
@@ -10,7 +10,10 @@ from torchbenchmark.tasks import SPEECH
 
 class Model(BenchmarkModel):
     task = SPEECH.SYNTHESIS
-    def __init__(self, device=None, jit=False):
+
+    # Training batch size comes from the source code:
+    # Source: https://github.com/NVIDIA/tacotron2/blob/bb6761349354ee914909a42208e4820929612069/hparams.py#L84
+    def __init__(self, device=None, jit=False, train_bs=64, eval_bs=64):
         super().__init__()
         """ Required """
         self.device = device
@@ -19,130 +22,141 @@ class Model(BenchmarkModel):
             # TODO - currently load_model assumes cuda
             return
 
-        self.hparams = self.create_hparams()
-        self.model = load_model(self.hparams).to(device=device)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams.learning_rate,
-                                 weight_decay=self.hparams.weight_decay)
+        self.train_hparams = self.create_hparams(batch_size=train_bs)
+        self.eval_hparams = self.create_hparams(batch_size=eval_bs)
+        self.train_model = load_model(self.train_hparams).to(device=device)
+        self.eval_model = load_model(self.eval_hparams).to(device=device)
+        self.train_optimizer = torch.optim.Adam(self.train_model.parameters(),
+                                                lr=self.train_hparams.learning_rate,
+                                                weight_decay=self.train_hparams.weight_decay)
+        self.eval_optimizer = torch.optim.Adam(self.eval_model.parameters(),
+                                               lr=self.eval_hparams.learning_rate,
+                                               weight_decay=self.eval_hparams.weight_decay)
         self.criterion = Tacotron2Loss().to(device=device)
-        train_loader, valset, collate_fn = prepare_dataloaders(self.hparams)
-        self.example_input, self.target = self.model.parse_batch(list(train_loader)[0], device=self.device)
+        train_loader, valset, collate_fn = prepare_dataloaders(self.train_hparams)
+        self.example_input, self.target = self.train_model.parse_batch(next(iter(train_loader)), device=self.device)
 
-
-    def create_hparams(hparams_string=None, verbose=False):
+    # Parameters were obtained from the source code.
+    # Source: https://github.com/NVIDIA/tacotron2/blob/bb6761349354ee914909a42208e4820929612069/hparams.py#L5
+    def create_hparams(hparams_string=None, verbose=False, batch_size=64):
         """Create model hyperparameters. Parse nondefault from given string."""
-        root = str(Path(__file__).parent)
+        root = str(Path(__file__).parent.parent.parent)
         hparams = Namespace(**{
             ################################
             # Experiment Parameters        #
             ################################
-            'epochs':2,
-            'iters_per_checkpoint':1000,
-            'seed':1234,
-            'dynamic_loss_scaling':True,
-            'fp16_run':False,
-            'distributed_run':False,
-            'dist_backend':"nccl",
-            'dist_url':"tcp://localhost:54321",
-            'cudnn_enabled':True,
-            'cudnn_benchmark':False,
-            'ignore_layers':['embedding.weight'],
+            'epochs': 2,  # Reduced in TorchBench to shorten number of train iterations.
+            'iters_per_checkpoint': 1000,
+            'seed': 1234,
+            'dynamic_loss_scaling': True,
+            'fp16_run': False,
+            'distributed_run': False,
+            'dist_backend': "nccl",
+            'dist_url': "tcp://localhost:54321",
+            'cudnn_enabled': True,
+            'cudnn_benchmark': False,
+            'ignore_layers': ['embedding.weight'],
 
             ################################
             # Data Parameters             #
             ################################
-            'load_mel_from_disk':False,
-            'training_files':f'{root}/filelists/ljs_audio_text_train_filelist.txt',
-            'validation_files':f'{root}/filelists/ljs_audio_text_val_filelist.txt',
-            'text_cleaners':['english_cleaners'],
+            'load_mel_from_disk': False,
+            'training_files': f'{root}/data/.data/tacotron2-minimal/filelists/ljs_audio_text_train_filelist.txt',
+            'validation_files': f'{root}/data/.data/tacotron2-minimal/filelists/ljs_audio_text_val_filelist.txt',
+            'text_cleaners': ['english_cleaners'],
 
             ################################
             # Audio Parameters             #
             ################################
-            'max_wav_value':32768.0,
-            'sampling_rate':22050,
-            'filter_length':1024,
-            'hop_length':256,
-            'win_length':1024,
-            'n_mel_channels':80,
-            'mel_fmin':0.0,
-            'mel_fmax':8000.0,
+            'max_wav_value': 32768.0,
+            'sampling_rate': 22050,
+            'filter_length': 1024,
+            'hop_length': 256,
+            'win_length': 1024,
+            'n_mel_channels': 80,
+            'mel_fmin': 0.0,
+            'mel_fmax': 8000.0,
 
             ################################
             # Model Parameters             #
             ################################
-            'n_symbols':len(symbols),
-            'symbols_embedding_dim':512,
+            'n_symbols': len(symbols),
+            'symbols_embedding_dim': 512,
 
             # Encoder parameters
-            'encoder_kernel_size':5,
-            'encoder_n_convolutions':3,
-            'encoder_embedding_dim':512,
+            'encoder_kernel_size': 5,
+            'encoder_n_convolutions': 3,
+            'encoder_embedding_dim': 512,
 
             # Decoder parameters
-            'n_frames_per_step':1,  # currently only 1 is supported
-            'decoder_rnn_dim':1024,
-            'prenet_dim':256,
-            'max_decoder_steps':1000,
-            'gate_threshold':0.5,
-            'p_attention_dropout':0.1,
-            'p_decoder_dropout':0.1,
+            'n_frames_per_step': 1,  # currently only 1 is supported
+            'decoder_rnn_dim': 1024,
+            'prenet_dim': 256,
+            'max_decoder_steps': 1000,
+            'gate_threshold': 0.5,
+            'p_attention_dropout': 0.1,
+            'p_decoder_dropout': 0.1,
 
             # Attention parameters
-            'attention_rnn_dim':1024,
-            'attention_dim':128,
+            'attention_rnn_dim': 1024,
+            'attention_dim': 128,
 
             # Location Layer parameters
-            'attention_location_n_filters':32,
-            'attention_location_kernel_size':31,
+            'attention_location_n_filters': 32,
+            'attention_location_kernel_size': 31,
 
             # Mel-post processing network parameters
-            'postnet_embedding_dim':512,
-            'postnet_kernel_size':5,
-            'postnet_n_convolutions':5,
+            'postnet_embedding_dim': 512,
+            'postnet_kernel_size': 5,
+            'postnet_n_convolutions': 5,
 
             ################################
             # Optimization Hyperparameters #
             ################################
-            'use_saved_learning_rate':False,
-            'learning_rate':1e-3,
-            'weight_decay':1e-6,
-            'grad_clip_thresh':1.0,
-            'batch_size':1,
-            'mask_padding':True  # set model's padded outputs to padded values
+            'use_saved_learning_rate': False,
+            'learning_rate': 1e-3,
+            'weight_decay': 1e-6,
+            'grad_clip_thresh': 1.0,
+            'batch_size': batch_size,
+            'mask_padding': True  # set model's padded outputs to padded values
         })
         return hparams
 
-
     def get_module(self):
+        if self.device == 'cuda':
+            raise NotImplementedError('CUDA disabled due to CUDA out of memory on CI GPU')
         if self.device == 'cpu':
             raise NotImplementedError('CPU not supported')
         if self.jit:
             raise NotImplementedError('JIT not supported')
-        return self.model, (self.example_input,)
+        return self.train_model, (self.example_input,)
 
     def train(self, niterations=1):
+        if self.device == 'cuda':
+            raise NotImplementedError('CUDA disabled due to CUDA out of memory on CI GPU')
         if self.device == 'cpu':
             raise NotImplementedError("Disabled due to excessively slow runtime - see GH Issue #100")
         if self.jit:
             raise NotImplementedError('JIT not supported')
-        self.model.train()
+        self.train_model.train()
         for _ in range(niterations):
-            self.model.zero_grad()
-            y_pred = self.model(self.example_input)
+            self.train_model.zero_grad()
+            y_pred = self.train_model(self.example_input)
 
             loss = self.criterion(y_pred, self.target)
             loss.backward()
-            self.optimizer.step()
-
+            self.train_optimizer.step()
 
     def eval(self, niterations=1):
+        if self.device == 'cuda':
+            raise NotImplementedError('CUDA disabled due to CUDA out of memory on CI GPU')
         if self.device == 'cpu':
             raise NotImplementedError('CPU not supported')
         if self.jit:
             raise NotImplementedError('JIT not supported')
-        self.model.eval()
+        self.eval_model.eval()
         for _ in range(niterations):
-            self.model(self.example_input)
+            self.eval_model(self.example_input)
 
 
 if __name__ == '__main__':
