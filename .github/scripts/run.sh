5168d888610074d3376fe3dc8b32ad7adc050623
Jiewen Tan <jwtan@fb.com>
2021-12-10 09:17:56 +0000

    [wconstab/ltc] Enable lazy_bench.py for the TB CI

--- .github/scripts/run.sh
+++ .github/scripts/run.sh
@@ -51,10 +51,10 @@ echo "Running check_lazy.py"
 python check_lazy.py --output_file ${DATA_DIR}/sweep.out
 python check_lazy.py --json_to_csv ${DATA_DIR}/sweep.out --output_file ${DATA_DIR}/sweep.csv
 
-# echo "Running lazy_bench.py"
-# pushd ../pytorch/lazy_tensor_core/
-# LTC_TS_CUDA=1 python lazy_bench.py -d cuda --fuser fuser2  --test train -x div -x hard -k resnet18 --repeat 3
-# LTC_TS_CUDA=1 python lazy_bench.py -d cuda --fuser fuser2  --test eval -k resnet18 --repeat 3
-# popd
+echo "Running lazy_bench.py"
+pushd ../pytorch/lazy_tensor_core/
+LTC_TS_CUDA=1 python lazy_bench.py -d cuda --fuser fuser2  --test train -x div -x hard -k resnet18 --repeat 3
+LTC_TS_CUDA=1 python lazy_bench.py -d cuda --fuser fuser2  --test eval -k resnet18 --repeat 3
+popd
 
 echo "Benchmark finished successfully. Output data dir is ${DATA_DIR}."
