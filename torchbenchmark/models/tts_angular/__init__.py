f41a4a0888705460d0cf730279c5ef64bebca16a
Xu Zhao <xzhao9@fb.com>
2021-12-15 15:34:48 -0800

    Improve the quality of tts_angular, fix train and eval batch sizes (#634)
    Summary:
    Train (bs=64):
    ![image](https://user-images.githubusercontent.com/502017/145630415-4c03dfef-cddf-48b2-a9ef-de78755252ec.png)
    
    Train bs=64 is default for the model, source: https://github.com/mozilla/TTS/blob/master/TTS/speaker_encoder/config.json#L38
    
    Eval (bs=256):
    ![image](https://user-images.githubusercontent.com/502017/145630266-6e01b278-2296-429a-9a5d-4ada5c2300e0.png)
    
    256 is the largest BS supported by T4.
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/634
    
    Reviewed By: aaronenyeshi
    
    Differential Revision: D33026223
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 623a556792bf40166f6550a3bdd30c1b5c63cb49

--- torchbenchmark/models/tts_angular/__init__.py
+++ torchbenchmark/models/tts_angular/__init__.py
@@ -2,19 +2,21 @@
 from ...util.model import BenchmarkModel
 from torchbenchmark.tasks import SPEECH
 
-from .angular_tts_main import TTSModel, SYNTHETIC_DATA
+from .angular_tts_main import TTSModel, EVAL_SYNTHETIC_DATA
 
 class Model(BenchmarkModel):
     task = SPEECH.SYNTHESIS
-    def __init__(self, device=None, jit=False):
+    # Original train batch size: 64
+    # Source: https://github.com/mozilla/TTS/blob/master/TTS/speaker_encoder/config.json#L38
+    def __init__(self, device=None, jit=False, train_bs=64, eval_bs=256):
         super().__init__()
         self.device = device
         self.jit = jit
-        self.model = TTSModel(device=self.device)
+        self.model = TTSModel(device=self.device, train_bs=train_bs, eval_bs=eval_bs)
         self.model.model.to(self.device)
 
     def get_module(self):
-        return self.model.model, [SYNTHETIC_DATA[0], ]
+        return self.model.model, [EVAL_SYNTHETIC_DATA[0], ]
 
     def set_train(self):
         # another model instance is used for training
@@ -32,11 +34,3 @@ class Model(BenchmarkModel):
             raise NotImplementedError()
         for _ in range(niter):
             self.model.eval()
-
-
-if __name__ == '__main__':
-    m = Model(device='cpu', jit=False)
-    model, example_inputs = m.get_module()
-    model(*example_inputs)
-    m.train()
-    m.eval()
