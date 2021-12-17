d39c264bef349acfa0e4a66d06a1af4e4f558b19
David Jurado <david.jurado@factored.ai>
2021-12-17 16:17:49 -0500

    Update Dockerfile

--- mlcube/run_test_bench.sh
+++ mlcube/run_test_bench.sh
@@ -11,5 +11,8 @@ else
     file_name=test_bench_"$MODE"_"$(date +%Y%m%d_%H%M%S)".txt
     echo "* Running: pytest ./test_bench.py" $COMMAND_ARGS 2>&1 |& tee -a $OUTPUT_DIR/$PLATFORM/$file_name
     pytest ./test_bench.py $COMMAND_ARGS 2>&1 |& tee -a $OUTPUT_DIR/$PLATFORM/$file_name
-    cp -Rf ./.benchmarks/* $OUTPUT_DIR/$PLATFORM
+    if [[ $COMMAND_ARGS != *"-k"* ]]
+    then
+        cp -Rf ./.benchmarks/* $OUTPUT_DIR/$PLATFORM
+    fi
 fi
