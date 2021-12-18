ea08835cf13b150620abae5d611f37556672725b
Xu Zhao <xzhao9@fb.com>
2021-12-17 20:59:19 -0500

    Add fp16 and fx2trt support to all torchvision models (#663)

--- torchbenchmark/models/mobilenet_v2/__init__.py
+++ torchbenchmark/models/mobilenet_v2/__init__.py
@@ -6,25 +6,33 @@ import torchvision.models as models
 from ...util.model import BenchmarkModel
 from torchbenchmark.tasks import COMPUTER_VISION
 
-#######################################################
-#
-#       DO NOT MODIFY THESE FILES DIRECTLY!!!
-#       USE `gen_torchvision_benchmarks.py`
-#
-#######################################################
+from torchbenchmark.util.env_check import parse_extraargs
+
 class Model(BenchmarkModel):
     task = COMPUTER_VISION.CLASSIFICATION
 
     # Train batch size: 96
     # Source: https://arxiv.org/pdf/1801.04381.pdf
-    def __init__(self, device=None, jit=False, train_bs=96, eval_bs=16):
+    def __init__(self, device=None, jit=False, train_bs=96, eval_bs=16, extra_args=[]):
         super().__init__()
         self.device = device
         self.jit = jit
         self.model = models.mobilenet_v2().to(self.device)
         self.eval_model = models.mobilenet_v2().to(self.device)
         self.example_inputs = (torch.randn((train_bs, 3, 224, 224)).to(self.device),)
-        self.infer_example_inputs = (torch.randn((eval_bs, 3, 224, 224)).to(self.device),)
+        self.eval_example_inputs = (torch.randn((eval_bs, 3, 224, 224)).to(self.device),)
+
+        # process extra args
+        self.extra_args = parse_extraargs(extra_args)
+        if self.extra_args.eval_fp16:
+            self.eval_model.half()
+            self.eval_example_inputs = (self.eval_example_inputs[0].half(),)
+        if self.extra_args.fx2trt:
+            assert self.device == 'cuda', "fx2trt is only available with CUDA."
+            assert not self.jit, "fx2trt with JIT is not available."
+            from torchbenchmark.util.fx2trt import lower_to_trt
+            self.eval_model = lower_to_trt(module=self.eval_model, input=self.eval_example_inputs, \
+                                           max_batch_size=eval_bs, fp16_mode=self.extra_args.eval_fp16)
 
         if self.jit:
             if hasattr(torch.jit, '_script_pdt'):
@@ -60,7 +68,7 @@ class Model(BenchmarkModel):
 
     def eval(self, niter=1):
         model = self.eval_model
-        example_inputs = self.infer_example_inputs
+        example_inputs = self.eval_example_inputs
         for i in range(niter):
             model(*example_inputs)
 
