3bf0f3972f32984f91a1bc8cbeb83abd85ab57d9
Xu Zhao <xzhao9@fb.com>
2021-12-22 16:46:29 -0500

    Removed unused property.

--- torchbenchmark/models/resnet50/__init__.py
+++ torchbenchmark/models/resnet50/__init__.py
@@ -3,7 +3,6 @@ from torchbenchmark.tasks import COMPUTER_VISION
 
 class Model(TorchVisionModel):
     task = COMPUTER_VISION.CLASSIFICATION
-    optimized_for_inference = True
 
     def __init__(self, device=None, jit=False, train_bs=32, eval_bs=32, extra_args=[]):
         super().__init__(model_name="resnet50", device=device, jit=jit,
