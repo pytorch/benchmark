cf987731eaf9203a83efcb0a97499a047b4f2e43
Elijah Rippeth <elijah.rippeth@gmail.com>
2021-12-25 07:33:04 -0500

    initial torchtext refactor.

--- torchbenchmark/models/pytorch_struct/__init__.py
+++ torchbenchmark/models/pytorch_struct/__init__.py
@@ -2,18 +2,18 @@
 pytorch_struct model, Unsupervised CFG task
 https://github.com/harvardnlp/pytorch-struct/blob/master/notebooks/Unsupervised_CFG.ipynb
 """
-import os
 import pytest
-import torchtext
 import numpy as np
 import torch, random
-import torch_struct
 from torch_struct import SentCFG
 from .networks.NeuralCFG import NeuralCFG
 
-from torchbenchmark.util.torchtext_legacy.field import Field
-from torchbenchmark.util.torchtext_legacy.datasets import UDPOS
-from torchbenchmark.util.torchtext_legacy.iterator import BucketIterator
+from torchtext.datasets import UDPOS
+from torchtext.vocab import build_vocab_from_iterator
+from torchtext.data.utils import (
+    get_tokenizer,
+    ngrams_iterator,
+)
 
 from ...util.model import BenchmarkModel
 from torchbenchmark.tasks import OTHER
@@ -24,33 +24,11 @@ np.random.seed(1337)
 torch.backends.cudnn.deterministic = False
 torch.backends.cudnn.benchmark = False
 
-def _prefetch(loader, device, limit=10):
-    data = []
-    for _, ex in zip(range(limit), loader):
-        words, lengths = ex.word
-        words = words.long()
-        words = words.to(device).transpose(0, 1)
-        data.append((words, lengths))
-    return data
 
-def TokenBucket(
-    train, batch_size, device, key=lambda x: max(len(x.word[0]), 5)
-):
-    def batch_size_fn(x, _, size):
-        return size + key(x)
+def yield_tokens(data_iter, ngrams, tokenizer):
+    for _, text in data_iter:
+        yield ngrams_iterator(tokenizer(text), ngrams)
 
-    return BucketIterator(
-        train,
-        train=True,
-        sort=False,
-        sort_within_batch=True,
-        shuffle=True,
-        batch_size=batch_size,
-        sort_key=lambda x: key(x),
-        repeat=True,
-        batch_size_fn=batch_size_fn,
-        device=device,
-    )
 
 class Model(BenchmarkModel):
   task = OTHER.OTHER_TASKS
@@ -62,28 +40,21 @@ class Model(BenchmarkModel):
     self.device = device
     self.jit = jit
 
-    WORD = Field(include_lengths=True)
-    UD_TAG = Field(init_token="<bos>", eos_token="<eos>", include_lengths=True)
+    tokenizer = get_tokenizer('basic_english')
 
-    train, val, test = UDPOS.splits(
-      fields=(('word', WORD), ('udtag', UD_TAG), (None, None)), 
-      filter_pred=lambda ex: 5 < len(ex.word) < 30
-    )
+    train_iter = UDPOS(split='train')
+    vocab = build_vocab_from_iterator(yield_tokens(train_iter, ngrams=5, tokenizer=tokenizer), specials=["<unk>"])
+    vocab.set_default_index(vocab["<unk>"])
 
-    WORD.build_vocab(train.word, min_freq=3)
-    UD_TAG.build_vocab(train.udtag)
-    self.train_iter = TokenBucket(train, 
-                                  batch_size=train_bs,
-                                  device=self.device)
+    self.train_iter = UDPOS(split='train')
 
     # Build model
     H = 256
     T = 30
     NT = 30
-    self.model = NeuralCFG(len(WORD.vocab), T, NT, H)
+    self.model = NeuralCFG(len(vocab), T, NT, H)
     self.model.to(device=device)
     self.opt = torch.optim.Adam(self.model.parameters(), lr=0.001, betas=[0.75, 0.999])
-    self.train_data = _prefetch(self.train_iter, self.device)
 
   def get_module(self):
     for words, _ in self.train_data:
@@ -92,7 +63,7 @@ class Model(BenchmarkModel):
   def train(self, niter=1):
     if self.jit:
         raise NotImplementedError("JIT is not supported by this model")
-    for _, (words, lengths) in zip(range(niter), self.train_data):
+    for _, (words, lengths) in zip(range(niter), self.train_iter):
       losses = []
       self.opt.zero_grad()
       params = self.model(words)
@@ -103,8 +74,10 @@ class Model(BenchmarkModel):
       torch.nn.utils.clip_grad_norm_(self.model.parameters(), 3.0)
       self.opt.step()
 
+
   def eval(self, niter=1):
     raise NotImplementedError("Eval is not supported by this model")
+
 
 def cuda_sync(func, sync=False):
     func()
