cb4f38ca672b655142036f357c7728cbaf25b99c
Xu Zhao <xzhao9@fb.com>
2022-01-10 15:37:26 -0500

    Fix a bug in model name.

--- torchbenchmark/models/resnext50_32x4d/__init__.py
+++ torchbenchmark/models/resnext50_32x4d/__init__.py
@@ -5,5 +5,5 @@ class Model(TorchVisionModel):
     task = COMPUTER_VISION.CLASSIFICATION
 
     def __init__(self, device=None, jit=False, train_bs=8, eval_bs=8, extra_args=[]):
-        super().__init__(model_name="resnet50_32x4d", device=device, jit=jit,
+        super().__init__(model_name="resnext50_32x4d", device=device, jit=jit,
                          train_bs=train_bs, eval_bs=eval_bs, extra_args=extra_args)
