666308a731f82957ab2b2db8b1fb1314144940b1
Xu Zhao <xzhao9@fb.com>
2022-01-13 15:24:13 -0500

    Finished delta score. Working on overall score and domain score.

--- torchbenchmark/score/compute_score_v2.py
+++ torchbenchmark/score/compute_score_v2.py
@@ -1,12 +1,11 @@
 """
 Compute TorchBench Score V2.
 """
-import os
 import re
 import yaml
-import json
 import importlib
 from pathlib import Path
+from typing import List
 
 TORCHBENCH_V2_REF_DATA = Path(__file__).parent.joinpath("configs/v2/config-v2.yaml")
 TORCHBENCH_V2_DEFAULT_THRESHOLD = 0.07
@@ -32,10 +31,12 @@ def _parse_test_name(name):
     return (test, model_name, device, mode)
 
 class TorchBenchV2Test:
-    def __init__(self, test_name):
-        self._name = test_name
-        self._test_type, self._model, self._device, self._mode = _parse_test_name(test_name)
+    def __init__(self, test_item):
+        self._name = test_item.keys()[0]
+        self._test_type, self._model, self._device, self._mode = _parse_test_name(self._name)
         self._task = _get_model_task(self._model)
+        self._stable = test_item["stable"]
+        self._norm = test_item["norm"]
     @property
     def name(self) -> str:
         return self._name
@@ -57,17 +58,31 @@ class TorchBenchV2Test:
     @property
     def domain(self) -> str:
         return self._task.name
+    @property
+    def norm(self) -> float:
+        return self._norm
+    @property
+    def stable(self) -> bool:
+        return self._stable
+
 class TorchBenchV2Suite:
-    def __init__(self):
+    def __init__(self, norm):
         self._tests = []
         self._tests_dict = {}
+        self._threshold = norm["stable_threshold"]
+        for test in norm["tests"]:
+            test_item = TorchBenchV2Test(test)
+            self._add_test(test_item)
     @property
-    def all_tests(self):
-        return self._tests
-    def add_test(self, test: TorchBenchV2Test):
+    def all_stable_tests(self) -> List[TorchBenchV2Test]:
+        return list(filter(lambda x: x.stable, self._tests))
+    @property
+    def threshold(self) -> float:
+        return self.threshold
+    def _add_test(self, test: TorchBenchV2Test):
         self._tests.append(test)
         self._tests_dict[test.name] = test
-    def get_test_by_name(self, name):
+    def get_test_by_name(self, name) -> TorchBenchV2Test:
         return self._tests_dict[name]
 
 class TorchBenchScoreV2:
@@ -76,43 +91,25 @@ class TorchBenchScoreV2:
         if not ref_data:
             with open(TORCHBENCH_V2_REF_DATA) as ref_file:
                 ref_data = yaml.full_load(ref_file)
-        self.norm = self._setup_benchmark_norms(ref_data)
-        for skip_test in self.skip_tests:
-            self.norm.pop(skip_test, None)
         # Build the suite
-        self.suite = TorchBenchV2Suite()
-        for test_name in self.norm:
-            test = TorchBenchV2Test(test_name)
-            self.suite.add_test(test)
+        self.suite = TorchBenchV2Suite(self._setup_benchmark_norms(ref_data))
 
     def _get_test_delta_weight(self, ref_norm, data_norm):
         delta = (ref_norm - data_norm) / ref_norm * 100.0
-        # No valid signal found
-        if abs(delta) <= TORCHBENCH_V2_THRESHOLD:
+        # Not a valid signal because it is below threshold
+        if abs(delta) <= self.suite.threshold:
             return 0.0
         return delta / 100.0
 
     # compute the V2 total score
-    def _get_score(self, data_norm):
+    def _get_delta_score(self, data_norm):
         delta = 0.0
-        for test in self.norm:
-            ref_norm = self.norm[test]["norm"]
-            data_test_norm = data_norm[test]["norm"]
+        for test in self.suite.all_stable_tests:
+            ref_norm = test.norm
+            data_test_norm = data_norm["tests"][test.name]["norm"]
             delta_weight = self._get_test_delta_weight(ref_norm, data_test_norm)
             delta += delta_weight
         return delta
-
-    def compute_score(self, data):
-        """
-        This API calculates the total V2 score for all the benchmark tests in V2 set.
-        """
-        # Check the input test set is the superset of the ref
-        data_norm = self._get_norm_from_ref_json_obj(data)
-        diff_set = set(self.norm.keys()) - set(data_norm.keys())
-        assert not diff_set, f"The request benchmark json is missing V2 test: {diff_set}"
-        summary = {}
-        summary["total"] = self._get_score(data_norm)
-        return summary
 
     def _setup_benchmark_norms(self, ref_data):
         """
@@ -133,12 +130,29 @@ class TorchBenchScoreV2:
         and calculates the normalization values based on the reference data.
         """
         norm = dict()
+        norm["stable_threshold"] = TORCHBENCH_V2_DEFAULT_THRESHOLD
+        norm["tests"] = dict()
         for b in ref_json_obj['benchmarks']:
             name = b['name']
-            norm.setdefault(name, dict())
-            norm[name].setdefault('norm', dict())
-            norm[name]['norm'] = b['stats']['mean']
+            norm['tests'].setdefault(name, dict())
+            norm['tests'][name]['norm'] = b['stats']['median']
+            norm['tests'][name]['stable'] = True
         return norm
 
     def get_norm(self, data):
         return self._get_norm_from_ref_json_obj(data)
+
+    def compute_score(self, data):
+        """
+        This API calculates the total V2 score for all the benchmark tests in the set.
+        """
+        # Check the input test set is the superset of the ref
+        data_norm = self._get_norm_from_ref_json_obj(data)
+        diff_set = set(self.norm.keys()) - set(data_norm.keys())
+        assert not diff_set, f"The request benchmark json doesn't include the V2 test: {diff_set}"
+        summary = {}
+        # overall score
+        # delta score
+        summary["delta"] = self._get_delta_score(data_norm)
+        # domain score
+        return summary
