9ef429049419798e66078837671b23a751d894ee
Xu Zhao <xzhao9@fb.com>
2022-01-24 19:52:02 -0500

    Update the bisection script.

--- .github/scripts/run-bisection.sh
+++ .github/scripts/run-bisection.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 # Script that run the PR bisection
-# Requirements:
-# 1. Conda environment created using the following command:
-#     conda create -y -n bisection python=3.7
-#     # numpy=1.17 is from yolov3 requirements.txt, requests=2.22 is from demucs
-#     conda install numpy=1.17 requests=2.22 ninja pyyaml mkl mkl-include setuptools cmake cffi typing_extensions future six dataclasses
-# 2. PyTorch git repo (specified by PYTORCH_SRC_DIR) and TorchBench git repo (specified by TORCHBENCH_SRC_DIR)
-#    in clean state with the latest code.
-# 3. Bisection config, in the YAML format.
-#    An example of bisection configuration in YAML can be found in bisection-config.sample.yaml
 
 set -xeo pipefail
 
