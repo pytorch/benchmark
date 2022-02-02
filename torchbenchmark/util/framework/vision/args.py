ae6f8369597034a2df605682be0217e3f8ba6697
Xu Zhao <xzhao9@fb.com>
2022-02-02 15:11:11 -0500

    Fix another bug.

--- torchbenchmark/util/framework/vision/args.py
+++ torchbenchmark/util/framework/vision/args.py
@@ -47,7 +47,7 @@ def apply_args(model: BenchmarkModel, args: argparse.Namespace):
 
 def enable_tensortrt(eval_input: Tuple[torch.tensor], eval_fp16: bool, eval_model: torch.nn.Module) -> torch.nn.Module:
     import torch_tensorrt
-    trt_input = torch_tensorrt.Input(eval_input[0].shape)
+    trt_input = [torch_tensorrt.Input(eval_input[0].shape)]
     if eval_fp16:
         enabled_precisions = torch_tensorrt.dtype.half
     else:
