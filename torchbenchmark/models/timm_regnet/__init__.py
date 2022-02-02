162cf2d771940e46ceb4afda16765de2e45463f9
Xu Zhao <xzhao9@fb.com>
2022-02-02 16:57:22 -0500

    Added extra args.

--- torchbenchmark/models/timm_regnet/__init__.py
+++ torchbenchmark/models/timm_regnet/__init__.py
@@ -13,7 +13,7 @@ class Model(BenchmarkModel):
 
     def __init__(self, device=None, jit=False,
                  variant='regnety_016', precision='float32',
-                 eval_bs=32, train_bs=32):
+                 eval_bs=32, train_bs=32, extra_args=[]):
         super().__init__()
         self.device = device
         self.jit = jit
