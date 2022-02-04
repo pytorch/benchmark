df892278e1e09780a6634ab164902b8bdca3c639
Xu Zhao <xzhao9@fb.com>
2022-02-04 14:08:32 -0500

    Cleanup the code.

--- torchbenchmark/util/env_check.py
+++ torchbenchmark/util/env_check.py
@@ -1,13 +1,6 @@
 import importlib
 from typing import List, Dict
 
-def validate_args(**kwargs):
-    print(kwargs.keys())
-    assert "device" in kwargs, "Model must specify `device`."
-    assert "test" in kwargs, "Model must specify `test`, test can be either 'train' or 'eval'."
-    assert kwargs["test"] == "train" or kwargs["test"] == "eval", "Model test can only be train or eval."
-    assert "extra_args" in kwargs, "Model must specify `extra_args`"
-
 def get_pkg_versions(packages: List[str]) -> Dict[str, str]:
     versions = {}
     for module in packages:
