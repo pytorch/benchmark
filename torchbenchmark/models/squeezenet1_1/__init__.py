191363eed6006b7de604a565862fc28d340ed258
Xu Zhao <xzhao9@fb.com>
2022-02-09 15:18:56 -0500

    Fixed super slomo

--- torchbenchmark/models/squeezenet1_1/__init__.py
+++ torchbenchmark/models/squeezenet1_1/__init__.py
@@ -1,5 +1,7 @@
 from torchbenchmark.util.framework.vision.model_factory import TorchVisionModel
 from torchbenchmark.tasks import COMPUTER_VISION
+import torch.optim as optim
+import torch
 
 class Model(TorchVisionModel):
     task = COMPUTER_VISION.CLASSIFICATION
@@ -7,15 +9,19 @@ class Model(TorchVisionModel):
     # Original train batch size: 512, out of memory on V100 GPU
     # Use hierarchical batching to scale down: 512 = batch_size (32) * epoch_size (16)
     # Source: https://github.com/forresti/SqueezeNet
-    def __init__(self, test, device, jit=False, train_bs=32, eval_bs=16, extra_args=[]):
+    DEFAULT_TRAIN_BSIZE = 32
+    DEFAULT_EVAL_BSIZE = 16
+
+    def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
         super().__init__(model_name="squeezenet1_1", test=test, device=device, jit=jit,
-                         train_bs=train_bs, eval_bs=eval_bs, extra_args=extra_args)
+                         batch_size=batch_size, extra_args=extra_args)
         self.epoch_size = 16
+        if test == "train":
+            raise NotImplementedError("Temporarily disable training test because it causes CUDA OOM on T4")
     
     # Temporarily disable training because this will cause CUDA OOM in CI
     # TODO: re-enable this test when better hardware is available
     def train(self, niter=1):
-        raise NotImplementedError("Temporarily disable training test because it causes CUDA OOM on T4")
         optimizer = optim.Adam(self.model.parameters())
         loss = torch.nn.CrossEntropyLoss()
         for _ in range(niter):
