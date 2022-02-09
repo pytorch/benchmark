43d7814bd80c29c7926617d5dda43549012e0e7b
Xu Zhao <xzhao9@fb.com>
2022-02-09 14:13:43 -0500

    Fixed mobilenet_v2

--- torchbenchmark/models/LearningToPaint/__init__.py
+++ torchbenchmark/models/LearningToPaint/__init__.py
@@ -21,13 +21,11 @@ torch.backends.cudnn.benchmark = True
 
 class Model(BenchmarkModel):
     task = REINFORCEMENT_LEARNING.OTHER_RL
+    DEFAULT_TRAIN_BSIZE = 96
+    DEFAULT_EVAL_BSIZE = 96
 
-    def __init__(self, test, device, jit=False, train_bs=96, eval_bs=96, extra_args=[]):
-        super().__init__()
-        self.device = device
-        self.jit = jit
-        self.test = test
-        self.extra_args = extra_args
+    def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
+        super().__init__(test=test, device=device, jit=jit, batch_size=batch_size, extra_args=extra_args)
         # Train: These options are from source code.
         # Source: https://arxiv.org/pdf/1903.04411.pdf
         # Code: https://github.com/megvii-research/ICCV2019-LearningToPaint/blob/master/baseline/train.py
@@ -41,8 +39,6 @@ class Model(BenchmarkModel):
             'tau': 0.001,
             'rmsize': 800,
         })
-        self.train_bs = train_bs
-        self.eval_bs = eval_bs
 
         # Train: input images are from CelebFaces and resized to 128 x 128.
         # Create 2000 random tensors for input, but randomly sample 200,000 images.
@@ -52,34 +48,26 @@ class Model(BenchmarkModel):
         # LearningToPaint includes actor, critic, and discriminator models.
         self.Decoder = FCN()
         self.step = 0
+        self.env = fastenv(max_episode_length=self.args.max_step, env_batch=self.batch_size,
+                        images=self.image_examples, device=self.device, Decoder=self.Decoder)
+        self.agent = DDPG(batch_size=self.batch_size, env_batch=self.batch_size,
+                                max_step=self.args.max_step, tau=self.args.tau, discount=self.args.discount,
+                                rmsize=self.args.rmsize, device=self.device, Decoder=self.Decoder)
+        self.evaluate = Evaluator(args=self.args, env_batch=self.batch_size, writer=None)
+        self.observation = self.env.reset()
+        self.agent.reset(self.observation, self.args.noise_factor)
 
         if test == "train":
-            self.env = fastenv(max_episode_length=self.args.max_step, env_batch=self.train_bs,
-                                    images=self.image_examples, device=self.device, Decoder=self.Decoder)
-            self.agent = DDPG(batch_size=self.train_bs, env_batch=self.train_bs,
-                                    max_step=self.args.max_step, tau=self.args.tau, discount=self.args.discount,
-                                    rmsize=self.args.rmsize, device=self.device, Decoder=self.Decoder)
-            self.evaluate = Evaluator(args=self.args, env_batch=train_bs, writer=None)
             self.agent.train()
-            self.observation = self.env.reset()
-            self.agent.reset(self.observation, self.args.noise_factor)
         elif test == "eval":
-            self.env = fastenv(max_episode_length=self.args.max_step, env_batch=self.eval_bs,
-                                    images=self.image_examples, device=self.device, Decoder=self.Decoder)
-            self.agent = DDPG(batch_size=self.eval_bs, env_batch=self.eval_bs,
-                                    max_step=self.args.max_step, tau=self.args.tau, discount=self.args.discount,
-                                    rmsize=self.args.rmsize, device=self.device, Decoder=self.Decoder)
-            self.evaluate = Evaluator(args=self.args, env_batch=eval_bs, writer=None)
             self.agent.eval()
-            self.observation = self.env.reset()
-            self.agent.reset(self.observation, self.args.noise_factor)
 
     def get_module(self):
         action = self.agent.select_action(self.observation, noise_factor=self.args.noise_factor)
         self.observation, reward, done, _ = self.env.step(action)
         self.agent.observe(reward, self.observation, done, self.step)
         state, action, reward, \
-            next_state, terminal = self.agent.memory.sample_batch(self.eval_bs, self.device)
+            next_state, terminal = self.agent.memory.sample_batch(self.batch_size, self.device)
         state = torch.cat((state[:, :6].float() / 255, state[:, 6:7].float() / self.args.max_step,
                            self.agent.coord.expand(state.shape[0], 2, 128, 128)), 1)
         return self.agent.actor, (state, )
@@ -120,6 +108,8 @@ class Model(BenchmarkModel):
         for _ in range(niter):
             reward, dist = self.evaluate(self.env, self.agent.select_action)
 
-    # Using separate models for train and infer, so skip this function.
     def _set_mode(self, train):
-        pass
+        if train:
+            self.agent.train()
+        else:
+            self.agent.eval()
--- torchbenchmark/models/mnasnet1_0/__init__.py
+++ torchbenchmark/models/mnasnet1_0/__init__.py
@@ -3,7 +3,9 @@ from torchbenchmark.tasks import COMPUTER_VISION
 
 class Model(TorchVisionModel):
     task = COMPUTER_VISION.CLASSIFICATION
+    DEFAULT_TRAIN_BSIZE = 32
+    DEFAULT_EVAL_BSIZE = 32
 
-    def __init__(self, test, device, jit=False, train_bs=32, eval_bs=32, extra_args=[]):
+    def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
         super().__init__(model_name="mnasnet1_0", test=test, device=device, jit=jit,
-                         train_bs=train_bs, eval_bs=eval_bs, extra_args=extra_args)
+                         batch_size=batch_size, extra_args=extra_args)
