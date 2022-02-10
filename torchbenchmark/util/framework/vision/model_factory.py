40a1d64db5e10f86ed3a6685f5707ab588549419
Xu Zhao <xzhao9@fb.com>
2022-02-10 18:31:03 -0500

    Fix result for model

--- torchbenchmark/util/framework/vision/model_factory.py
+++ torchbenchmark/util/framework/vision/model_factory.py
@@ -27,6 +27,8 @@ class TorchVisionModel(BenchmarkModel):
             # setup optimizer and loss_fn
             self.optimizer = optim.Adam(self.model.parameters())
             self.loss_fn = torch.nn.CrossEntropyLoss()
+            self.real_input = [ torch.rand_like(self.example_inputs[0]) ]
+            self.real_output = [ torch.rand_like(self.example_outputs) ]
         elif test == "eval":
             self.model.eval()
 
