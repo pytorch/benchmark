edf348864dcbd7e6a327314edb2bcdab57f2002b
Xu Zhao <xzhao9@fb.com>
2022-02-11 08:42:55 -0500

    Fix torch trt interface

--- torchbenchmark/util/backends/torch_trt.py
+++ torchbenchmark/util/backends/torch_trt.py
@@ -1,7 +1,7 @@
 import torch
 from typing import Tuple
 
-def enable_torchtrt(example_input: Tuple[torch.tensor], precision: str, model: torch.nn.Module) -> torch.nn.Module:
+def enable_torchtrt(precision: str, model: torch.nn.Module, example_inputs: Tuple[torch.tensor]) -> torch.nn.Module:
     import torch_tensorrt
     if precision == "fp16":
         torchtrt_dtype = torch_tensorrt.dtype.half
@@ -11,6 +11,6 @@ def enable_torchtrt(example_input: Tuple[torch.tensor], precision: str, model: t
         torch_dtype = torch.float32
     else:
         raise NotImplementedError("torch_tensorrt only supports fp32 or fp16 precision")
-    trt_input = [torch_tensorrt.Input(shape=example_input[0].shape, dtype=torch_dtype)]
+    trt_input = [torch_tensorrt.Input(shape=example_inputs[0].shape, dtype=torch_dtype)]
 
     return torch_tensorrt.compile(model, inputs=trt_input, enabled_precisions=torchtrt_dtype)
