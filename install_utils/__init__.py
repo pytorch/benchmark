7fadcc8ae2023b83bcc99cb23889681012beacfc
Xu Zhao <xzhao9@fb.com>
2022-02-15 14:52:48 -0500

    Fix import dependency

---
+++ install_utils/__init__.py
@@ -0,0 +1,22 @@
+import importlib
+from urllib import request
+from typing import List, Dict
+
+TORCH_DEPS = ['torch', 'torchvision', 'torchtext']
+proxy_suggestion = "Unable to verify https connectivity, " \
+                   "required for setup.\n" \
+                   "Do you need to use a proxy?"
+
+def get_pkg_versions(packages: List[str]) -> Dict[str, str]:
+    versions = {}
+    for module in packages:
+        module = importlib.import_module(module)
+        versions[module] = module.__version__
+    return versions
+
+def _test_https(test_url: str = 'https://github.com', timeout: float = 0.5) -> bool:
+    try:
+        request.urlopen(test_url, timeout=timeout)
+    except OSError:
+        return False
+    return True
