5864dc1a32b29ee0c84ddf6f4f34f3bcedff5c15
Xu Zhao <xzhao9@fb.com>
2022-02-22 14:16:08 -0500

    Correct grammar error.

--- run.py
+++ run.py
@@ -188,4 +188,4 @@ if __name__ == "__main__":
     else:
         run_one_step(test, model_flops=model_flops)
     if hasattr(m, 'correctness'):
-        print('{:<20} {:>20}'.format("Corrnectness:", "%.15f" % m.correctness), sep='')
+        print('{:<20} {:>20}'.format("Correctness:", "%.15f" % m.correctness), sep='')
