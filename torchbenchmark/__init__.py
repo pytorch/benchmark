1893cbe3d07de62a3ebce7dbf46e9443c149ae99
Xu Zhao <xzhao9@fb.com>
2022-02-23 21:49:50 -0500

    Fix another bug

--- torchbenchmark/__init__.py
+++ torchbenchmark/__init__.py
@@ -372,11 +372,11 @@ class ModelTask(base_task.TaskBase):
         out = instance.eval()
         model_name = getattr(instance, 'name', None)
         if not isinstance(out, tuple):
-            raise RuntimeError('Model {model_name} eval() output is not a tuple')
+            raise RuntimeError(f'Model {model_name} eval() output is not a tuple')
         for ind, element in enumerate(out):
             if not isinstance(element, torch.Tensor):
-                raise RuntimeError('Model {model_name} eval() output is tuple, but'
-                                   ' its {ind}-th element is not a Tensor.')
+                raise RuntimeError(f'Model {model_name} eval() output is tuple, but'
+                                   f' its {ind}-th element is not a Tensor.')
 
     @base_task.run_in_worker(scoped=True)
     @staticmethod
--- torchbenchmark/models/timm_resnest/__init__.py
+++ torchbenchmark/models/timm_resnest/__init__.py
@@ -64,4 +64,4 @@ class Model(BenchmarkModel):
         with torch.no_grad():
             for _ in range(niter):
                 out = self._step_eval()
-        return out
+        return (out, )
