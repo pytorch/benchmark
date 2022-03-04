f6019d15d45915b9195384eba00657d179a2054a
Xu Zhao <xzhao9@fb.com>
2022-03-04 17:49:18 -0500

    Try another lowering imp.

--- torchbenchmark/util/backends/fx2trt.py
+++ torchbenchmark/util/backends/fx2trt.py
@@ -1,6 +1,60 @@
 import torch
 from typing import Tuple, Optional
 
+def enable_hf_fx2trt_no_lower(traced_model, input_all):
+    import fx2trt_oss.tracer.acc_tracer.acc_tracer as acc_tracer
+
+    from fx2trt_oss.fx.fx2trt import (
+        TRTInterpreter,
+        InputTensorSpec,
+    )
+    from fx2trt_oss.fx.tools.trt_splitter import (
+        TRTSplitter,
+        TRTSplitterSetting,
+    )
+    from fx2trt_oss.fx.trt_module import TRTModule
+    def get_submod_inputs(mod, submod, inputs):
+        acc_inputs = None
+
+        def get_input(self, inputs):
+            nonlocal acc_inputs
+            acc_inputs = inputs
+
+        handle = submod.register_forward_pre_hook(get_input)
+        mod(*inputs)
+        handle.remove()
+        return acc_inputs
+
+    # Trace with acc_tracer
+    acc_model = acc_tracer.trace(traced_model, input_all)
+    # Split out unsupported ops
+    splitter_setting = TRTSplitterSetting()
+    splitter_setting.use_implicit_batch_dim = False
+    splitter = TRTSplitter(acc_model, input_all, settings=splitter_setting)
+    splitter.node_support_preview()
+    split_mod = splitter()
+
+    for name, _ in split_mod.named_children():
+        if "_run_on_acc" in name:
+            print("replace submod=", name)
+            submod = getattr(split_mod, name)
+            # Get submodule inputs for fx2trt
+            acc_inputs = get_submod_inputs(split_mod, submod, input_all)
+
+            # fx2trt replacement
+            interp = TRTInterpreter(
+                submod,
+                InputTensorSpec.from_tensors(acc_inputs),
+                explicit_batch_dimension=True,
+            )
+            r = interp.run(
+                max_workspace_size=1 << 30,
+                strict_type_constraints=True,
+            )
+            trt_mod = TRTModule(*r)
+            setattr(split_mod, name, trt_mod)
+    return split_mod
+
 def enable_fx2trt(max_batch_size: int, fp16: bool, model: torch.nn.Module, example_inputs: Tuple[torch.tensor],
                   is_hf_model: bool=False, hf_max_length: Optional[int]=None) -> torch.nn.Module:
     # special enablement for huggingface models
@@ -11,14 +65,15 @@ def enable_fx2trt(max_batch_size: int, fp16: bool, model: torch.nn.Module, examp
             batch_size=max_batch_size,
             sequence_length=hf_max_length,
         )
-        return lower_to_trt(
-            traced_model,
-            example_inputs,
-            max_batch_size=max_batch_size,
-            fp16_mode=True,
-            explicit_batch_dimension=True,
-            max_workspace_size=20 << 30,
-        )
+        # return lower_to_trt(
+        #     traced_model,
+        #     example_inputs,
+        #     max_batch_size=max_batch_size,
+        #     fp16_mode=True,
+        #     explicit_batch_dimension=True,
+        #     max_workspace_size=20 << 30,
+        # )
+        return enable_hf_fx2trt_no_lower(traced_model=traced_model, input_all=example_inputs)
     return lower_to_trt(module=model, input=example_inputs, \
                         max_batch_size=max_batch_size, fp16_mode=fp16)
 
