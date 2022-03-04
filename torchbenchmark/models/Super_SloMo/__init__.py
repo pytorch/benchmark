0a9ed43158f7c0950b1d710b1d6089a7b3803c97
Xu Zhao <xzhao9@fb.com>
2022-03-03 20:08:38 -0500

    Fix final init bug

--- torchbenchmark/models/Super_SloMo/__init__.py
+++ torchbenchmark/models/Super_SloMo/__init__.py
@@ -34,7 +34,7 @@ class Model(BenchmarkModel):
     DEFAULT_EVAL_BSIZE = 10
 
     def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
-        if self.jit and self.test == "eval" and self.devcie == "cuda":
+        if jit and test == "eval" and device == "cuda":
             raise NotImplementedError("Disabled eval jit test due to insufficient GPU memory size")
         super().__init__(test=test, device=device, jit=jit, batch_size=batch_size, extra_args=extra_args)
 
