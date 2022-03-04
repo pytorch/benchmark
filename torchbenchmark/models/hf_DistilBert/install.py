46707b3d8f740c6fcf82a359d1e7c1a6ef3f813d
Xu Zhao <xzhao9@fb.com>
2022-03-03 19:04:09 -0500

    Fixed patch code.

--- torchbenchmark/models/hf_DistilBert/install.py
+++ torchbenchmark/models/hf_DistilBert/install.py
@@ -1,10 +1,11 @@
 
 import subprocess
 import sys
-from torchbenchmark.util.framework.huggingface.patch import patch_transformer
+from torchbenchmark.util.framework.huggingface.patch import patch_transformers
 
 def pip_install_requirements():
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-q', '-r', 'requirements.txt'])
+
 if __name__ == '__main__':
     pip_install_requirements()
-    patch_transformer()
+    patch_transformers()
--- torchbenchmark/util/framework/huggingface/patch.py
+++ torchbenchmark/util/framework/huggingface/patch.py
@@ -7,11 +7,11 @@ import patch
 
 PATCH_DIR = os.path.join(os.path.dirname(__file__), "patches")
 
-def patch_transformer():
+def patch_transformers():
     import transformers
     patch_file = os.path.join(PATCH_DIR, "0001-transformers-enable-fx.patch")
     transformers_dir = os.path.dirname(transformers.__file__)
     p = patch.fromfile(patch_file)
     if not p.apply(strip=1, root=transformers_dir):
-        print("Failed to patch fastNLP. Exit.")
+        print("Failed to patch trasformers package. Exit.")
         sys.exit(1)
