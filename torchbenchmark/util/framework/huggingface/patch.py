f05f417d50f0582e3d329b90b96e22c09465204d
Xu Zhao <xzhao9@fb.com>
2022-03-04 13:02:22 -0500

    Update the transformers package version

--- torchbenchmark/util/framework/huggingface/patch.py
+++ torchbenchmark/util/framework/huggingface/patch.py
@@ -12,9 +12,7 @@ def patch_transformers():
     import transformers
     patch_file = os.path.join(PATCH_DIR, "0001-transformers-enable-fx.patch")
     transformers_dir = os.path.dirname(transformers.__file__)
-    # only patch on Linux because the patch doesn't apply on Windows or macOS
-    if platform == "linux" or platform == "linux2":
-        p = patch.fromfile(patch_file)
-        if not p.apply(strip=1, root=transformers_dir):
-            print("Failed to patch trasformers package. Exit.")
-            sys.exit(1)
+    p = patch.fromfile(patch_file)
+    if not p.apply(strip=1, root=transformers_dir):
+        print("Failed to patch trasformers package. Exit.")
+        sys.exit(1)
