6c2e74ef91052e64e38359467779020baeeb6273
Xu Zhao <xzhao9@fb.com>
2022-03-10 18:23:56 -0500

    Fix another issue

--- torchbenchmark/models/Background_Matting/__init__.py
+++ torchbenchmark/models/Background_Matting/__init__.py
@@ -30,6 +30,8 @@ class Model(BenchmarkModel):
     # Original hardware: unknown
     # Source: https://arxiv.org/pdf/2004.00626.pdf
     DEFAULT_TRAIN_BSIZE = 4
+    DEFAULT_EVAL_BSIZE = 1
+    ALLOW_CUSTOMIZE_BSIZE = False
 
     def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
         super().__init__(test=test, device=device, jit=jit, batch_size=batch_size, extra_args=extra_args)
