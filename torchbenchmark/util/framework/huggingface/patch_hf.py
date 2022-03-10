8ff17104393c68263ae9da37b6b5f5028925e831
Xu Zhao <xzhao9@fb.com>
2022-03-09 19:58:19 -0500

    Enable hf_BigBird half fp16

--- torchbenchmark/util/framework/huggingface/patch_hf.py
+++ torchbenchmark/util/framework/huggingface/patch_hf.py
@@ -9,16 +9,15 @@ PATCH_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "patches")
 
 def patch_transformers():
     import transformers
-    import patch
-    patch_file = os.path.join(PATCH_DIR, "0001-transformers-enable-fx.patch")
     transformers_dir = os.path.dirname(transformers.__file__)
-    p = patch.fromfile(patch_file)
-    try:
-        subprocess.check_output(["patch", "-p1", "--forward", "-i", patch_file, "-r", "/tmp/rej"], cwd=transformers_dir)
-    except subprocess.SubprocessError as e:
-        output_str = str(e.output)
-        if "previously applied" in output_str:
-            return
-        else:
-            print(str(output_str))
-            sys.exit(1)
+    for patch_file in os.listdir(PATCH_DIR):
+        patch_file_fullpatch = os.path.join(PATCH_DIR, patch_file)
+        try:
+            subprocess.check_output(["patch", "-p1", "--forward", "-i", patch_file_fullpatch, "-r", "/tmp/rej"], cwd=transformers_dir)
+        except subprocess.SubprocessError as e:
+            output_str = str(e.output)
+            if "previously applied" in output_str:
+                return
+            else:
+                print(str(output_str))
+                sys.exit(1)
