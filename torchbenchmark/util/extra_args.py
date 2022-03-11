536969ca90bea853a10515facf24bed016f98aa5
Xu Zhao <xzhao9@fb.com>
2022-03-11 06:15:42 -0800

    Enable fp16 by default for hf models (#782)
    Summary:
    This PR sets `fp16` to be the default precision of all huggingface models.
    This PR also includes an extra patch to the transformers package, because `hf_BigBird` needs to be patched in order to support fp16.
    
    I believe this patch should also be upstream-ed: https://github.com/huggingface/transformers/pull/16034
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/782
    
    Reviewed By: frank-wei
    
    Differential Revision: D34803502
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 3d46f7983aa32333b12af605f69e45f1fe3134d7

--- torchbenchmark/util/extra_args.py
+++ torchbenchmark/util/extra_args.py
@@ -31,7 +31,7 @@ def check_fp16(model: 'torchbenchmark.util.model.BenchmarkModel', fp16: str) -> 
 
 # torchvision models uses fp16 half mode by default, others use fp32
 def get_fp16_default(model: 'torchbenchmark.util.model.BenchmarkModel') -> str:
-    if is_torchvision_model(model) and model.test == 'eval' and model.device == 'cuda':
+    if (is_torchvision_model(model) or is_hf_model(model)) and model.test == 'eval' and model.device == 'cuda':
         return "half"
     return "no"
 
