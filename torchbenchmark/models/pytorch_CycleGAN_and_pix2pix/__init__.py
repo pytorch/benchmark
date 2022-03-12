14a2f5469efe2c0d8bcdef9012551b728976aba1
Xu Zhao <xzhao9@fb.com>
2022-03-11 19:04:58 -0500

    Cleanup intermediate files

--- torchbenchmark/models/pytorch_CycleGAN_and_pix2pix/__init__.py
+++ torchbenchmark/models/pytorch_CycleGAN_and_pix2pix/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import torch
 import os
+from pathlib import Path
 
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
@@ -11,6 +12,11 @@ from typing import Tuple
 from .train_cyclegan import prepare_training_loop
 from .test_cyclegan import get_model
 
+def _create_data_dir(suffix):
+    data_dir = Path(__file__).parent.joinpath(".data", suffix)
+    data_dir.mkdir(parents=True, exist_ok=True)
+    return data_dir
+
 class Model(BenchmarkModel):
     task = COMPUTER_VISION.GENERATION
     DEFAULT_TRAIN_BSIZE = 1
@@ -19,15 +25,21 @@ class Model(BenchmarkModel):
 
     def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
         super().__init__(test=test, device=device, jit=jit, batch_size=batch_size, extra_args=extra_args)
+        checkpoints_dir = _create_data_dir("checkpoints")
+        results_dir = _create_data_dir("results")
+        checkpoints_arg = f"--checkpoints_dir {checkpoints_dir}"
+        results_arg = f"--results_dir {results_dir}"
         device_arg = ""
         if self.device == "cpu":
             device_arg = "--gpu_ids -1"
         elif self.device == "cuda":
             device_arg = "--gpu_ids 0"
         if self.test == "train":
-            train_args = f"--dataroot {os.path.dirname(__file__)}/datasets/horse2zebra --name horse2zebra --model cycle_gan --display_id 0 --n_epochs 3 --n_epochs_decay 3 {device_arg}"
+            train_args = f"--dataroot {os.path.dirname(__file__)}/datasets/horse2zebra --name horse2zebra --model cycle_gan --display_id 0 --n_epochs 3 \
+                           --n_epochs_decay 3 {device_arg} {checkpoints_arg}"
             self.training_loop = prepare_training_loop(train_args.split(' '))
-        args = f"--dataroot {os.path.dirname(__file__)}/datasets/horse2zebra/testA --name horse2zebra_pretrained --model test --no_dropout {device_arg}"
+        args = f"--dataroot {os.path.dirname(__file__)}/datasets/horse2zebra/testA --name horse2zebra_pretrained --model test \
+                 --no_dropout {device_arg} {checkpoints_arg} {results_arg}"
         self.model, self.input = get_model(args, self.device)
 
     def get_module(self):
