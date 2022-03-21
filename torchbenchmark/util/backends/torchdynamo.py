9eeca08b615c70b955081d21297ddb8785ba7c79
Xu Zhao <xzhao9@fb.com>
2022-03-21 17:09:20 -0400

    Initial torchdynamo support

--- torchbenchmark/util/backends/torchdynamo.py
+++ torchbenchmark/util/backends/torchdynamo.py
@@ -4,65 +4,28 @@ Support TorchDynamo(https://github.com/facebookresearch/torchdynamo) backends
 import argparse
 from typing import List
 
+class NullContext:
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+def null_experiment(args, model_iter_fn, model, example_inputs):
+    """
+    A no-op experiment useful for making sure TorchBenchmark alone works properly.
+    """
+
+    return []
+
 def parse_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', extra_args: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     args = parser.parse_args(extra_args)
     group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        "--coverage", action="store_true", help="(default) " + help(coverage_experiment)
-    )
-    group.add_argument(
-        "--online-autotune", action="store_true", help=help(speedup_experiment)
-    )
-    group.add_argument(
-        "--offline-autotune", action="store_true", help=help(speedup_experiment)
-    )
-    group.add_argument(
-        "--speedup-fixed1",
-        action="store_true",
-        help="speedup using experimental fixed_strategy backend",
-    )
-    group.add_argument(
-        "--speedup-fixed2",
-        action="store_true",
-        help="speedup using experimental fixed_strategy backend",
-    )
-    group.add_argument(
-        "--speedup-ltc",
-        action="store_true",
-        help="speedup using the ltc backend",
-    )
-    group.add_argument(
-        "--speedup-ltc-trivial",
-        action="store_true",
-        help="speedup using the ltc backend without reusing compiled graph",
-    )
-    group.add_argument(
-        "--overhead", action="store_true", help=help(overhead_experiment)
-    )
-    group.add_argument(
-        "--speedup-ts", action="store_true", help=help(speedup_experiment_ts)
-    )
-    group.add_argument(
-        "--speedup-sr", action="store_true", help=help(speedup_experiment_sr)
-    )
-    group.add_argument(
-        "--speedup-onnx", action="store_true", help=help(speedup_experiment_onnx)
-    )
-    group.add_argument(
-        "--speedup-trt", action="store_true", help=help(speedup_experiment_trt)
-    )
-    group.add_argument(
-        "--accuracy-aot-nop",
-        action="store_true",
-        help="Accuracy testing for AOT vs Eager",
-    )
-    group.add_argument(
-        "--speedup-aot-efficient-fusion",
-        action="store_true",
-        help="speedup using experimental fixed_strategy backend",
-    )
     group.add_argument("--nothing", action="store_true", help=help(null_experiment))
+    parser.add_argument(
+        "--nopython", action="store_true", help="Turn graph breaks into errors"
+    )
     group.add_argument(
         "--nops",
         action="store_true",
@@ -71,5 +34,14 @@ def parse_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', ex
     args = parser.parse_args()
     return args
 
-def apply_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', extra_args: List[str]):
-    pass
+def apply_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', args: argparse.Namespace):
+    import torchdynamo
+    optimize_ctx = NullContext()
+    if args.nothing:
+        pass
+    elif args.nops:
+        optimize_ctx = torchdynamo.eval_frame._optimize_catch_errors(
+            torchdynamo.testing.debug_insert_nops, nopython=args.nopython
+        )
+    model.add_context(optimize_ctx)
+    torchdynamo.reset()
