08083fd77f38f81f6eb1c9a3d76d765d07306257
Xu Zhao <xzhao9@fb.com>
2022-03-25 14:48:30 -0400

    Remove unnecessary files.

--- torchbenchmark/util/torch_feature_checker.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def check_native_amp():
-    try:
-        if getattr(torch.cuda.amp, 'autocast') is not None:
-            return True
-    except AttributeError:
-        return False
-    return False
