6989d26aff5fbd9c569e29d63b0cd9c14f0b8260
Xu Zhao <xzhao9@fb.com>
2022-03-29 20:08:36 -0400

    Install dependencies.

--- torchbenchmark/models/fambench_xlmr/install.py
+++ torchbenchmark/models/fambench_xlmr/install.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import subprocess
 from torchbenchmark import REPO_PATH
 
@@ -8,5 +9,9 @@ def update_fambench_submodule():
                       "--init", "--recursive", os.path.join("submodules","FAMBench")]
     subprocess.check_call(update_command, cwd=REPO_PATH)
 
+def pip_install_requirements():
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-q', '-r', 'requirements.txt'])
+
 if __name__ == "__main__":
     update_fambench_submodule()
+    pip_install_requirements()
