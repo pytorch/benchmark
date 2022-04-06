18a3138f7791c05f1c2b44b24b0ba7a86ace5c32
Xu Zhao <xzhao9@fb.com>
2022-04-05 20:18:09 -0400

    Add precision

--- torchbenchmark/util/backends/torchdynamo.py
+++ torchbenchmark/util/backends/torchdynamo.py
@@ -14,7 +14,10 @@ def parse_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', dy
     args = parser.parse_args(dyamo_args)
     return args
 
-def apply_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', args: argparse.Namespace):
+def apply_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', args: argparse.Namespace, precision: str):
     import torchdynamo
-    model.add_context(functools.partial(torchdynamo.optimize, args.torchdynamo))
+    if precision == "fp16" and args.torchdynamo == "fx2trt":
+        model.add_context(functools.partial(torchdynamo.optimize, "fx2trt_compiler_fp16"))
+    else:
+        model.add_context(functools.partial(torchdynamo.optimize, args.torchdynamo))
     torchdynamo.reset()
