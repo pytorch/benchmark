77fea82dca4363ae197dd8234ba7645cec8b4abc
Ankith Gunapal <agunapal@fb.com>
2022-04-15 13:57:01 -0700

    Use torch.nn.Module wrapper

--- torchbenchmark/models/pplbench_beanmachine/__init__.py
+++ torchbenchmark/models/pplbench_beanmachine/__init__.py
@@ -10,6 +10,31 @@ from pplbench.ppls.beanmachine import robust_regression
 from pplbench.models.robust_regression import RobustRegression
 from pplbench.ppls.beanmachine.inference import MCMC
 
+class Pplbench(torch.nn.Module):
+    def __init__(self):
+        super(Pplbench, self).__init__()
+
+        # Instantiate model
+        self.model = RobustRegression()
+
+        # Get data for evaluating model
+        self.train_data, self.test_data = self.model.generate_data(seed=int(time.time()))
+
+        # Create inference object with training data
+        self.infer_obj = MCMC(robust_regression.RobustRegression,
+                              self.train_data.attrs)
+        self.infer_obj.compile()
+
+    def forward(self, train_data, test_data):
+
+        # Run inference on given data
+        samples = self.infer_obj.infer(data=train_data, iterations=1, num_warmup=0, seed=random.randint(1, int(1e7)))
+
+        # Evaluate the model with test data
+        out = self.model.evaluate_posterior_predictive(samples, test_data)
+
+        return torch.Tensor(out)
+
 class Model(BenchmarkModel):
     task = OTHER.OTHER_TASKS
     DEFAULT_EVAL_BSIZE = 1
@@ -21,17 +46,9 @@ class Model(BenchmarkModel):
             raise NotImplementedError("The eval test only supports CPU.")
 
         # Instantiate model
-        self.model = RobustRegression()
+        self.model = Pplbench()
 
-        # Get data for evaluating model
-        self.train_data, self.test_data = self.model.generate_data(seed=int(time.time()))
-
-        # Create inference object with training data
-        self.infer_obj = MCMC(robust_regression.RobustRegression,
-                    self.train_data.attrs)
-        self.infer_obj.compile()
-
-        self.example_inputs = self.test_data
+        self.example_inputs = (self.model.train_data, self.model.test_data)
 
     def get_module(self):
         return self.model, self.example_inputs
@@ -42,12 +59,5 @@ class Model(BenchmarkModel):
     def eval(self, niter=1) -> Tuple[torch.Tensor]:
         model, example_inputs = self.get_module()
 
-        # Run inference on given data
-        samples = self.infer_obj.infer(data=self.train_data, iterations=1, num_warmup=0,
-                                  seed=random.randint(1, int(1e7)))
-
-        # Evaluate the model with test data
-        out = model.evaluate_posterior_predictive(
-                samples, self.test_data
-            )
-        return (torch.Tensor(out), )
+        out = model(*example_inputs)
+        return (out, )
