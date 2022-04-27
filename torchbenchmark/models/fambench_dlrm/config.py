f6c4d27188bb2677f0a135b92fa2b6e51c75eb52
Xu Zhao <xzhao9@fb.com>
2022-04-27 14:15:03 -0700

    Update fambench_dlrm configs (#883)
    Summary:
    Instead of using dummy config, we should use realistic config for fambench_dlrm.
    Note that some of the options in the realistic config are disabled because we currently don't support fbgemm and quantized mlp.
    
    Runtime result:
    ```
    $ python run.py fambench_dlrm -d cuda
    Unable to import mlperf_logging,  No module named 'mlperf_logging'
    Running eval method from fambench_dlrm on cuda in eager mode.
    GPU Time:            114.694 milliseconds
    CPU Dispatch Time:   113.238 milliseconds
    CPU Total Wall Time: 114.780 milliseconds
    ```
    
    ```
    python run.py fambench_dlrm -d cuda -t train
    Unable to import mlperf_logging,  No module named 'mlperf_logging'
    Running train method from fambench_dlrm on cuda in eager mode.
    GPU Time:            344.140 milliseconds
    CPU Dispatch Time:   289.747 milliseconds
    CPU Total Wall Time: 344.274 milliseconds
    ```
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/883
    
    Reviewed By: aaronenyeshi
    
    Differential Revision: D35975894
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 23f637a8551a51b09eea03c2d4011e38470de139

--- torchbenchmark/models/fambench_dlrm/config.py
+++ torchbenchmark/models/fambench_dlrm/config.py
@@ -8,40 +8,70 @@ def cfg_to_str(cfg: dataclasses.dataclass) -> List[str]:
     out = []
     for fld in dataclasses.fields(cfg):
         new_option = rewrite_option(fld.name)
-        out.append(new_option)
-        out.append(str(getattr(cfg, fld.name)))
+        val = getattr(cfg, fld.name)
+        if isinstance(val, bool):
+            if val:
+                out.append(new_option)
+        else:
+            out.append(new_option)
+            out.append(str(getattr(cfg, fld.name)))
     return out
 
-# Original config location:
+# dummy config location:
 # https://github.com/facebookresearch/FAMBench/blob/a0f12ca4fe8973f4cc65d18b51ce3aa94ceec0ac/benchmarks/run_dlrm_ootb_train.sh#L54
+# config: A.1dev-embed32-fp32
 @dataclasses.dataclass
 class FAMBenchTrainConfig:
-    mini_batch_size: int = 64
-    test_mini_batch_size: int = 64
+    mini_batch_size: int = 1024
+    test_mini_batch_size: int = 1024
     test_num_workers: int = 0
     data_generation: str = "random"
-    arch_mlp_bot:str = "512-512-64"
-    arch_mlp_top:str = "1024-1024-1024-1"
-    arch_sparse_feature_size:int = 64
-    arch_embedding_size:str = "1000000-1000000-1000000-1000000-1000000-1000000-1000000-1000000"
-    num_indices_per_lookup:int = 100
+    arch_mlp_bot:str = "2000-1500-1500-1500-192"
+    arch_mlp_top:str = "4000-4000-4000-4000-4000-4000-4000-4000-4000-1"
+    arch_sparse_feature_size:int = 192
+    arch_embedding_size:str = "965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965-965"
+    num_indices_per_lookup:int = 55
+    num_indices_per_lookup_fixed:int = 1
     numpy_rand_seed:int = 727
-    # not used, overridden by Model.DEFAULT_NUM_BATCHES
-    num_batches:int = 200
+    weighted_pooling: str = "learned"
+    # torchbench: run 2 batches only (original 15)
+    num_batches:int = 2
+    # torchbench: these items in the original config are disabled
+    # because they are handled by the framework
+    # num_batches:int = 15
+    # warmup_step = 5
+    # use_gpu: bool = True
+    # precache_ml_data: bool = True
 
-# Original config location:
+# dummy config location:
 # https://github.com/facebookresearch/FAMBench/blob/a0f12ca4fe8973f4cc65d18b51ce3aa94ceec0ac/benchmarks/run_dlrm_ootb_infer.sh#L54
+# config: A.1dev-embed4-fp16
 @dataclasses.dataclass
 class FAMBenchEvalConfig:
-    mini_batch_size:int = 64
-    test_mini_batch_size:int = 64
+    mini_batch_size:int = 1024
+    test_mini_batch_size:int = 1024
     test_num_workers:int = 0
     data_generation:str = "random"
-    arch_mlp_bot:str = "512-512-64"
-    arch_mlp_top:str = "1024-1024-1024-1"
-    arch_sparse_feature_size:int = 64
-    arch_embedding_size:str = "1000000-1000000-1000000-1000000-1000000-1000000-1000000-1000000"
-    num_indices_per_lookup:int = 100
+    arch_mlp_bot:str = "1414-1750-1750-1750-1750-1750-1750-1750-1750-96"
+    arch_mlp_top:str = "1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1450-1"
+    arch_sparse_feature_size:int = 96
+    arch_embedding_size:str = "555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693-555693"
+    num_indices_per_lookup:int = 8
+    num_indices_per_lookup_fixed:int = 1
     numpy_rand_seed:int = 727
-    # not used, overridden by Model.DEFAULT_NUM_BATCHES
-    num_batches:int = 200
+    weighted_pooling: str = "fixed"
+    # original number of batches: 15
+    num_batches:int = 15
+    # torchbench: these items in the original config are disabled
+    # because they either handled by the framework
+    # or requires extra dependencies that we don't support yet (such as fbgemm and torch2trt_for_mlp)
+    # disable warmup
+    # warmup_step: int = 5
+    # do not support quantize, torch2trt_for_mlp or fbgemm
+    # quantize_emb_with_bit: int = 4
+    # use_fbgemm_gpu: bool = True
+    # use_gpu: bool = True
+    # inference_only: bool = True
+    # precache_ml_data: bool = True
+    # use_torch2trt_for_mlp: bool = True
+    # quantize_mlp_with_bit: int = 16
