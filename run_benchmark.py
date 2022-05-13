359b844cf5064b99212a46d4b40a40788569fb08
Xu Zhao <xzhao9@fb.com>
2022-05-13 14:28:11 -0400

    Fix userbenchmark

--- run_benchmark.py
+++ run_benchmark.py
@@ -15,10 +15,10 @@ def run():
     args, bm_args = parser.parse_known_args()
 
     try:
-        benchmark = importlib.import_module(f"benchmarks.{args.bm_name}")
+        benchmark = importlib.import_module(f"userbenchmark.{args.bm_name}")
         benchmark.run(bm_args)
     except ImportError as e:
-        print(f"Failed to import benchmark module {args.bm_name}, error: {str(e)}")
+        print(f"Failed to import user benchmark module {args.bm_name}, error: {str(e)}")
 
 if __name__ == "__main__":
     run()
