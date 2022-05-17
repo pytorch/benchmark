Fix fp16 precision and cos sim correctness (#913)
Summary:
Currently we are using fp32 as the baseline for all correctness tests.
When using `torch.allclose()` to compare the result between fp32 and torchdynamo eager+fp16, the test returns "incorrect"; on the other hand, using cosine similarity, it returns "correct".

Here is an overview of the results:

|               | torch.allclose() | cosine similarity |
|---------------|------------------|-------------------|
| fp32 baseline vs. torchdynamo-eager-fp16 | *incorrect*        | correct           |
| fp16 baseline vs. torchdynamo-eager-fp16 | correct          | correct           |

Therefore, we are using cosine similarity for all fp16 correctness tests.

Note that this is not a problem of torchdynamo - using `torch.allclose()` to compare fp32 baseline and fp16 result without torchdynamo, the result is also `incorrect`.

Pull Request resolved: https://github.com/pytorch/benchmark/pull/913

Reviewed By: frank-wei

Differential Revision: D36461801

Pulled By: xuzhao9

fbshipit-source-id: d9bf1a759768153d12774acdb5447024fbf33517

torchbenchmark/util/model.py
@@ -100,11 +100,9 @@ class BenchmarkModel(metaclass=PostInitProcessor):
             apply_opt_args(self, self.opt_args)
         # if test is eval, check correctness
         if self.device == "cuda" and self.test == "eval" and self.need_correctness_check:
-            # if fx2trt is used (either with or without dynamo), use cosine similarity
-            # instead of torch.allclose, because fp16+TensorRT is loose on accuracy
-            if self.dargs.precision == "fp16" and self.dynamo and self.opt_args.torchdynamo == "fx2trt":
-                self.correctness = correctness_check(self, cos_sim=True)
-            elif self.dargs.precision == "fp16" and (not self.dynamo) and self.opt_args.fx2trt:
+            # if fp16 is used, use cosine similarity instead of torch.allclose
+            # because cosine similarity is more relaxed
+            if self.dargs.precision == "fp16":
                 self.correctness = correctness_check(self, cos_sim=True)
             else:
                 self.correctness = correctness_check(self, cos_sim=False)