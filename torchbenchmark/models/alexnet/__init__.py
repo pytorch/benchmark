234914634815df17587c79499654c538df1240e7
Xu Zhao <xzhao9@fb.com>
2022-05-18 17:44:42 -0400

    Increase the default batch size of alexnet

--- torchbenchmark/models/alexnet/__init__.py
+++ torchbenchmark/models/alexnet/__init__.py
@@ -6,7 +6,7 @@ class Model(TorchVisionModel):
     # Train batch size: use the smallest example batch of 128 (assuming only 1 worker)
     # Source: https://arxiv.org/pdf/1404.5997.pdf
     DEFAULT_TRAIN_BSIZE = 128
-    DEFAULT_EVAL_BSIZE = 16
+    DEFAULT_EVAL_BSIZE = 128
 
     def __init__(self, test, device, jit, batch_size=None, extra_args=[]):
         super().__init__(model_name="alexnet", test=test, device=device, jit=jit,
