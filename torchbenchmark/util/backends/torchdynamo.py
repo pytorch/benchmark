75ca4de44172a3276e7b57d739a1ae5e96e3e32e
Xu Zhao <xzhao9@fb.com>
2022-05-18 13:43:42 -0700

    Patch torchdynamo to workaround dynamo#159 (#874)
    Summary:
    Patch torchdynamo to workaround https://github.com/pytorch/torchdynamo/issues/159 to measure performance in a more real settings
    Test action: https://github.com/pytorch/benchmark/actions/runs/2197225065
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/874
    
    Reviewed By: erichan1
    
    Differential Revision: D36451910
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 834b81dfbe7b673a373bbd29575c1aed40dfc4eb

--- torchbenchmark/util/backends/torchdynamo.py
+++ torchbenchmark/util/backends/torchdynamo.py
@@ -1,6 +1,7 @@
 """
 Support TorchDynamo(https://github.com/facebookresearch/torchdynamo) backends
 """
+import os
 import argparse
 import functools
 from typing import List
@@ -14,7 +15,23 @@ def parse_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', dy
     args = parser.parse_args(dyamo_args)
     return args
 
+
+def patch_torchdynamo():
+    """Patch TorchDynamo to workaround a performance issue:
+       https://github.com/facebookresearch/torchdynamo/issues/159"""
+    import patch
+    import torchdynamo
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    patch_file = os.path.join(current_dir, "torchdynamo.patch")
+    torchdynamo_dir = os.path.dirname(torchdynamo.__file__)
+    p = patch.fromfile(patch_file)
+    if not p.apply(strip=1, root=torchdynamo_dir):
+        print("Failed to patch torchdynamo. Exit.")
+        exit(1)
+
+
 def apply_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', args: argparse.Namespace, precision: str):
+    patch_torchdynamo()
     import torchdynamo
     if args.torchdynamo == "fx2trt" and precision == "fp16":
         model.add_context(functools.partial(torchdynamo.optimize, torchdynamo.optimizations.backends.fx2trt_compiler_fp16))
