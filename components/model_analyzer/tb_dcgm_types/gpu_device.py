fb0ab48243c87acac0b2ac1118716cd2e1ac92f3
FindHao <yhao24@ncsu.edu>
2022-05-24 00:48:16 +0000

    add referring about ConvertSMVer2Cores

--- components/model_analyzer/tb_dcgm_types/gpu_device.py
+++ components/model_analyzer/tb_dcgm_types/gpu_device.py
@@ -125,14 +125,8 @@ def ConvertSMVer2Cores(major, minor):
     # Returns the number of CUDA cores per multiprocessor for a given
     # Compute Capability version. There is no way to retrieve that via
     # the API, so it needs to be hard-coded.
-    # See _ConvertSMVer2Cores in helper_cuda.h in NVIDIA's CUDA Samples.
-    return {(1, 0): 8,    # Tesla
-            (1, 1): 8,
-            (1, 2): 8,
-            (1, 3): 8,
-            (2, 0): 32,   # Fermi
-            (2, 1): 48,
-            (3, 0): 192,  # Kepler
+    # Refer to https://github.com/NVIDIA/cuda-samples/blob/master/Common/helper_cuda.h
+    return {(3, 0): 192,  # Kepler
             (3, 2): 192,
             (3, 5): 192,
             (3, 7): 192,
@@ -146,6 +140,7 @@ def ConvertSMVer2Cores(major, minor):
             (7, 2): 64,
             (7, 5): 64,   # Turing
             (8, 0): 64,   # Ampere
-            (8, 6): 64,
+            (8, 6): 128,
+            (8, 7): 128
             }.get((major, minor), 0)
 
