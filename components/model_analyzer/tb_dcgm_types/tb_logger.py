be43d1b1b7b8756987fd4f45823c4ec77f746d9f
FindHao <yhao24@ncsu.edu>
2022-05-25 18:19:47 +0000

    update set_logger;  remove licence comment for fp32active; add requirements.txt

--- components/model_analyzer/tb_dcgm_types/tb_logger.py
+++ components/model_analyzer/tb_dcgm_types/tb_logger.py
@@ -5,11 +5,11 @@ import logging
 LOGGER_NAME = 'TorchBenchLogger'
 
 
-def set_logger():
+def set_logger(logger_level=logging.INFO):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger = logging.getLogger(LOGGER_NAME)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logger_level)
     logger.addHandler(handler)
     return logger
