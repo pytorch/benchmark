71488b08d571ec56d140533735fc0e67965c69ae
Xu Zhao <xzhao9@fb.com>
2022-06-01 13:37:14 -0400

    Pre-download multi30k dataset for attention model

--- torchbenchmark/models/attention_is_all_you_need_pytorch/install.py
+++ torchbenchmark/models/attention_is_all_you_need_pytorch/install.py
@@ -10,8 +10,11 @@ def spacy_download(language):
     subprocess.check_call([sys.executable, '-m', 'spacy', 'download', language])
 
 def preprocess():
+    current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+    multi30k_data_dir = os.path.join(current_dir.parent.parent, "data", ".data", "multi30k")
     root = os.path.join(str(Path(__file__).parent), ".data")
-    subprocess.check_call([sys.executable, 'preprocess.py', '-lang_src', 'de', '-lang_trg', 'en', '-share_vocab', '-save_data', os.path.join(root, 'm30k_deen_shr.pkl')])
+    subprocess.check_call([sys.executable, 'preprocess.py', '-lang_src', 'de', '-lang_trg', 'en', '-share_vocab',
+                           '-save_data', os.path.join(root, 'm30k_deen_shr.pkl'), '-data_path', multi30k_data_dir])
 
 if __name__ == '__main__':
     pip_install_requirements()
