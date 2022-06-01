cb6e804c5749930ea4333033d439aa6af8c63a43
Xu Zhao <xzhao9@fb.com>
2022-06-01 17:36:07 -0400

    Fix v1 setup

--- torchbenchmark/models/attention_is_all_you_need_pytorch/install.py
+++ torchbenchmark/models/attention_is_all_you_need_pytorch/install.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import os
+import tarfile
 from pathlib import Path
 
 
@@ -12,9 +13,16 @@ def spacy_download(language):
 
 def preprocess():
     current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+    data_dir = os.path.join(current_dir.parent.parent, "data")
+    decompress_dir = os.path.join(data_dir, ".data")
     multi30k_data_dir = os.path.join(current_dir.parent.parent, "data", ".data", "multi30k")
+    for tarball in filter(lambda x: x.endswith(".tar.gz"), os.listdir(data_dir)):
+        tarball_path = os.path.join(data_dir, tarball)
+        tar = tarfile.open(tarball_path)
+        tar.extractall(path=decompress_dir)
+        tar.close()
     root = os.path.join(str(Path(__file__).parent), ".data")
-    os.mkdir(root)
+    os.makedirs(root, exist_ok=True)
     subprocess.check_call([sys.executable, 'preprocess.py', '-lang_src', 'de', '-lang_trg', 'en', '-share_vocab',
                            '-save_data', os.path.join(root, 'm30k_deen_shr.pkl'), '-data_path', multi30k_data_dir])
 if __name__ == '__main__':
