c3ba32b677409ef6b8f1c3f742f5af40d1536ee5
Xu Zhao <xzhao9@fb.com>
2022-06-01 17:05:01 -0400

    Fix v2 workflow (#938)

--- torchbenchmark/models/attention_is_all_you_need_pytorch/install.py
+++ torchbenchmark/models/attention_is_all_you_need_pytorch/install.py
@@ -10,9 +10,12 @@ def spacy_download(language):
     subprocess.check_call([sys.executable, '-m', 'spacy', 'download', language])
 
 def preprocess():
+    current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+    multi30k_data_dir = os.path.join(current_dir.parent.parent, "data", ".data", "multi30k")
     root = os.path.join(str(Path(__file__).parent), ".data")
-    subprocess.check_call([sys.executable, 'preprocess.py', '-lang_src', 'de', '-lang_trg', 'en', '-share_vocab', '-save_data', os.path.join(root, 'm30k_deen_shr.pkl')])
-
+    os.mkdir(root)
+    subprocess.check_call([sys.executable, 'preprocess.py', '-lang_src', 'de', '-lang_trg', 'en', '-share_vocab',
+                           '-save_data', os.path.join(root, 'm30k_deen_shr.pkl'), '-data_path', multi30k_data_dir])
 if __name__ == '__main__':
     pip_install_requirements()
     spacy_download('en')
