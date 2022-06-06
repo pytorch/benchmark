1aa7f056e70113a6b25707f395c10eccc858ef38
FindHao <yhao24@ncsu.edu>
2022-06-06 18:03:30 +0000

    add groupby_wo_aggregate to group all records

--- components/model_analyzer/tb_dcgm_types/record_aggregator.py
+++ components/model_analyzer/tb_dcgm_types/record_aggregator.py
@@ -172,6 +172,28 @@ class RecordAggregator:
                     aggregated_result[record_type]
         return groupby_result
 
+    def groupby_wo_aggregate(self, record_types, groupby_criterion):
+        """
+        Similar to groupby(). But this function will return raw grouped records rather aggrated record.
+        """
+
+        field_values = {
+            record_type: set([
+                groupby_criterion(record)
+                for record in self._records[record_type]
+            ]) for record_type in record_types
+        }
+        groupby_result = defaultdict(list)
+        for record_type in record_types:
+            groupby_result[record_type] = defaultdict(list)
+            for field_value in field_values[record_type]:
+                temp_records_aggregator = self.filter_records(
+                    record_types=[record_type],
+                    filters=[lambda r: groupby_criterion(r) == field_value ])
+                groupby_result[record_type][field_value] = temp_records_aggregator.get_records()
+        return groupby_result
+
+
     def record_types(self):
         """
         Returns
