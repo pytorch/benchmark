d5dbd7666e2cc42aa38bfff9ea14db2792c49e53
Xu Zhao <xzhao9@fb.com>
2022-06-15 17:03:10 -0400

    Change default batch size to 1

--- torchbenchmark/models/detectron2_maskrcnn/__init__.py
+++ torchbenchmark/models/detectron2_maskrcnn/__init__.py
@@ -41,7 +41,7 @@ class Model(BenchmarkModel):
     task = COMPUTER_VISION.DETECTION
     model_file = os.path.join(MODEL_DIR, ".data", f"{MODEL_NAME}.pkl")
     DEFAULT_TRAIN_BSIZE = 1
-    DEFAULT_EVAL_BSIZE = 2
+    DEFAULT_EVAL_BSIZE = 1
 
     def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
         super().__init__(test=test, device=device, jit=jit, batch_size=batch_size, extra_args=extra_args)
