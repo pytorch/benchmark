26ea06203dbe2006edab166b663f80dc94fdd969
Xu Zhao <xzhao9@fb.com>
2022-06-17 12:31:45 -0400

    Changed confg py

--- torchbenchmark/models/FewShot_ProtoNets/config.py
+++ torchbenchmark/models/FewShot_ProtoNets/config.py
@@ -5,8 +5,7 @@ PATH = os.path.dirname(os.path.realpath(__file__))
 
 
 root = str(Path(__file__).parent.parent)
-DATA_PATH = os.path.join(root, "data", ".data", "omiglot_minimal")
-
+DATA_PATH = os.path.join(root, "data", ".data", "omniglot_minimal")
 EPSILON = 1e-8
 
 if DATA_PATH is None or not os.path.exists(DATA_PATH):
