a528b1bbf3a791d8782e5a3bd8f81840ec2e80ae
Will Constable <whc@fb.com>
2022-06-21 18:47:55 +0000

    Separate distributed component requirements

--- install.py
+++ install.py
@@ -36,12 +36,12 @@ def decompress_input():
         tar.close()
         print("OK")
 
-def pip_install_requirements():
+def pip_install_requirements(requirements_txt="requirements.txt"):
     if not _test_https():
         print(proxy_suggestion)
         sys.exit(-1)
     try:
-        subprocess.run([sys.executable, '-m', 'pip', 'install', '-q', '-r', 'requirements.txt'],
+        subprocess.run([sys.executable, '-m', 'pip', 'install', '-q', '-r', requirements_txt],
                         check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         return (False, e.output)
@@ -55,6 +55,7 @@ if __name__ == '__main__':
                         help="Specify one or more models to install. If not set, install all models.")
     parser.add_argument("--continue_on_fail", action="store_true")
     parser.add_argument("--verbose", "-v", action="store_true")
+    parser.add_argument("--component", choices=["distributed"], help="Install requirements for optional components.")
     args = parser.parse_args()
 
     os.chdir(os.path.realpath(os.path.dirname(__file__)))
@@ -78,6 +79,14 @@ if __name__ == '__main__':
         print(errmsg)
         sys.exit(-1)
     decompress_input()
+
+    if args.component == "distributed":
+        success, errmsg = pip_install_requirements(requirements_txt="torchbenchmark/util/distributed/requirements.txt")
+        if not success:
+            print("Failed to install torchbenchmark requirements:")
+            print(errmsg)
+            if not args.continue_on_fail:
+                sys.exit(-1)
 
     success, errmsg = pip_install_requirements()
     if not success:
