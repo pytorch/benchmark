5f28f2c76a5a6d65dc2a9c59b72914bc31bcf91b
Xu Zhao <xzhao9@fb.com>
2022-06-28 19:20:58 -0400

    Add back fcos r 50 fpn

--- torchbenchmark/util/framework/detectron2/config.py
+++ torchbenchmark/util/framework/detectron2/config.py
@@ -2,7 +2,7 @@ import argparse
 
 def parse_tb_args(args):
     parser = argparse.ArgumentParser()
-    # default resolution: 800x1344
+    # default resolution: 800x1333
     parser.add_argument("--resize", choices=["default", "448x608"], default="default", help="Resize the image to specified size")
     args, unknown_args = parser.parse_known_args(args)
     return args, unknown_args
