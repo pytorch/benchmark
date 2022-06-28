0c4f3399fdb3ba97a3e2e62b03ea7175adc6c47b
Xu Zhao <xzhao9@fb.com>
2022-06-28 13:43:20 -0400

    Add fcos model

--- torchbenchmark/models/detectron2_fcos_r_50_fpn/__init__.py
+++ torchbenchmark/models/detectron2_fcos_r_50_fpn/__init__.py
@@ -10,5 +10,5 @@ class Model(Detectron2Model):
     model_file = os.path.join(MODEL_DIR, ".data", f"{MODEL_NAME}.pkl")
 
     def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
-        super().__init__(variant="COCO-InstanceSegmentation/mask_rcnn_R_50_C4_1x.yaml", test=test, device=device,
+        super().__init__(variant="COCO-Detection/fcos_R_50_FPN_1x.py", test=test, device=device,
                          jit=jit, batch_size=batch_size, extra_args=extra_args)
