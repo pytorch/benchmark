9a14a23d4ecf015fbca21ea078ce79277a0ba790
Xu Zhao <xzhao9@fb.com>
2022-06-29 09:37:43 -0700

    Add torchdynamo cudagraph test (#993)
    Summary: Pull Request resolved: https://github.com/pytorch/benchmark/pull/993
    
    Reviewed By: frank-wei
    
    Differential Revision: D37497790
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 5e16eb6db52b9cb40cc2711057f5cebd3f094b2b

--- torchbenchmark/util/model.py
+++ torchbenchmark/util/model.py
@@ -108,7 +108,7 @@ class BenchmarkModel(metaclass=PostInitProcessor):
             # in this case, use more relaxed cosine similarity instead of torch.allclose
             # for correctness testing
             # see: https://github.com/pytorch/torchdynamo/pull/438
-            if self.dargs.precision == "fp16" or self.dynamo == "fx2trt" or self.opt_args.fx2trt:
+            if self.dargs.precision == "fp16" or (self.dynamo and self.opt_args.torchdynamo == "fx2trt") or (not self.dynamo and self.opt_args.fx2trt):
                 self.correctness = correctness_check(self, cos_sim=True, deepcopy=self.DEEPCOPY)
             else:
                 self.correctness = correctness_check(self, cos_sim=False, deepcopy=self.DEEPCOPY)
