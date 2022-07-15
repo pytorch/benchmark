33196c26e7eeb2c1529aca1c608d7c7bc971033f
FindHao <yhao24@ncsu.edu>
2022-07-15 17:15:24 +0000

    check PATCH_DIR and patch_file_fullpath

--- torchbenchmark/util/framework/huggingface/patch_hf.py
+++ torchbenchmark/util/framework/huggingface/patch_hf.py
@@ -16,17 +16,20 @@ def cache_model(name: str):
     model_ctor.from_config(model_config)
 
 def patch_transformers():
-    pass
-    # import transformers
-    # transformers_dir = os.path.dirname(transformers.__file__)
-    # for patch_file in os.listdir(PATCH_DIR):
-    #     patch_file_fullpatch = os.path.join(PATCH_DIR, patch_file)
-    #     try:
-    #         subprocess.check_output(["patch", "-p1", "--forward", "-i", patch_file_fullpatch, "-r", "/tmp/rej"], cwd=transformers_dir)
-    #     except subprocess.SubprocessError as e:
-    #         output_str = str(e.output)
-    #         if "previously applied" in output_str:
-    #             return
-    #         else:
-    #             print(str(output_str))
-    #             sys.exit(1)
+    import transformers
+    transformers_dir = os.path.dirname(transformers.__file__)
+    if not os.path.exists(PATCH_DIR):
+        return
+    for patch_file in os.listdir(PATCH_DIR):
+        patch_file_fullpatch = os.path.join(PATCH_DIR, patch_file)
+        if not patch_file_fullpatch.endswith(".patch"):
+            continue
+        try:
+            subprocess.check_output(["patch", "-p1", "--forward", "-i", patch_file_fullpatch, "-r", "/tmp/rej"], cwd=transformers_dir)
+        except subprocess.SubprocessError as e:
+            output_str = str(e.output)
+            if "previously applied" in output_str:
+                return
+            else:
+                print(str(output_str))
+                sys.exit(1)
