dacf4247c4f1b7b717f960af084edffdd31b77b1
Will Constable <whc@fb.com>
2022-07-22 01:19:18 +0000

    construct DS examples inside getter to avoid always constructing them

--- torchbenchmark/util/framework/huggingface/model_factory.py
+++ torchbenchmark/util/framework/huggingface/model_factory.py
@@ -2,6 +2,7 @@ import math
 import random
 import torch
 from torch import optim
+from torch.jit.frontend import NotSupportedError
 from torchbenchmark.util.model import BenchmarkModel
 import transformers
 from transformers import AutoConfig, ReformerConfig, BigBirdConfig, BertConfig
@@ -57,22 +58,14 @@ class HuggingFaceModel(BenchmarkModel):
         self.model = class_ctor.from_config(config).to(device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
 
+        # populate these on-demand to avoid wasting memory when not used
+        self.vocab_size = config.vocab_size
+        self.dynamic_example_inputs = None
+
         if test == "train":
             input_ids = torch.randint(0, config.vocab_size, (self.batch_size, self.max_length)).to(device)
             decoder_ids = torch.randint(0, config.vocab_size, (self.batch_size, self.max_length)).to(device)
             self.example_inputs = {'input_ids': input_ids, 'labels': decoder_ids}
-
-            nbuckets = 8
-            nsamples = 32
-            n = int(math.log2(self.max_length))
-            buckets = [2**n for n in range(n - nbuckets, n)]
-            self.dynamic_example_inputs = [
-                {
-                    'input_ids': torch.randint(0, config.vocab_size, (self.batch_size, bucket_len)).to(device),
-                    'labels': torch.randint(0, config.vocab_size, (self.batch_size, bucket_len)).to(device)}
-                for bucket_len in random.choices(buckets, k=nsamples)
-            ]
-
             self.model.train()
         elif test == "eval":
             # Cut the length of sentence when running on CPU, to reduce test time
@@ -92,12 +85,23 @@ class HuggingFaceModel(BenchmarkModel):
         return self.model, (self.example_inputs["input_ids"], )
 
     def get_dynamic_shapes_module(self):
+        if self.dynamic_example_inputs is None:
+            nbuckets = 8
+            nsamples = 32
+            n = int(math.log2(self.max_length))
+            buckets = [2**n for n in range(n - nbuckets, n)]
+            self.dynamic_example_inputs = [
+                {
+                    'input_ids': torch.randint(0, self.vocab_size, (self.batch_size, bucket_len)).to(self.device),
+                    'labels': torch.randint(0, self.vocab_size, (self.batch_size, bucket_len)).to(self.device)}
+                for bucket_len in random.choices(buckets, k=nsamples)
+            ]
+
         if class_models[self.name][3] == 'AutoModelForSeq2SeqLM':
-            k = 'labels' if self.test == 'train' else 'decoder_input_ids'
-            return ArgsToKwargsWrapper(self.model), (
-                    self.example_inputs['input_ids'], self.example_inputs[k])
+            raise NotSupportedError("Not yet supported")
+
         # TODO(whc) why is labels not passed through?
-        return self.model, ([i['input_ids'] for i in self.dynamic_example_inputs], )
+        return self.model, [(i['input_ids'],) for i in self.dynamic_example_inputs]
 
     def enable_fp16_half(self):
         self.model = self.model.half()
