80947f0839e1c866a4c5f08695cde67e0710bb27
Xu Zhao <xzhao9@fb.com>
2022-07-25 21:46:04 -0400

    Improve jit backend code

--- torchbenchmark/util/backends/jit.py
+++ torchbenchmark/util/backends/jit.py
@@ -13,17 +13,17 @@ def parse_torchscript_args(args) -> argparse.Namespace:
 
 @create_backend
 def torchscript(model: 'torchbenchmark.util.model.BenchmarkModel', backend_args: List[str]):
-    # customized jit callback function
     model.jit = True
     args = parse_torchscript_args(backend_args)
+    # customized jit callback function
     if hasattr(model, 'jit_callback'):
         model.jit_callback()
-        return
-    module, example_inputs = model.get_module()
-    if hasattr(torch.jit, '_script_pdt'):
-        module = torch.jit._script_pdt(module, example_inputs=[example_inputs, ])
     else:
-        module = torch.jit.script(module, example_inputs=[example_inputs, ])
+        module, example_inputs = model.get_module()
+        if hasattr(torch.jit, '_script_pdt'):
+            module = torch.jit._script_pdt(module, example_inputs=[example_inputs, ])
+        else:
+            module = torch.jit.script(module, example_inputs=[example_inputs, ])
     if model.test == "eval" and not args.no_ofi:
         module = torch.jit.optimize_for_inference(module)
     model.set_module(module)
