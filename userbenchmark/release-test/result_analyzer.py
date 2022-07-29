0dbaebb76f7b3316fd6a1756350a7753e0efe396
Xu Zhao <xzhao9@fb.com>
2022-07-28 23:04:08 -0400

    Working on analyzing the result

--- userbenchmark/release-test/result_analyzer.py
+++ userbenchmark/release-test/result_analyzer.py
@@ -3,6 +3,15 @@ from pathlib import Path
 def get_run_keys(work_dir: Path):
     pass
 
-def analyze(work_dir: Path):
-    # get test keys (directory starting with "pytorch-")
+def get_workloads(run_key_dir: Path):
     pass
+
+def analyze_workload(workload_dir: Path, workload_name: str):
+    pass
+
+def generate_result(workload_results):
+    pass
+
+def analyze(work_dir: Path):
+    # get base_args (directory starting with "pytorch-")
+    return None
