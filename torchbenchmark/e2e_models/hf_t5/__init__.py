7c3090b10f9a7cadd43f84affa554823ea2b6db7
Eric Han <erichan1@fb.com>
2022-08-17 20:15:58 +0000

    add first working version of T5 and wmt16 dataset for T5 translation training

---
+++ torchbenchmark/e2e_models/hf_t5/__init__.py
@@ -0,0 +1,392 @@
+from accelerate.utils.dataclasses import DeepSpeedPlugin
+import torch
+import math
+import os
+from pathlib import Path
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data import DataLoader
+from torchbenchmark.util.e2emodel import E2EBenchmarkModel
+from torchbenchmark.tasks import NLP
+from datasets import load_metric
+from accelerate import Accelerator
+from transformers import (
+    AutoConfig,
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    DataCollatorForSeq2Seq,
+    default_data_collator,
+    get_scheduler,
+    MBartTokenizer,
+    MBartTokenizerFast
+)
+from typing import Optional
+from torchbenchmark.util.framework.transformers.translation.dataset import prep_dataset, preprocess_dataset
+from torchbenchmark.util.framework.transformers.translation.args import parse_args, parse_torchbench_args
+
+# setup environment variable
+CURRENT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
+
+class Model(E2EBenchmarkModel):
+    task = NLP.TRANSLATION
+    DEFAULT_TRAIN_BSIZE: int = 32
+    DEFAULT_EVAL_BSIZE: int = 1
+
+    def __init__(self, test, batch_size=None, extra_args=[]):
+        super().__init__(test=test, batch_size=batch_size, extra_args=extra_args)
+        # TODO: currently only support 1 GPU device
+        self.device = "cuda"
+        self.device_num = 1
+        # Parse the extra arguments
+        self.tb_args = parse_torchbench_args(extra_args)
+        torch.manual_seed(1337)
+        torch.backends.cudnn.deterministic = False
+        torch.backends.cudnn.benchmark = True
+
+        # Parameters
+        model_name = "t5-base"
+        dataset_name = "wmt16"
+        dataset_config_name = "ro-en"
+        source_lang = "en" 
+        target_lang = "ro" 
+        source_prefix = "translate English to Romanian: "
+        max_source_length = "1024"
+        max_target_length = "128"
+        learning_rate = "2e-5"
+        num_train_epochs = "1"
+        max_train_steps = "1000" # overrides num_train_epochs
+        # this benchmark runs on a single GPU
+        cuda_visible_devices = "0"
+        output_dir = os.path.join(CURRENT_DIR, ".output")
+        os.environ["CUDA_VISIBLE_DEVICES"] = cuda_visible_devices
+        in_arg = ["--model_name_or_path", model_name, 
+                  "--dataset_name", dataset_name,
+                  "--dataset_config_name", dataset_config_name,
+                  "--source_lang", source_lang,
+                  "--target_lang", target_lang,
+                  "--source_prefix", source_prefix,
+                  "--max_source_length", max_source_length,
+                  "--max_target_length", max_target_length,
+                  "--per_device_train_batch_size", str(self.batch_size), 
+                  "--per_device_eval_batch_size", str(self.batch_size),
+                  "--learning_rate", learning_rate,
+                  "--num_train_epochs", num_train_epochs,
+                  "--max_train_steps", max_train_steps,
+                  "--output_dir", output_dir]
+        hf_args = parse_args(in_arg)
+
+        # ideally we don't modify the model code directly, but attaching deepspeed
+        # must be done before self.prep initialiazes accelerator.
+        if self.tb_args.distributed == "deepspeed":
+            zero_opt_cfg = {
+                "zero_optimization": {
+                    "stage": 1,
+                    "reduce_bucket_size": 2e8,
+                    "overlap_comm": True,
+                    "contiguous_gradients": False
+                }
+            }
+            hf_args.deepspeed_plugin = DeepSpeedPlugin()
+            hf_args.deepspeed_plugin.deepspeed_config.update(zero_opt_cfg)
+        elif self.tb_args.distributed == "ddp":
+            hf_args.apply_ddp = True
+        elif self.tb_args.distributed == "none":
+            hf_args.apply_ddp = False
+        else:
+            raise RuntimeError(f"Unsupported distributed scheme {self.tb_args.distributed} for model hf_t5")
+
+        # setup other members
+        self.prep(hf_args)
+        if test == "train":
+            self.num_examples = len(self.train_dataloader) * self.batch_size
+        elif test == "eval":
+            self.num_examples = len(self.eval_dataloader) * self.batch_size
+    
+    def prep(self, hf_args):
+        # Sending telemetry. Tracking the example usage helps us better allocate resources to maintain them. The
+        # information sent is the one passed as arguments along with your Python/PyTorch versions.
+        # send_example_telemetry("run_translation_no_trainer", args) # comment out for simplicity
+
+        # Initialize the accelerator. We will let the accelerator handle device placement for us in this example.
+        # If we're using tracking, we also need to initialize it here and it will by default pick up all supported trackers
+        # in the environment
+        # Initialize the accelerator. We will let the accelerator handle device placement for us in this example.
+        if hasattr(hf_args, "deepspeed_plugin"):
+            # Note: self.tb_args.fp16 could be renamed to better clarify its meaning
+            assert self.tb_args.fp16=="amp", "deepspeed is only supported with bf16/amp enabled"
+            accelerator = Accelerator(deepspeed_plugin=hf_args.deepspeed_plugin, mixed_precision='bf16')
+        else:
+            accelerator = Accelerator(mixed_precision='fp16' if self.tb_args.fp16=='amp' else 'no')
+
+        # Handle the repository creation
+        if accelerator.is_main_process:
+            if hf_args.output_dir is not None:
+                os.makedirs(hf_args.output_dir, exist_ok=True)
+        accelerator.wait_for_everyone()
+
+        raw_datasets = prep_dataset(hf_args)
+
+        # Load pretrained model and tokenizer
+        #
+        # In distributed training, the .from_pretrained methods guarantee that only one local process can concurrently
+        # download model & vocab.
+        if hf_args.config_name:
+            config = AutoConfig.from_pretrained(hf_args.config_name)
+        elif hf_args.model_name_or_path:
+            config = AutoConfig.from_pretrained(hf_args.model_name_or_path)
+        # else: # TODO: comment out for simplicity, but probably want this.
+        #     config = CONFIG_MAPPING[hf_args.model_type]()
+        #     logger.warning("You are instantiating a new config instance from scratch.")
+
+        if hf_args.tokenizer_name:
+            tokenizer = AutoTokenizer.from_pretrained(hf_args.tokenizer_name, use_fast=not hf_args.use_slow_tokenizer)
+        elif hf_args.model_name_or_path:
+            tokenizer = AutoTokenizer.from_pretrained(hf_args.model_name_or_path, use_fast=not hf_args.use_slow_tokenizer)
+        else:
+            raise ValueError(
+                "You are instantiating a new tokenizer from scratch. This is not supported by this script."
+                "You can do it from another script, save it, and load it from here, using --tokenizer_name."
+            )
+
+        if hf_args.model_name_or_path:
+            model = AutoModelForSeq2SeqLM.from_pretrained(
+                hf_args.model_name_or_path,
+                from_tf=bool(".ckpt" in hf_args.model_name_or_path),
+                config=config,
+            )
+        else:
+            # logger.info("Training new model from scratch")
+            model = AutoModelForSeq2SeqLM.from_config(config)
+
+        model.resize_token_embeddings(len(tokenizer))
+
+        # Set decoder_start_token_id
+        if model.config.decoder_start_token_id is None and isinstance(tokenizer, (MBartTokenizer, MBartTokenizerFast)):
+            assert (
+                hf_args.target_lang is not None and hf_args.source_lang is not None
+            ), "mBart requires --target_lang and --source_lang"
+            if isinstance(tokenizer, MBartTokenizer):
+                model.config.decoder_start_token_id = tokenizer.lang_code_to_id[hf_args.target_lang]
+            else:
+                model.config.decoder_start_token_id = tokenizer.convert_tokens_to_ids(hf_args.target_lang)
+
+        if model.config.decoder_start_token_id is None:
+            raise ValueError("Make sure that `config.decoder_start_token_id` is correctly defined")
+
+        # For translation we set the codes of our source and target languages (only useful for mBART, the others will
+        # ignore those attributes).
+        if isinstance(tokenizer, (MBartTokenizer, MBartTokenizerFast)):
+            if hf_args.source_lang is not None:
+                tokenizer.src_lang = hf_args.source_lang
+            if hf_args.target_lang is not None:
+                tokenizer.tgt_lang = hf_args.target_lang
+
+        prefix = hf_args.source_prefix if hf_args.source_prefix is not None else ""
+
+        train_dataset, eval_dataset = preprocess_dataset(hf_args, raw_datasets, tokenizer, prefix, accelerator)
+
+        # # Log a few random samples from the training set:
+        # for index in random.sample(range(len(train_dataset)), 3):
+        #     logger.info(f"Sample {index} of the training set: {train_dataset[index]}.")
+            
+        # DataLoaders creation:
+        label_pad_token_id = -100 if hf_args.ignore_pad_token_for_loss else tokenizer.pad_token_id
+        if hf_args.pad_to_max_length:
+            # If padding was already done ot max length, we use the default data collator that will just convert everything
+            # to tensors.
+            self.data_collator = default_data_collator
+        else:
+            # Otherwise, `DataCollatorWithPadding` will apply dynamic padding for us (by padding to the maximum length of
+            # the samples passed). When using mixed precision, we add `pad_to_multiple_of=8` to pad all tensors to multiple
+            # of 8s, which will enable the use of Tensor Cores on NVIDIA hardware with compute capability >= 7.5 (Volta).
+            self.data_collator = DataCollatorForSeq2Seq(
+                tokenizer,
+                model=model,
+                label_pad_token_id=label_pad_token_id,
+                pad_to_multiple_of=8 if accelerator.use_fp16 else None,
+            )
+
+        train_dataloader = DataLoader(
+            train_dataset, shuffle=True, collate_fn=self.data_collator, batch_size=hf_args.per_device_train_batch_size)
+        eval_dataloader = DataLoader(eval_dataset, collate_fn=self.data_collator, batch_size=hf_args.per_device_eval_batch_size)
+
+        # Optimizer
+        # Split weights in two groups, one with weight decay and the other not.
+        no_decay = ["bias", "LayerNorm.weight", "layer_norm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+                "weight_decay": hf_args.weight_decay,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+                "weight_decay": 0.0,
+            },
+        ]
+        optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=hf_args.learning_rate)
+
+        # Scheduler and math around the number of training steps.
+        overrode_max_train_steps = False
+        num_update_steps_per_epoch = math.ceil(len(train_dataloader) / hf_args.gradient_accumulation_steps)
+        if hf_args.max_train_steps is None:
+            hf_args.max_train_steps = hf_args.num_train_epochs * num_update_steps_per_epoch
+            overrode_max_train_steps = True
+
+        lr_scheduler = get_scheduler(
+            name=hf_args.lr_scheduler_type,
+            optimizer=optimizer,
+            num_warmup_steps=hf_args.num_warmup_steps,
+            num_training_steps=hf_args.max_train_steps,
+        )
+
+        # Prepare everything with our `accelerator`.
+        model, optimizer, train_dataloader, eval_dataloader, lr_scheduler = accelerator.prepare(
+            model, optimizer, train_dataloader, eval_dataloader, lr_scheduler
+        )
+
+        # We need to recalculate our total training steps as the size of the training dataloader may have changed.
+        num_update_steps_per_epoch = math.ceil(len(train_dataloader) / hf_args.gradient_accumulation_steps)
+        if overrode_max_train_steps:
+            hf_args.max_train_steps = hf_args.num_train_epochs * num_update_steps_per_epoch
+        # Afterwards we recalculate our number of training epochs
+        hf_args.num_train_epochs = math.ceil(hf_args.max_train_steps / num_update_steps_per_epoch)
+        # TODO: not doing checkpointing
+        # # Figure out how many steps we should save the Accelerator states
+        # if hasattr(hf_args.checkpointing_steps, "isdigit"):
+        #     checkpointing_steps = hf_args.checkpointing_steps
+        #     if hf_args.checkpointing_steps.isdigit():
+        #         checkpointing_steps = int(hf_args.checkpointing_steps)
+        # else:
+        #     checkpointing_steps = None
+        
+        # TODO: commented out tracking
+        # # We need to initialize the trackers we use, and also store our configuration.
+        # # We initialize the trackers only on main process because `accelerator.log`
+        # # only logs on main process and we don't want empty logs/runs on other processes.
+        # if hf_args.with_tracking:
+        #     if accelerator.is_main_process:
+        #         experiment_config = vars(hf_args)
+        #         # TensorBoard cannot log Enums, need the raw value
+        #         experiment_config["lr_scheduler_type"] = experiment_config["lr_scheduler_type"].value
+        #         accelerator.init_trackers("translation_no_trainer", experiment_config)
+
+        # metric = evaluate.load("sacrebleu") # TODO bring this back, potentially w/o evaluate library?
+
+        # Setup class members
+        self.hf_args = hf_args
+        self.model = model
+        self.optimizer = optimizer
+        self.train_dataloader = train_dataloader
+        self.eval_dataloader = eval_dataloader
+        self.lr_scheduler = lr_scheduler
+        self.accelerator = accelerator
+
+        if hf_args.apply_ddp:
+            local_rank = int(os.getenv("LOCAL_RANK", -1))
+            self.model = DDP(
+                self.model,
+                device_ids=[local_rank],
+                # If buffer broadcast is necessary, specific optimizations might be
+                # necessary to optimize performance. Disable it by default.
+                broadcast_buffers=False,
+                # Set gradient as bucket view to avoid unnecessary copies
+                gradient_as_bucket_view=True,
+                # TODO: tune bucket_cap_mb
+                static_graph=True,
+            )
+
+    def train(self):
+        completed_steps = 0
+        for epoch in range(self.hf_args.num_train_epochs):
+            self.model.train()
+            for step, batch in enumerate(self.train_dataloader):
+                outputs = self.model(**batch)
+                loss = outputs.loss
+                loss = loss / self.hf_args.gradient_accumulation_steps
+                self.accelerator.backward(loss)
+                if step % self.hf_args.gradient_accumulation_steps == 0 or step == len(self.train_dataloader) - 1:
+                    self.optimizer.step()
+                    self.lr_scheduler.step()
+                    self.optimizer.zero_grad()
+                    completed_steps += 1
+                if completed_steps >= self.hf_args.max_train_steps:
+                    break
+        # self.eval2() # TODO: bring back eval
+
+    def eval(self) -> Optional[dict]:
+        self.model.eval()
+        for _step, batch in enumerate(self.eval_dataloader):
+            outputs = self.model(**batch)
+            predictions = outputs.logits.argmax(dim=-1) if not self.is_regression else outputs.logits.squeeze()
+            self.metric.add_batch(
+                    predictions=self.accelerator.gather(predictions),
+                    references=self.accelerator.gather(batch["labels"]),
+                )
+        eval_metric = self.metric.compute()
+        return eval_metric
+    
+    def eval2(self):
+        self.model.eval()
+
+        if args.val_max_target_length is None:
+            args.val_max_target_length = args.max_target_length
+
+        gen_kwargs = {
+            "max_length": args.val_max_target_length if args is not None else config.max_length,
+            "num_beams": args.num_beams,
+        }
+        samples_seen = 0
+        for step, batch in enumerate(self.eval_dataloader):
+            with torch.no_grad():
+                generated_tokens = self.accelerator.unwrap_model(self.model).generate(
+                    batch["input_ids"],
+                    attention_mask=batch["attention_mask"],
+                    **gen_kwargs,
+                )
+
+                generated_tokens = self.accelerator.pad_across_processes(
+                    generated_tokens, dim=1, pad_index=tokenizer.pad_token_id
+                )
+                labels = batch["labels"]
+                if not args.pad_to_max_length:
+                    # If we did not pad to max length, we need to pad the labels too
+                    labels = accelerator.pad_across_processes(batch["labels"], dim=1, pad_index=tokenizer.pad_token_id)
+
+                generated_tokens = self.accelerator.gather(generated_tokens).cpu().numpy()
+                labels = self.accelerator.gather(labels).cpu().numpy()
+
+                if args.ignore_pad_token_for_loss:
+                    # Replace -100 in the labels as we can't decode them.
+                    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+
+                decoded_preds = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+                decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+                decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+
+                # If we are in a multiprocess environment, the last batch has duplicates
+                if accelerator.num_processes > 1:
+                    if step == len(eval_dataloader) - 1:
+                        decoded_preds = decoded_preds[: len(eval_dataloader.dataset) - samples_seen]
+                        decoded_labels = decoded_labels[: len(eval_dataloader.dataset) - samples_seen]
+                    else:
+                        samples_seen += len(decoded_labels)
+
+                self.metric.add_batch(predictions=decoded_preds, references=decoded_labels)
+        eval_metric = self.metric.compute()
+        logger.info({"bleu": eval_metric["score"]})
+
+    def next_batch(self):
+        return next(iter(self.train_dataloader))
+
+    def run_forward(self, input):
+        """
+        compute model forward and return loss
+        """
+        return self.model(**input).loss
+
+    def run_backward(self, loss):
+        self.accelerator.backward(loss)
+
+    def run_optimizer_step(self):
+        self.optimizer.step()
+
