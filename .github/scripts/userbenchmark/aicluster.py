86487df9a607839a2b860acc37fa8671b0f1653e
Xu Zhao <xzhao9@fb.com>
2022-09-06 17:00:46 -0400

    Working on S3 buckets.

--- .github/scripts/userbenchmark/aicluster.py
+++ .github/scripts/userbenchmark/aicluster.py
@@ -12,7 +12,6 @@ import boto3
 import sys
 import os
 import argparse
-import botocore
 from pathlib import Path
 
 AICLUSTER_S3_BUCKET = "ossci-metrics"
@@ -42,32 +41,24 @@ class S3Client:
         self.bucket = bucket
         self.object = object
 
-    def get_s3_path(self, prefix, file_name, obj_path=False):
-        if obj_path:
-            return "/".join([self.object, prefix, file_name])
-        else:
-            return "/".join([prefix, file_name])
+    def download_file(self, key, dest_dir):
+        filename = self.get_filename_from_key(key)
+        assert filename, f"Expected non-empty filename from key {key}."
+        with open(os.path.join(dest_dir, filename), 'wb') as f:
+            self.s3.download_fileobj(self.bucket, key, f)
 
-    def download_file(self, prefix, file_name, dest_path):
-        s3_path = self.get_s3_path(prefix, file_name, obj_path=True)
-        with open(os.path.join(dest_path, file_name), 'wb') as f:
-            self.s3.download_fileobj(self.bucket, s3_path, f)
-
-    def upload_file(self, prefix, file_name):
-        s3_path = self.get_s3_path(prefix, file_name)
-        response = self.s3.upload_file(s3_path, self.bucket, self.object)
+    def upload_file(self, key, file_path):
+        response = self.s3.upload_file(file_path, self.bucket, key)
         print(f"S3 client response: {response}")
 
     def exists(self, prefix, file_name):
-        try:
-            s3_path = self.get_s3_path(prefix, file_name, obj_path=True)
-            self.s3.Object(self.bucket, s3_path).load()
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] == "404":
-                return False
-            else:
-                raise RuntimeError(f"Error when checking s3 file {file_name} exist: {e}")
-        return True
+        """Test if the key object/prefix/file_name exists in the S3 bucket.
+           If True, return the S3 object key. Return None otherwise. """
+        s3_key = f"{self.object}/{prefix}/{file_name}"
+        result = self.s3.list_objects_v2(Bucket=self.bucket, Prefix=s3_key)
+        if 'Contents' in result:
+            return s3_key
+        return None
 
     def list_directory(self, directory=None):
         """List the directory files in the S3 bucket path.
@@ -77,8 +68,9 @@ class S3Client:
         keys = filter(lambda x: not x == prefix, [e['Key'] for p in pages for e in p['Contents']])
         return list(keys)
     
-    def get_filename_from_key():
-        pass
+    def get_filename_from_key(object_key):
+        filename = object_key.split('/')[-1]
+        return filename
 
 def determine_success_today(index):
     """
@@ -95,20 +87,15 @@ def get_metrics_index(s3, benchmark_name, work_dir):
     2. if not found, create an initial one with the metrics files from S3 directory
     3. Otherwise, compare the downloaded index file with the metrics file list, update the index file, and return
     """
-    def exist_index_file(index_file_keys):
-        if not index_file_keys:
-            return None
-        for key in index_file_keys:
-            if s3.get_filename_from_key() == INDEX_FILE_NAME:
-                return key
-        return None
-    def gen_index_obj(index_file):
-        # download and load the index file if exists, otherwise, return empty object
-        pass
+    def gen_index_obj(s3, index_key):
+        "download and load the index file if exists, otherwise, return empty object."
+        if not index_key:
+            return {}
+        s3.download_file()
     def update_index_from_metrics():
         pass
-    index_file = exist_index_file(s3.list_directory(directory=benchmark_name))
-    index_obj = gen_index_obj(index_file)
+    index_key = s3.exists(prefix=benchmark_name, file_name=INDEX_FILE_NAME)
+    index_obj = gen_index_obj(index_key)
     metric_files = s3.list_directory(directory=None)
     updated_index = update_index_from_metrics(index_obj, metric_files)
     return updated_index
