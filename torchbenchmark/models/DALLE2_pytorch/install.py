35c4e71e00eea728ccf674f575c22f349a2cfdf3
Nicolas Macchioni <nmacchioni@fb.com>
2022-09-09 20:50:35 +0000

    Create patches to remove loading bars

--- torchbenchmark/models/DALLE2_pytorch/install.py
+++ torchbenchmark/models/DALLE2_pytorch/install.py
@@ -1,9 +1,26 @@
+import os
+import patch
 import subprocess
 import sys
 
+def patch_dalle2():
+    import dalle2_pytorch
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    dalle2_dir = os.path.dirname(dalle2_pytorch.__file__)
+    #dalle2_target_file = os.path.join(dalle2_dir, "dalle2_pytorch", "dalle2_pytorch.py")
+    #setup_target_file = os.path.join(dalle2_dir, "setup.py")
+    dalle2_patch = patch.fromfile(os.path.join(current_dir, "dalle2_pytorch.patch"))
+    setup_patch = patch.fromfile(os.path.join(current_dir, "setup.patch"))
+    if not dalle2_patch.apply(strip=1, root=dalle2_dir):
+        print("Failed to patch dalle2_pytorch/dalle2_pytorch.py. Exit.")
+        exit(1)
+    if not setup_patch.apply(strip=1, root=dalle2_dir):
+        print("Failed to patch setup.py. Exit.")
+        exit(1)
 
-def setup_install():
+def pip_install_requirements():
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-q', '-r', 'requirements.txt'])
 
 if __name__ == '__main__':
-    setup_install()
+    pip_install_requirements()
+    patch_dalle2()
