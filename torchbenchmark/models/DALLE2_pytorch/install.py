2b8d966678e311f68755f1a618454205ecb0017d
Nicolas Macchioni <nmacchioni@fb.com>
2022-09-09 21:20:03 +0000

    Remove setup.py patch

--- torchbenchmark/models/DALLE2_pytorch/install.py
+++ torchbenchmark/models/DALLE2_pytorch/install.py
@@ -7,15 +7,9 @@ def patch_dalle2():
     import dalle2_pytorch
     current_dir = os.path.dirname(os.path.abspath(__file__))
     dalle2_dir = os.path.dirname(dalle2_pytorch.__file__)
-    #dalle2_target_file = os.path.join(dalle2_dir, "dalle2_pytorch", "dalle2_pytorch.py")
-    #setup_target_file = os.path.join(dalle2_dir, "setup.py")
     dalle2_patch = patch.fromfile(os.path.join(current_dir, "dalle2_pytorch.patch"))
-    setup_patch = patch.fromfile(os.path.join(current_dir, "setup.patch"))
     if not dalle2_patch.apply(strip=1, root=dalle2_dir):
         print("Failed to patch dalle2_pytorch/dalle2_pytorch.py. Exit.")
-        exit(1)
-    if not setup_patch.apply(strip=1, root=dalle2_dir):
-        print("Failed to patch setup.py. Exit.")
         exit(1)
 
 def pip_install_requirements():
