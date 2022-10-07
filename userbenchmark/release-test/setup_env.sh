ae28679265d285df4a4d2989998aa1522f06bf7c
Wei Wang <weiwangmeta@meta.com>
2022-10-07 21:40:08 +0000

    restore pytorch=${PYTORCH_VERSION}

--- userbenchmark/release-test/setup_env.sh
+++ userbenchmark/release-test/setup_env.sh
@@ -31,7 +31,7 @@ conda install -y -c pytorch ${MAGMA_VERSION}
 #conda install -y pytorch=${PYTORCH_VERSION}  cudatoolkit=${CUDA_VERSION} \
 #                 -c ${PYTORCH_CHANNEL} -c conda-forge
 # weiwangmeta: hardcode for now
-conda install pytorch=1.13 pytorch-cuda=11.6 -c pytorch-test -c nvidia
+conda install pytorch=${PYTORCH_VERSION} pytorch-cuda=11.6 -c pytorch-test -c nvidia
 python -c 'import torch; print(torch.__version__); print(torch.version.git_version)'
 
 # temp workaround to buid torchvision before vision rc binary is available
