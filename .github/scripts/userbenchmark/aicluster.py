121a55843bb315c89c10fbe2fc7f193557a5fa84
Xu Zhao <xzhao9@fb.com>
2022-10-17 13:50:27 -0700

    Fix aicluster ci (#1250)
    Summary:
    Update the name of the platform to be consistent with other workflows.
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/1250
    
    Reviewed By: erichan1
    
    Differential Revision: D40436442
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 9e6fb4c755d171e58c6df2a2e8cde3a6a9af8302

--- .github/scripts/userbenchmark/aicluster.py
+++ .github/scripts/userbenchmark/aicluster.py
@@ -158,10 +158,10 @@ def upload_metrics_to_scribe(s3, benchmark_name, index, work_dir):
             metrics_path = str(work_dir.joinpath(upload_metrics).resolve())
             with open(metrics_path, "r") as mp:
                 benchmark_time, benchmark_data = process_benchmark_json(mp)
-            uploader = TorchBenchUserbenchmarkUploader()
+            uploader = TorchBenchUserbenchmarkUploader(platform_name="ai_cluster")
             # user who run the benchmark on ai cluster
             uploader.UNIX_USER = "diegosarina"
-            uploader.SUBMISSION_GROUP_GUID = "ai-cluster"
+            uploader.SUBMISSION_GROUP_GUID = "ai_cluster"
             uploader.post_userbenchmark_results(benchmark_time, benchmark_data)
             # update the index file
             index[upload_metrics]["uploaded-scribe"] = True
