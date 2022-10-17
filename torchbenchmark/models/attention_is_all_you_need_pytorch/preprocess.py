46dcb6ab08eb4143cab491426d5feb6881f9f3db
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-17 15:43:11 +0000

    Adding tarfile member sanitization to extractall()

--- torchbenchmark/models/attention_is_all_you_need_pytorch/preprocess.py
+++ torchbenchmark/models/attention_is_all_you_need_pytorch/preprocess.py
@@ -92,7 +92,26 @@ def download_and_extract(download_dir, url, src_filename, trg_filename):
 
     sys.stderr.write(f"Extracting {compressed_file}.\n")
     with tarfile.open(compressed_file, "r:gz") as corpus_tar:
-        corpus_tar.extractall(download_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(corpus_tar, download_dir)
 
     src_path = file_exist(download_dir, src_filename)
     trg_path = file_exist(download_dir, trg_filename)
