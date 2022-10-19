8072cfb11d42392b97d7880de8556947497637a2
Xu Zhao <xzhao9@fb.com>
2022-10-19 18:28:22 -0400

    Fix conda install

--- scripts/install_nightlies.sh
+++ scripts/install_nightlies.sh
@@ -4,7 +4,7 @@ set -e
 . ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
 
-conda install -y numpy requests ninja pyyaml setuptools gitpython beautifulsoup4
+conda install -y numpy requests ninja pyyaml setuptools gitpython beautifulsoup4 regex
 conda install -y -c pytorch magma-cuda116
 
 # install the most recent successfully built pytorch packages
