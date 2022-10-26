9bbf27dd34eeaf72d013fb82b7477ff94a4bb32c
Xu Zhao <i@xuzhao.net>
2022-10-26 03:43:38 +0000

    Fix build on python 3.10

--- torchbenchmark/models/attention_is_all_you_need_pytorch/install.py
+++ torchbenchmark/models/attention_is_all_you_need_pytorch/install.py
@@ -14,12 +14,12 @@ def preprocess():
     multi30k_data_dir = os.path.join(current_dir.parent.parent, "data", ".data", "multi30k")
     root = os.path.join(str(Path(__file__).parent), ".data")
     os.makedirs(root, exist_ok=True)
-    subprocess.check_call([sys.executable, 'preprocess.py', '-lang_src', 'de', '-lang_trg', 'en', '-share_vocab',
+    subprocess.check_call([sys.executable, 'preprocess.py', '-lang_src', 'de_core_news_sm', '-lang_trg', 'en_core_web_sm', '-share_vocab',
                            '-save_data', os.path.join(root, 'm30k_deen_shr.pkl'), '-data_path', multi30k_data_dir])
 
 if __name__ == '__main__':
     pip_install_requirements()
-    spacy_download('en')
-    spacy_download('de')
+    spacy_download('en_core_web_sm')
+    spacy_download('de_core_news_sm')
     # Preprocessed pkl is larger than 100MB so we cannot skip preprocess
     preprocess()
