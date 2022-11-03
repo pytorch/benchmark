4b53ff9ae26998c64c6324fb9deb819f03d6aa37
David Berard <dberard@fb.com>
2022-11-02 18:21:52 -0700

    Allow passing in batch_size as an argument to distributed trainer
    Previously there was no way to specify batch size. Now you can provide
    args.batch_size.
    
    [ghstack-poisoned]

--- torchbenchmark/util/distributed/core_model/trainer.py
+++ torchbenchmark/util/distributed/core_model/trainer.py
@@ -30,9 +30,11 @@ class Trainer():
         ]
         extra_args.extend(model_args)
 
+        batch_size = args.batch_size if hasattr(args, "batch_size") else None
+
         # create model instance after Trainer setup, so that
         # visible devices won't be revised in model constructor
-        self.benchmark: BenchmarkModel = model_class(test="train", device="cuda", batch_size=None, extra_args=extra_args)
+        self.benchmark: BenchmarkModel = model_class(test="train", device="cuda", batch_size=batch_size, extra_args=extra_args)
 
         self.rank = dist.get_rank()
 
