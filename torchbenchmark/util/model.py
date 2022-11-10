1f0d2677c6a97ed92bdc89251ec29594715f1aa7
David Berard <dberard@fb.com>
2022-11-10 05:06:17 +0000

    Update on "[ddp+dynamo] Add distributed correctness checks"
    DDP+Dynamo experiments run in different processes, and DDP also occurs after the general correctness check - so they need to be handled separately.
    
    Process:
    1) When setting up the measurements in `ddp_experiments/__init__.py`, categorize some measurements as "reference" (e.g. the eager measurements) and others as "test". Also assign a file path.
    2) For reference measurements, do an initial correctness measurement and dump the results into a file. For test measurements, load the reference measurements from the file and compare the initial correctness measurements.
    3) Make sure to run the correctness measurement on all ranks, even if we're only doing the correctness check on rank 0.
    4) Make sure to disable non-distributed correctness checks to avoid an additional iteration that might affect dynamo and eager parameters differently.
    
    Currently hf_T5, hf_T5_large, hf_Bert, hf_GPT2_large, and timm_vision_transformer are passing; ~resnet50 is failing. Still investigating the resnet50 issue.~ resnet50 is also failing correctness with `python run.py resnet50 -t train -d cuda --torchdynamo inductor`, so this isn't a DDP-specific problem.
    
    Usage:
    ```
    python userbenchmark/ddp_experiments/__init__.py --job_dir /fsx/path/to/dir/shared/across/cluster --check_correctness_distributed
    ```
    
    i.e. adding --check_correctness_distributed will add correctness checks. Note that the correctness checks here shouldn't be used if you care about performance; we modify the models with model.eval() to get rid of dropouts, so it's not representative of actual performance. Since we probably don't care about performance anyway when using this option, we also reduce the number of iterations to speed up the test time.
    
    [ghstack-poisoned]

--- torchbenchmark/util/model.py
+++ torchbenchmark/util/model.py
@@ -123,7 +123,6 @@ class BenchmarkModel(metaclass=PostInitProcessor):
     def __post__init__(self):
         should_check_correctness = check_correctness_p(self, self.opt_args, self.dargs)
         if should_check_correctness:
-            print("CORRECTNESS CHECK dberard")
             self.eager_output = stableness_check(self, cos_sim=False, deepcopy=self.DEEPCOPY, rounds=1)
             if isinstance(self.eager_output, Tuple):
                 self.eager_output = tuple((t.detach() if isinstance(t, torch.Tensor) else t) for t in self.eager_output)
