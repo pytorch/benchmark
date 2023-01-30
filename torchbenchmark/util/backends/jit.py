eb49327b9b4901c0a1b967b940160751f0c720d6
Xu Zhao <xzhao9@fb.com>
2023-01-30 10:29:23 -0800

    Update the backend interface (#1384)
    Summary:
    This PR updates the flow of argument parsing. It parses all the backend arguments before initializing the model.
    
    This makes model developers no need to worry about arguments in the backend.
    Later, we will move fuser and tensorrt backends to the same format.
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/1384
    
    Reviewed By: erichan1
    
    Differential Revision: D42823999
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 7a4c9abe916f2724cf9764bcd1b81180fab60be5

--- torchbenchmark/util/backends/jit.py
+++ torchbenchmark/util/backends/jit.py
@@ -4,28 +4,30 @@ import argparse
 from torchbenchmark.util.backends import create_backend
 from typing import List
 
-def parse_torchscript_args(args) -> argparse.Namespace:
+def parse_torchscript_args(args: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     # enable ofi by default
     parser.add_argument("--no-ofi", action='store_true', help="disable optimize_for_inference")
-    args = parser.parse_args(args)
-    return args
+    args, unknown_args = parser.parse_known_args(args)
+    return args, unknown_args
 
 @create_backend
 def torchscript(model: 'torchbenchmark.util.model.BenchmarkModel', backend_args: List[str]):
     model.jit = True
-    args = parse_torchscript_args(backend_args)
-    # customized jit callback function
-    if hasattr(model, 'jit_callback'):
-        if args.no_ofi:
-            raise NotImplementedError("Customized jit callback doesn't support options.")
-        model.jit_callback()
-        return
-    module, example_inputs = model.get_module()
-    if hasattr(torch.jit, '_script_pdt'):
-        module = torch.jit._script_pdt(module, example_inputs=[example_inputs, ])
-    else:
-        module = torch.jit.script(module, example_inputs=[example_inputs, ])
-    if model.test == "eval" and not args.no_ofi:
-        module = torch.jit.optimize_for_inference(module)
-    model.set_module(module)
+    backend_args, extra_args = parse_torchscript_args(backend_args)
+    def _torchscript():
+        # customized jit callback function
+        if hasattr(model, 'jit_callback'):
+            if backend_args.no_ofi:
+                raise NotImplementedError("Customized jit callback doesn't support options.")
+            model.jit_callback()
+            return
+        module, example_inputs = model.get_module()
+        if hasattr(torch.jit, '_script_pdt'):
+            module = torch.jit._script_pdt(module, example_inputs=[example_inputs, ])
+        else:
+            module = torch.jit.script(module, example_inputs=[example_inputs, ])
+        if model.test == "eval" and not backend_args.no_ofi:
+            module = torch.jit.optimize_for_inference(module)
+        model.set_module(module)
+    return _torchscript, extra_args
