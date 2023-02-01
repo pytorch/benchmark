288185ef60ed4a142e5ed9dfb3f0603163e1a6ad
Xu Zhao <xzhao9@fb.com>
2023-02-01 11:07:58 -0500

    Add cuda graph

--- torchbenchmark/util/backends/cudagraph.py
+++ torchbenchmark/util/backends/cudagraph.py
@@ -1,26 +1,24 @@
 import torch
-from typing import Tuple
+from torchbenchmark.util.backends import create_backend
+from typing import List
 
-def enable_cudagraph(model: 'torchbenchmark.util.model.BenchmarkModel', example_inputs: Tuple[torch.tensor]):
-    optimizer = model.optimizer
-    loss_fn = model.loss_fn
-    # warmup
-    s = torch.cuda.Stream()
-    s.wait_stream(torch.cuda.current_stream())
-    with torch.cuda.stream(s):
-        for _ in range(3):
-            optimizer.zero_grad(set_to_none=True)
-            y_pred = model.model(*example_inputs)
-            loss = loss_fn(y_pred, model.example_outputs)
-            loss.backward()
-            optimizer.step()
-    torch.cuda.current_stream().wait_stream(s)
-    # capture
-    g = torch.cuda.CUDAGraph()
-    optimizer.zero_grad(set_to_none=True)
-    with torch.cuda.graph(g):
-        static_y_pred = model.model(*example_inputs)
-        static_loss = loss_fn(static_y_pred, model.example_outputs)
-        static_loss.backward()
-        optimizer.step()
-    model.g = g
+WARMUP_ITER = 3
+
+@create_backend
+def cudagraph(model: 'torchbenchmark.util.model.BenchmarkModel', backend_args: List[str]):
+    def _cudagraph():
+        # warmup
+        s = torch.cuda.Stream()
+        s.wait_stream(torch.cuda.current_stream())
+        with torch.cuda.stream(s):
+            for _ in range(WARMUP_ITER):
+                model.invoke()
+        torch.cuda.current_stream().wait_stream(s)
+        # capture
+        cuda_graph = torch.cuda.CUDAGraph()
+        with torch.cuda.graph(cuda_graph):
+            model.invoke()
+        def _run_cudagraph(g=cuda_graph):
+            g.replay()
+        model.invoke = _run_cudagraph
+    return _cudagraph, backend_args
