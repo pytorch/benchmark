2fda5a4877645818c86c824dc0797feb8617a7f9
Xu Zhao <xzhao9@fb.com>
2023-02-06 19:23:41 -0500

    Use python_utils to create conda env

--- utils/python_utils.py
+++ utils/python_utils.py
@@ -12,14 +12,14 @@ PYTHON_VERSION_MAP = {
     },
 }
 
-def install_conda_env(pyver: str, name: str):
+def create_conda_env(pyver: str, name: str):
     command = [ "conda", "create", "-n", name, f"python={pyver}" ]
     subprocess.check_call(command)
  
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--pyver", type=str, default=DEFAULT_PYTHON_VERSION, help="Specify the Python version.")
-    parser.add_argument("--install-conda-env", type=str, default=None, help="Install conda environment of the default Python version.")
+    parser.add_argument("--create-conda-env", type=str, default=None, help="Create conda environment of the default Python version.")
     args = parser.parse_args()
-    if args.install_conda_env:
-        install_conda_env(args.pyver, args.install_conda_env)
+    if args.create_conda_env:
+        create_conda_env(args.pyver, args.install_conda_env)
