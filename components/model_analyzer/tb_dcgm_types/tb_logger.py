549348221c1ccdc391f2219015230f7324913b42
FindHao <find@findhao.net>
2023-03-02 16:15:28 -0500

    fix typo

--- components/model_analyzer/tb_dcgm_types/tb_logger.py
+++ components/model_analyzer/tb_dcgm_types/tb_logger.py
@@ -5,7 +5,7 @@ import logging
 LOGGER_NAME = 'TorchBenchLogger'
 
 
-def set_logger(logger_level=logging.warning):
+def set_logger(logger_level=logging.WARNING):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
