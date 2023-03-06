fc2af708583029b5973de44fc7814593916ffc41
Xu Zhao <xzhao9@fb.com>
2023-03-06 11:00:50 -0500

    Add pix2pix files

--- torchbenchmark/util/framework/diffusers/model_factory.py
+++ torchbenchmark/util/framework/diffusers/model_factory.py
@@ -11,16 +11,26 @@ class DiffuserModel(BenchmarkModel):
         super().__init__(test, device, jit, batch_size, extra_args)
         if self.device == "cpu":
             raise NotImplementedError(f"Model {self.name} does not support CPU device.")
-        pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(name, torch_dtype=torch.float32, safety_checker=None)
+        if not self.precision == "fp16":
+            raise NotImplementedError(f"Model {self.name} only supports fp16 precision.")
+        pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(name, torch_dtype=torch.float16, safety_checker=None)
         pipe.to(self.device)
         pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
         self.pipe = pipe
+        prompt = "turn him into cyborg"
         # use the same size as the example image
         # https://raw.githubusercontent.com/timothybrooks/instruct-pix2pix/main/imgs/example.jpg
-        self.example_inputs = torch.randn(self.batch_size, 3, 128, 128).to(self.device)
+        self.example_inputs = (prompt, torch.randn(self.batch_size, 3, 128, 128).to(self.device))
+
+    def enable_fp16_half(self):
+        pass
+
+    def get_module(self):
+        return self.pipe, self.example_inputs
 
     def train(self):
         raise NotImplementedError(f"Train is not implemented for model {self.name}")
 
     def eval(self):
-        images = self.pipe("turn him into cyborg", image=self.example_inputs).images
+        images = self.pipe(*self.example_inputs).images
+        return images
