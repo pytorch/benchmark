0990ae7c7201f5d1b20f368f64d32b83ef850704
chuanqiw <chuanqi.wang@intel.com>
2023-03-07 07:46:02 -0800

    Enabled fuser for jit backend and added fuser3 for llga path (#1449)
    Summary:
    Enabled fuser for jit backend and added fuser3 for llga path
    
    Works for Roadmap https://github.com/pytorch/benchmark/issues/1293 for jit support, below is an example on CLX machine
    
    ```
    $ python run.py alexnet -d cpu -m jit    -t eval
    Running eval method from alexnet on cpu in jit mode with input batch size 128.
    CPU Total Wall Time:  70.918 milliseconds
    Correctness:                         True
    $ python run.py alexnet -d cpu -m jit  --fuser fuser0  -t eval
    Running eval method from alexnet on cpu in jit mode with input batch size 128.
    CPU Total Wall Time:  70.741 milliseconds
    Correctness:                         True
    $ python run.py alexnet -d cpu -m jit  --fuser fuser3  -t eval
    Running eval method from alexnet on cpu in jit mode with input batch size 128.
    CPU Total Wall Time:  64.179 milliseconds
    Correctness:                         True
    ```
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/1449
    
    Reviewed By: davidberard98
    
    Differential Revision: D43837782
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 313578112f1a406d42bc5d0d599e5fc20f4bfd0b

--- torchbenchmark/util/backends/jit.py
+++ torchbenchmark/util/backends/jit.py
@@ -8,6 +8,7 @@ def parse_torchscript_args(args: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     # enable ofi by default
     parser.add_argument("--no-ofi", action='store_true', help="disable optimize_for_inference")
+    parser.add_argument("--fuser", type=str, default="", choices=["fuser0", "fuser1", "fuser2", "fuser3"], help="enable fuser")
     args, unknown_args = parser.parse_known_args(args)
     return args, unknown_args
 
@@ -15,6 +16,12 @@ def parse_torchscript_args(args: List[str]) -> argparse.Namespace:
 def torchscript(model: 'torchbenchmark.util.model.BenchmarkModel', backend_args: List[str]):
     model.jit = True
     backend_args, extra_args = parse_torchscript_args(backend_args)
+    if model.device == "cpu" and backend_args.fuser == "fuser2":
+        raise NotImplementedError(f"{backend_args.fuser} only works with GPU.")
+    if model.test != "eval" and backend_args.fuser == "fuser3":
+        raise NotImplementedError(f"{backend_args.fuser} only works with eval mode.")
+    if backend_args.fuser:
+        model.add_context(lambda: torch.jit.fuser(backend_args.fuser))
     def _torchscript():
         # customized jit callback function
         if hasattr(model, 'jit_callback'):
@@ -28,6 +35,9 @@ def torchscript(model: 'torchbenchmark.util.model.BenchmarkModel', backend_args:
         else:
             module = torch.jit.script(module, example_inputs=[example_inputs, ])
         if model.test == "eval" and not backend_args.no_ofi:
-            module = torch.jit.optimize_for_inference(module)
+            if backend_args.fuser != "fuser3":
+                module = torch.jit.optimize_for_inference(module)
+            else:
+                module = torch.jit.freeze(module)
         model.set_module(module)
     return _torchscript, extra_args
