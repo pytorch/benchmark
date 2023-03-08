1952a8b415ec8289275937e5e89ceb3ed2cb9f0d
Xu Zhao <xzhao9@fb.com>
2023-03-08 10:52:01 -0500

    Fix device

--- torchbenchmark/canary_models/stable_diffusion/__init__.py
+++ torchbenchmark/canary_models/stable_diffusion/__init__.py
@@ -27,6 +27,7 @@ class Model(BenchmarkModel):
         model_id = "stabilityai/stable-diffusion-2"
         scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
         self.pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+        self.pipe.to(self.device)
         self.example_inputs = "a photo of an astronaut riding a horse on mars"
 
     def enable_fp16_half(self):
