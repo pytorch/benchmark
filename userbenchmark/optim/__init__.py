2f32d4e19c86e96ead928fd5cc8a9a4ba1f7ef6d
Jane Xu <janeyx@fb.com>
2023-03-09 22:31:47 +0000

    Making more general. Add skips

--- userbenchmark/optim/__init__.py
+++ userbenchmark/optim/__init__.py
@@ -14,17 +14,13 @@ with add_path(REPO_PATH):
     from torchbenchmark.util.experiment.instantiator import list_models
 
 
-BM_NAME = 'optim'
+BM_NAME: str = 'optim'
 
-MODEL_NAMES = list_models()
-"""
-the OG
-[
-    'BERT_pytorch',
-    # 'hf_T5_large',
-    # 'resnet18',
-]
-"""
+continue_on_error: bool = False
+
+MODEL_NAMES: List[str] = list_models()
+
+DEVICES: str = ['cuda', 'cpu']
 
 OPTIM_NAMES = [o.__name__ for o in [Adadelta, Adagrad, Adam, AdamW, Adamax, ASGD, SGD, RAdam, Rprop, RMSprop, NAdam, SparseAdam]]
 
@@ -111,7 +107,42 @@ OPTIMIZERS = [
     # (torch.optim.LBFGS, {}),
 ]
 
-devices = ['cuda', 'cpu']
+DENSE_MODELS = set()
+"""
+collected so far, but we should have something else collect it for us
+               ['BERT_pytorch', 'Background_Matting', 'DALLE2_pytorch', 'LearningToPaint', 'Super_SloMo', 'alexnet',
+                'attention_is_all_you_need_pytorch', 'dcgan', 'demucs', 'densenet121', 'detectron2_fasterrcnn_r_101_c4',
+                'detectron2_fasterrcnn_r_101_dc5', 'detectron2_fasterrcnn_r_101_fpn', 'detectron2_fasterrcnn_r_50_c4',
+                'detectron2_fasterrcnn_r_50_dc5', 'detectron2_fasterrcnn_r_50_fpn', 'detectron2_maskrcnn',
+                'detectron2_maskrcnn_r_101_c4', 'detectron2_maskrcnn_r_101_fpn', 'detectron2_maskrcnn_r_50_c4',
+                'detectron2_maskrcnn_r_50_fpn', 'fambench_xlmr', 'fastNLP_Bert', 'functorch_dp_cifar10']
+
+"""
+# Skips! Exclusions are represented by a dictionary of incompatible configs, where
+# optim => optimizer name
+# model => model name
+# func_str => func string (e.g., pt2_)
+# device => device name
+# Exclusions are general and will try to match on everything. For an exclusion
+# {'optim': 'SparseAdam', 'model': 'BERT_pytorch'}, any configuration with
+# SparseAdam on BERT_pytorch will be skipped.
+EXCLUSIONS: List[Dict[str, Any]] = [
+    # SparseAdam does not support dense gradients
+    {'optim': 'SparseAdam', 'model': m} for m in DENSE_MODELS
+] + [
+    # DALL-E 2, timm_efficientdet Not Supported on CPU
+    {'model': 'DALLE2_pytorch', 'device': 'cpu'},
+    {'model': 'timm_efficientdet', 'device': 'cpu'},
+    # FCOS train is not supported by upstream detectron2.
+    # See GH Issue: https://github.com/facebookresearch/detectron2/issues/4369.
+    {'model': 'detectron2_fcos_r_50_fpn'},
+    # moco uses DDP and DistributedDataParallel/allgather requires cuda
+    {'model': 'moco', 'device': 'cpu'},
+    # pyhpc_equation_of_state and pyhpc_isoneutral_mixing have no parameters
+    {'model': 'pyhpc_equation_of_state'},
+    {'model': 'pyhpc_isoneutral_mixing'},
+    {'model': 'pyhpc_turbulent_kinetic_energy'},
+] 
 
 # Returns clones of params and not a generator.
 def get_model_params(m) -> Any:
@@ -132,7 +163,7 @@ def optimizer_step(optimizer):
     optimizer.step()
 
 def pt2_optimizer_step(optimizer):
-    @torchdynamo.optimize("inductor")
+    @torchdynamo.optimize('inductor')
     def f():
         optimizer.step()
     f()
@@ -148,15 +179,30 @@ def defaults_to_str(defaults: Dict[str, Any]) -> str:
 def defaults_require_cuda(defaults: Dict[str, Any]) -> bool:
     return 'fused' in defaults and defaults['fused'] or 'capturable' in defaults and defaults['capturable']
 
+def is_excluded(mn: str, d: str, on: str, func_str: str) -> bool:
+    return any([('model' not in e or e['model'] == mn) and
+                ('device' not in e or e['device'] == d) and
+                ('optim' not in e or e['optim'] == on) and
+                ('funct_str' not in e or e['func_str'] == func_str) for e in EXCLUSIONS])
+    
 def run_model(modelName, device, Optim, defaults, maybe_pt2_):
     try:
         Model = load_model_by_name(modelName)   
-        params = get_model_params(Model(device=device, test='train'))
+        try: 
+            params = get_model_params(Model(device=device, test='train'))
+        except NotImplementedError:
+            params = get_model_params(Model(device=device, test='eval'))
         if Optim.__name__ == 'SGD':
             defaults['lr'] = 1e-2
+        if len(params) > 0 and params[0].layout == torch.strided and 'Sparse' in Optim.__name__:
+            # don't run Sparse optimizers on dense gradients
+            DENSE_MODELS.add(modelName)
+            return None
         optim = Optim(params, **defaults)
         generate_random_gradients(params)
         pt2_description = '' if maybe_pt2_ == '' else '(pt2) '
+
+        print(f'{datetime.datetime.now()}     {modelName}, {device}, {Optim}, {defaults_to_str(defaults)}, {maybe_pt2_}')
 
         return benchmark.Timer(
             stmt=f'{maybe_pt2_}optimizer_step(optim)',
@@ -164,37 +210,56 @@ def run_model(modelName, device, Optim, defaults, maybe_pt2_):
             sub_label=f'{modelName}, {optim.__class__.__name__}, {device}',
             description=pt2_description + ('default' if len(defaults) == 0 else defaults_to_str(defaults))
         ).blocked_autorange()
-    except Exception: 
-        with open("errors.txt", "a") as f:
-            f.write(f"{datetime.datetime.now()} {modelName}, {device}, {Optim}, {defaults_to_str(defaults)}, {maybe_pt2_}\n")
+    except Exception as e: 
+        if not continue_on_error:
+            raise e
+        with open('errors.txt', 'a') as f:
+            f.write(f'{datetime.datetime.now()} {modelName}, {device}, {Optim}, {defaults_to_str(defaults)}, {maybe_pt2_}, {e.__str__}\n')
+        return None
 
 
-def run_benchmarks(optims: str, func_strs: List[str]) -> List[float]:
+def run_benchmarks(optims: List[str], func_strs: List[str], models: List[str], devices: List[str]) -> List[float]:
     results = []
-    for mn, d, (O, defaults), func_str in itertools.product(MODEL_NAMES, devices, OPTIMIZERS, func_strs):
-        if defaults_require_cuda(defaults) and d != 'cuda':
+    optim_cfgs = [(O, defaults) for (O, defaults) in OPTIMIZERS if O.__name__ in optims]
+    for mn, d, (O, defaults), func_str in itertools.product(models, devices, optim_cfgs, func_strs):
+        if is_excluded(mn, d, O.__name__, func_str) or (defaults_require_cuda(defaults) and d != 'cuda'):
             continue
-        if O.__name__ in optims:
-            bm = run_model(mn, d, O, defaults, func_str)
-            if bm is not None:
-                results.append(bm)
+        bm = run_model(mn, d, O, defaults, func_str)
+        if bm is not None:
+            results.append(bm)
     return results
 
 
 def parse_args(args: List[str]):
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--optim", "-o",
-        nargs="*",
+        '--optims', '-o',
+        nargs='*',
         default=OPTIM_NAMES,
         choices=OPTIM_NAMES,
-        help="List of optimizers to run tests on")
+        help='List of optimizers to run tests on')
     parser.add_argument(
-        "--funcs", "-f",
-        nargs="*",
+        '--funcs', '-f',
+        nargs='*',
         default=FUNC_STRS,
         choices=FUNC_STRS,
-        help="What optimizer.step() function variations to benchmark"
+        help='What optimizer.step() function variations to benchmark'
+    )
+    parser.add_argument(
+        '--models', '-m',
+        nargs='*',
+        default=MODEL_NAMES,
+        choices=MODEL_NAMES,
+        help='List of models to run tests on')
+    parser.add_argument(
+        '--devices', '-d',
+        nargs='*',
+        default=DEVICES,
+        choices=DEVICES,
+        help='List of devices to run tests on')
+    parser.add_argument(
+        '--continue-on-error', '-c',
+        action='store_true'
     )
     args = parser.parse_args(args)
     return args
@@ -209,13 +274,16 @@ def get_metrics(results: List[torch.utils.benchmark.utils.common.Measurement]) -
 
 def run(args: List[str]):
     args = parse_args(args)
-    results = run_benchmarks(args.optim, args.funcs)
+    global continue_on_error
+    continue_on_error = args.continue_on_error
+    results = run_benchmarks(args.optims, args.funcs, args.models, args.devices)
     metrics: Dict[str, float] = get_metrics(results) 
     dump_output(BM_NAME, get_output_json(BM_NAME, metrics))
     compare = benchmark.Compare(results)
     compare.trim_significant_figures()
     compare.colorize(rowwise=True)
     compare.print()
+    print(DENSE_MODELS)
 
 if __name__ == '__main__':
     run(sys.argv[1:])
