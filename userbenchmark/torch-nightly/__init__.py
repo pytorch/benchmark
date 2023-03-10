af104af6d43158f25b4d54473f44b18231d4a0e7
Xu Zhao <xzhao9@fb.com>
2023-03-10 10:52:04 -0500

    Add flaky test.

--- userbenchmark/torch-nightly/__init__.py
+++ userbenchmark/torch-nightly/__init__.py
@@ -3,6 +3,7 @@ Run PyTorch nightly benchmarking.
 """
 import argparse
 import itertools
+import yaml
 
 from typing import List
 from ..utils import REPO_PATH, add_path, get_output_dir, get_output_json, dump_output
@@ -13,7 +14,10 @@ with add_path(REPO_PATH):
 
 BM_NAME = "torch-nightly"
 
-def generate_ootb_model_configs(devices: List[str], tests: List[str], model_names: List[str]) -> List[TorchBenchModelConfig]:
+def generate_unstable_test_list(unstable_test_config: str):
+    pass
+
+def generate_model_configs(devices: List[str], tests: List[str], model_names: List[str]) -> List[TorchBenchModelConfig]:
     """Use the default batch size and default mode."""
     if not model_names:
         model_names = list_models()
@@ -29,19 +33,21 @@ def generate_ootb_model_configs(devices: List[str], tests: List[str], model_name
     ) for device, test, model_name in cfgs]
     return result
 
+def yaml_to_
+
 def parse_str_to_list(tests: str):
     pass
 
 def parse_args(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--device", "-d", default="cuda", choices=["cuda", "cpu"], help="The name of the config to run.")
-    parser.add_argument("--test", "-t", choices=["train", "eval", "train,eval", "eval,train"], help="Tests to run.")
+    parser.add_argument("--device", "-d", default="cuda", help="Device to run.")
+    parser.add_argument("--test", "-t", default="eval", help="Tests to run.")
     parser.add_argument("--model", "-m", default=None, nargs="+", help="Only run the specifice models, model names splited with space.")
+    parser.add_argument("--filter", default=None, help="YAML config to filter unstable tests.")
     parser.add_argument("--dryrun", action="store_true", help="Dryrun the command.")
     return parser.parse_args(args)
 
 def run(args: List[str]):
     args = parse_args(args)
-    benchmark_configs = generate_ootb_model_configs(parse_str_to_list(args.devices), parse_str_to_list(args.tests), parse_str_to_list(args.models))
-    for benchmark_config in benchmark_configs:
+    if args.filter:
         pass
