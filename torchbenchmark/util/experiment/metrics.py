5a6ceb7c83c2483e513403f94175676703ac3832
Xu Zhao <xzhao9@fb.com>
2023-03-13 12:46:28 -0400

    Make memory metrics work on modeltask

--- torchbenchmark/util/experiment/metrics.py
+++ torchbenchmark/util/experiment/metrics.py
@@ -99,14 +99,12 @@ def get_model_test_metrics(model: Union[BenchmarkModel, ModelTask], metrics= [],
     latencies = None
     cpu_peak_mem = None
     gpu_peak_mem = None
+    if not (isinstance(model, BenchmarkModel) or isinstance(model, ModelTask)):
+        raise ValueError(f"Expected BenchmarkModel or ModelTask, get type: {type(model)}")
+    device = model.device if isinstance(model, BenchmarkModel) else model.get_model_attribute("device")
     if 'latencies' in metrics:
-        if isinstance(model, BenchmarkModel):
-            latencies = _get_model_test_metrics(model)
-        elif isinstance(model, ModelTask):
-            latencies = _get_model_test_metrics_isolated(model)
-        else:
-            raise ValueError(f"Expected BenchmarkModel or ModelTask, get type: {type(model)}")
+        latencies = get_latencies(model.invoke, device)
     if 'cpu_peak_mem' in metrics or 'gpu_peak_mem' in metrics:
-        cpu_peak_mem, gpu_peak_mem = get_peak_memory(model.invoke, model.device, export_metrics_file=export_metrics_file, metrics_needed=metrics, metrics_gpu_backend=metrics_gpu_backend)
+        cpu_peak_mem, gpu_peak_mem = get_peak_memory(model.invoke, device, export_metrics_file=export_metrics_file, metrics_needed=metrics, metrics_gpu_backend=metrics_gpu_backend)
     return TorchBenchModelMetrics(latencies, cpu_peak_mem, gpu_peak_mem)
     
