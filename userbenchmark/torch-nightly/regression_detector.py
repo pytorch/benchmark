850221f42549456d6e8849c5c0260398b25b4e80
Xu Zhao <xzhao9@fb.com>
2023-03-14 16:01:19 -0400

    Add stable results

--- userbenchmark/torch-nightly/regression_detector.py
+++ userbenchmark/torch-nightly/regression_detector.py
@@ -4,7 +4,7 @@ Detect if the regression happens over nightly runs.
 
 from typing import Optional
 
-def detect_regression(a, b) -> Optional[str]:
-    """Detect if result b regresses from result a. If so, return a userbenchmark arg str for bisection. Otherwise, return None."""
-    # Check the two regression results are from the same config
+def run(a, b) -> Optional[str]:
+    """Detect if result b regresses from result a. If so, return path to the regression detection result file. Otherwise, return None."""
+    # Check the two regression results are from the same platform
     return None
