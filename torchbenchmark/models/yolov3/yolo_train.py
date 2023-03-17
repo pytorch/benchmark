01b8858d4f14c35167a8887246ae1aeec2b69fde
Xu Zhao <xzhao9@fb.com>
2023-03-17 13:07:02 -0400

    Another fix of yolov3

--- torchbenchmark/models/yolov3/yolo_train.py
+++ torchbenchmark/models/yolov3/yolo_train.py
@@ -215,32 +215,58 @@ def prepare_training_loop(args):
         nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
         # load with single process
         nw = 0
-        torch.set_default_device(device)
-        dataloader = torch.utils.data.DataLoader(dataset,
-                                                batch_size=batch_size,
-                                                num_workers=nw,
-                                                shuffle=not opt.rect,  # Shuffle=True unless rectangular training is used
-                                                pin_memory=True,
-                                                collate_fn=dataset.collate_fn,
-                                                generator=torch.Generator(device))
+        try:
+            dataloader = torch.utils.data.DataLoader(dataset,
+                                                    batch_size=batch_size,
+                                                    num_workers=nw,
+                                                    shuffle=not opt.rect,  # Shuffle=True unless rectangular training is used
+                                                    pin_memory=True,
+                                                    collate_fn=dataset.collate_fn,
+                                                    generator=torch.Generator(device))
 
-        # Testloader
-        testloader = torch.utils.data.DataLoader(LoadImagesAndLabels(test_path, imgsz_test, batch_size,
-                                                                    hyp=hyp,
-                                                                    rect=True,
-                                                                    cache_images=opt.cache_images,
-                                                                    single_cls=opt.single_cls),
-                                                batch_size=batch_size,
-                                                num_workers=nw,
-                                                pin_memory=True,
-                                                collate_fn=dataset.collate_fn,
-                                                generator=torch.Generator(device))
+            # Testloader
+            testloader = torch.utils.data.DataLoader(LoadImagesAndLabels(test_path, imgsz_test, batch_size,
+                                                                        hyp=hyp,
+                                                                        rect=True,
+                                                                        cache_images=opt.cache_images,
+                                                                        single_cls=opt.single_cls),
+                                                    batch_size=batch_size,
+                                                    num_workers=nw,
+                                                    pin_memory=True,
+                                                    collate_fn=dataset.collate_fn,
+                                                    generator=torch.Generator(device))
 
-        # TorchBench: prefetch the dataloader
-        if opt.prefetch:
-            dataloader = _prefetch_loader(dataloader, size=opt.train_num_batch*batch_size, 
-                                          fields=[0, 1],
-                                          collate_fn=lambda x: x.to(device) if isinstance(x, torch.Tensor) else x)
+            # TorchBench: prefetch the dataloader
+            if opt.prefetch:
+                dataloader = _prefetch_loader(dataloader, size=opt.train_num_batch*batch_size, 
+                                            fields=[0, 1],
+                                            collate_fn=lambda x: x.to(device) if isinstance(x, torch.Tensor) else x)
+        except RuntimeError:
+            dataloader = torch.utils.data.DataLoader(dataset,
+                                        batch_size=batch_size,
+                                        num_workers=nw,
+                                        shuffle=not opt.rect,  # Shuffle=True unless rectangular training is used
+                                        pin_memory=True,
+                                        collate_fn=dataset.collate_fn,
+                                        generator=torch.Generator("cpu"))
+
+            # Testloader
+            testloader = torch.utils.data.DataLoader(LoadImagesAndLabels(test_path, imgsz_test, batch_size,
+                                                                        hyp=hyp,
+                                                                        rect=True,
+                                                                        cache_images=opt.cache_images,
+                                                                        single_cls=opt.single_cls),
+                                                    batch_size=batch_size,
+                                                    num_workers=nw,
+                                                    pin_memory=True,
+                                                    collate_fn=dataset.collate_fn,
+                                                    generator=torch.Generator("cpu"))
+
+            # TorchBench: prefetch the dataloader
+            if opt.prefetch:
+                dataloader = _prefetch_loader(dataloader, size=opt.train_num_batch*batch_size, 
+                                            fields=[0, 1],
+                                            collate_fn=lambda x: x.to(device) if isinstance(x, torch.Tensor) else x)
 
         # Model parameters
         model.nc = nc  # attach number of classes to model
