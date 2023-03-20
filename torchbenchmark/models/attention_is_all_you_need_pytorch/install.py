0568a597b243cddf83123e20871842175d7aadb9
Xu Zhao <xzhao9@fb.com>
2023-03-20 09:41:37 -0400

    Fix preprocess in attention

--- torchbenchmark/models/attention_is_all_you_need_pytorch/install.py
+++ torchbenchmark/models/attention_is_all_you_need_pytorch/install.py
@@ -13,7 +13,8 @@ def preprocess():
     current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
     multi30k_data_dir = os.path.join(current_dir.parent.parent, "data", ".data", "multi30k")
     root = os.path.join(str(Path(__file__).parent), ".data")
-    os.mkdir(root)
+    if not os.path.exists(root):
+        os.mkdir(root)
     subprocess.check_call([sys.executable, 'preprocess.py', '-lang_src', 'de', '-lang_trg', 'en', '-share_vocab',
                            '-save_data', os.path.join(root, 'm30k_deen_shr.pkl'), '-data_path', multi30k_data_dir])
 if __name__ == '__main__':
