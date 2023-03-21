c3af4324848dbd384217d27f2b23bc9effb9e0b2
Xu Zhao <xzhao9@fb.com>
2023-03-21 16:12:29 -0400

    Remove unused files

--- userbenchmark/nvfuser/regression_detector.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-
-def run(control_file: str, treatment_file: str) -> Optional[str]:
-    """Detect regression from control and treatment metrics files.
-       If detected regression, return the output file path. Otherwise, return None."""
-    print("hello")
-    return None
