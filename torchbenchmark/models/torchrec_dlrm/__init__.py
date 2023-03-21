8c786c25343f9f0faae7ba990f69110636dbafaf
Jane Xu <janeyx@fb.com>
2023-03-21 15:21:40 -0700

    Make jit a kwarg with default value False in 4 models (#1497)
    Summary:
    The superclass already has a default of False for the `jit` argument. Keep it optional.
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/1497
    
    Reviewed By: xuzhao9
    
    Differential Revision: D44268932
    
    Pulled By: janeyx99
    
    fbshipit-source-id: 0dbd7b92572870aa3dbd114052523879bc7fbf2f

--- torchbenchmark/models/torchrec_dlrm/__init__.py
+++ torchbenchmark/models/torchrec_dlrm/__init__.py
@@ -34,7 +34,7 @@ class Model(BenchmarkModel):
     DEFAULT_TRAIN_BSIZE = 1024
     DEFAULT_EVAL_BSIZE = 1024
 
-    def __init__(self, test, device, jit, batch_size=None, extra_args=[]):
+    def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
         super().__init__(test=test, device=device, jit=jit, batch_size=batch_size, extra_args=extra_args)
         args = parse_args(self.extra_args)
         backend = "nccl" if self.device == "cuda" else "gloo"
