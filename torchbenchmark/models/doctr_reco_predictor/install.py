2e8a950bf15b23bcce26f13be18356bb2916519a
Xu Zhao <xzhao9@fb.com>
2023-04-06 11:05:36 -0700

    Avoid making conda a hard dependency (#1528)
    Summary:
    After some thoughts I believe we should keep conda an optional dependency because users might want to setup their own binary libraries and environment. We will first try installing binary libraries from conda, if failed, print a warning.
    
    Fixes https://github.com/pytorch/benchmark/issues/1512.
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/1528
    
    Reviewed By: janeyx99
    
    Differential Revision: D44730545
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: d932233a6946d07d716ad522c265cdcd43e750f2

--- torchbenchmark/models/doctr_reco_predictor/install.py
+++ torchbenchmark/models/doctr_reco_predictor/install.py
@@ -1,11 +1,13 @@
 import os
+import warnings
 import subprocess
 import sys
 
 def pip_install_requirements():
-    # install deps from conda-forge
-    # model doctr_reco_predictor needs weasyprint, which needs libglib and pango
-    subprocess.check_call(["conda", "install", "-y", "expecttest", "libglib", "pango", "-c", "conda-forge"])
+    try:
+        subprocess.check_call(["conda", "install", "-y", "expecttest", "libglib", "pango", "-c", "conda-forge"])
+    except:
+        warnings.warn("The doctr_reco_predictor model requires conda binary libaries to be installed. Missing conda packages might break this model.")
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-q', '-r', 'requirements.txt'])
 
 if __name__ == '__main__':
