3ae3103ace6858377e0d9c73c7ddbb0612aa05b9
ESI-SYD <yudong.si@intel.com>
2023-04-11 11:32:06 +0800

    Fix llama eval bug

--- torchbenchmark/models/llama/__init__.py
+++ torchbenchmark/models/llama/__init__.py
@@ -11,13 +11,14 @@ import torch
 
 class Model(BenchmarkModel):
     task = NLP.LANGUAGE_MODELING
+    DEFAULT_EVAL_BSIZE = 32
     
     def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
         super().__init__(test=test, device=device, jit=jit, batch_size=batch_size, extra_args=extra_args)
-        self.model_args = ModelArgs(vocab_size=32)
+        self.model_args = ModelArgs(vocab_size=32,device=self.device)
         torch.set_default_device(self.device)
         self.model = Transformer(self.model_args).to(self.device)
-        self.example_inputs = (torch.tensor([[1, 1], [1,1]], dtype=torch.int).to(self.device), 1)
+        self.example_inputs = (torch.tensor([[self.batch_size, 1], [1,1]], dtype=torch.int).to(self.device), 1)
 
         
     def get_module(self):
