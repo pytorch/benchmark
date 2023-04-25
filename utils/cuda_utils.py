84162b4be9f893b7e385a782c17077122a90a459
atalman <atalman@fb.com>
2023-04-25 05:35:45 -0700

    Add release 2.0.1 config
    Adjust freq back to those for T4
    
    Revert "Adjust freq back to those for T4"
    
    This reverts commit 027dee0d33b056bfc118b86802e3d3c7f252e92c.
    
    set mnsit to 10 epoch
    
    Release 2.1.0 benchmark
    
    use cuda 11.8
    
    Adopt release testing for A100
    
    test
    
    Use pip instead of conda
    
    tuning
    
    test
    
    create_and_activate
    
    test
    
    tests
    
    tets
    
    test
    
    Add clean update
    
    test
    
    Additional A100 tests
    
    Adopt for T4 or A100 tests
    
    test
    
    test

--- utils/cuda_utils.py
+++ utils/cuda_utils.py
@@ -134,6 +134,10 @@ def install_torch_build_deps(cuda_version: str):
                         f"cmake={PIN_CMAKE_VERSION}"]
     cmd = ["conda", "install", "-y"] + torch_build_deps
     subprocess.check_call(cmd)
+    # conda-forge build deps
+    build_deps = [ "ffmpeg" ]
+    cmd = ["conda", "install", "-y"] + build_deps
+    subprocess.check_call(cmd)
     # pip deps
     pip_deps = [ f"numpy=={PIN_NUMPY_VERSION}" ]
     cmd = ["pip", "install"] + pip_deps
