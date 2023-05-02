8c7d4cdbbadaf94c8dbf19182b04a8a2e93ecd59
Xu Zhao <xzhao9@fb.com>
2023-05-02 09:51:38 -0700

    Skip installing pytorch nightly. (#1607)
    Summary:
    After we deploy the torchbench nightly docker, we do not need to install pytorch nightly.
    
    Docker packages are available at: https://github.com/orgs/pytorch/packages/container/package/torchbench
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/1607
    
    Reviewed By: aaronenyeshi
    
    Differential Revision: D45470524
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: b3aeceafed21be61d9e048dbc166cbaff05cb505

--- torchbenchmark/models/DALLE2_pytorch/install.py
+++ torchbenchmark/models/DALLE2_pytorch/install.py
@@ -14,6 +14,12 @@ def patch_dalle2():
 
 def pip_install_requirements():
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-q', '-r', 'requirements.txt'])
+    # DALLE2_pytorch requires embedding-reader
+    # https://github.com/lucidrains/DALLE2-pytorch/blob/00e07b7d61e21447d55e6d06d5c928cf8b67601d/setup.py#L34
+    # embedding-reader requires an old version of pandas and pyarrow
+    # https://github.com/rom1504/embedding-reader/blob/a4fd55830a502685600ed8ef07947cd1cb92b083/requirements.txt#L5
+    # So we need to reinstall a newer version of pandas and pyarrow, to be compatible with other models
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-U', 'pandas', 'pyarrow'])
 
 if __name__ == '__main__':
     pip_install_requirements()
