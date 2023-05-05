c0e46188f9ed99d073f7355433e126d435f66314
Xu Zhao <xzhao9@fb.com>
2023-05-05 17:04:44 -0400

    Source envs

--- scripts/torchbench_test.sh
+++ scripts/torchbench_test.sh
@@ -5,7 +5,13 @@ if [ -z "${CONDA_ENV}" ]; then
   exit 1
 fi
 
-conda activate ${CONDA_ENV}
+if [ -z "${SETUP_SCRIPT}" ]; then
+  echo "ERROR: SETUP_SCRIPT is not set"
+  exit 1
+fi
+
+. "${SETUP_SCRIPT}"
+conda activate "${CONDA_ENV}"
 
 parent_dir=$(dirname "$(readlink -f "$0")")/..
 cd ${parent_dir}
