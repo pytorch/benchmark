4c6e5d30aea0ce1bdd6d1e80783632269517e17c
Xu Zhao <xzhao9@fb.com>
2023-05-05 21:12:44 -0400

    Cleanup unused scripts

--- scripts/install_basics_amzn_linux_2.sh
+++
@@ -1,5 +0,0 @@
-sudo yum makecache --refresh
-sudo yum install -y git jq \
-                vim wget curl ninja-build cmake \
-                gcc kernel-headers kernel-devel kernel-source \
-                libglvnd-glx libsndfile
