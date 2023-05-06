065c3580635b7af809a2b1035d40c192455d63a0
Jane Xu <janeyx@fb.com>
2023-05-05 19:10:03 -0700

    add delta to denoise in regression detector (#1616)
    Summary:
    Use a delta of 0.07 in anticipation that we'll get a lot of cpu noise still.
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/1616
    
    Reviewed By: xuzhao9
    
    Differential Revision: D45623742
    
    Pulled By: janeyx99
    
    fbshipit-source-id: 874c9108d16b012e75446b8a6773b403542eca8f

--- userbenchmark/optim/regression_detector.py
+++ userbenchmark/optim/regression_detector.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from ..utils import TorchBenchABTestResult, TorchBenchABTestMetric
 
+DEFAULT_REGRESSION_DELTA_THRESHOLD = 0.07
+
 def run(control, treatment) -> Optional[TorchBenchABTestResult]:
     control_env = control["environ"]
     treatment_env = treatment["environ"]
@@ -11,7 +13,8 @@ def run(control, treatment) -> Optional[TorchBenchABTestResult]:
         if control_metric_name in treatment_metrics:
             treatment_metric = treatment_metrics[control_metric_name]
             delta = (treatment_metric - control_metric) / control_metric
-            details[control_metric_name] = TorchBenchABTestMetric(control=control_metric, treatment=treatment_metric, delta=delta)
+            if delta > DEFAULT_REGRESSION_DELTA_THRESHOLD:
+                details[control_metric_name] = TorchBenchABTestMetric(control=control_metric, treatment=treatment_metric, delta=delta)
     return TorchBenchABTestResult(control_env=control_env, \
                                   treatment_env=treatment_env, \
                                   details=details, \
