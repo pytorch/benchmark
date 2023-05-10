8df0ebcbcb0e637f7a1a7b60e5169b00b5ef011e
Edward Z. Yang <ezyang@meta.com>
2023-05-10 07:17:35 -0700

    Add some better GNN benchmarks from pytorch_geometric
    Unlike the ones in canaries, these work with only a source install of
    latest pytorch_geometric, without requiring C++ libraries.  I've also
    taken care to make the models jittable, so that you actually get speedup
    from torch.compile them.
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>

---
+++ torchbenchmark/models/basic_gnn_gcn/install.py
@@ -0,0 +1,4 @@
+from torchbenchmark.util.framework.gnn import install_pytorch_geometric
+
+if __name__ == '__main__':
+    install_pytorch_geometric()
---
+++ torchbenchmark/util/framework/gnn/__init__.py
@@ -0,0 +1,11 @@
+import subprocess
+import os.path
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def install_pytorch_geometric():
+    pip_install_requirements()
+
+def pip_install_requirements():
+    requirements_file = os.path.join(CURRENT_DIR, "requirements.txt")
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-q', '-r', requirements_file])
