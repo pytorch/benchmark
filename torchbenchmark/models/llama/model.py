e88010da309c109ef50d962660d73a275e9d050f
Mark Saroufim <marksaroufim@gmail.com>
2023-05-10 11:05:07 -0700

    Update model.py

--- torchbenchmark/models/llama/model.py
+++ torchbenchmark/models/llama/model.py
@@ -15,7 +15,7 @@ class ModelArgs:
     dim: int = 512
     n_layers: int = 8
     n_heads: int = 8
-    vocab_size: int = 32000
+    vocab_size: int = 32000 # this is the max vocab size supported by sentencepiece
     multiple_of: int = 256  # make SwiGLU hidden layer size multiple of large power of 2
     norm_eps: float = 1e-5
 
