497fe74a4368accf79292a925872975e6b850a27
Xu Zhao <xzhao9@fb.com>
2023-05-18 14:26:57 -0400

    Add regression detector for V3

--- userbenchmark/torch-nightly/regression_detector.py
+++ userbenchmark/torch-nightly/regression_detector.py
@@ -1,17 +1,20 @@
 from typing import Optional
 from ..utils import TorchBenchABTestResult, TorchBenchABTestMetric
 
+DEFAULT_REGRESSION_DELTA_THRESHOLD = 0.07
+
 def run(control, treatment) -> Optional[TorchBenchABTestResult]:
     control_env = control["environ"]
     treatment_env = treatment["environ"]
     control_metrics = control["metrics"]
-    treatment_metrics = control["metrics"]
+    treatment_metrics = treatment["metrics"]
     details = {}
     for metric_names in control_metrics.keys():
         control_metric = control_metrics[metric_names]
         treatment_metric = treatment_metrics[metric_names]
         delta = (treatment_metric - control_metric) / control_metric
-        details[metric_names] = TorchBenchABTestMetric(control=control_metric, treatment=treatment_metric, delta=delta)
+        if delta > DEFAULT_REGRESSION_DELTA_THRESHOLD:
+            details[metric_names] = TorchBenchABTestMetric(control=control_metric, treatment=treatment_metric, delta=delta)
     return TorchBenchABTestResult(control_env=control_env, \
                                   treatment_env=treatment_env, \
                                   details=details, \
