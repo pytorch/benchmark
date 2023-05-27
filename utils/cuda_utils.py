089000fda4369aa33fe9ad7f9de47da21a2340a3
Xu Zhao <xzhao9@fb.com>
2023-05-27 08:38:41 -0400

    Fix cuda utils build

--- utils/cuda_utils.py
+++ utils/cuda_utils.py
@@ -107,6 +107,10 @@ def install_torch_build_deps(cuda_version: str):
                         f"cmake={PIN_CMAKE_VERSION}"]
     cmd = ["conda", "install", "-y"] + torch_build_deps
     subprocess.check_call(cmd)
+    # conda-forge build deps
+    build_deps = [ "ffmpeg" ]
+    cmd = ["conda", "install", "-y", "-c", "conda-forge"] + build_deps
+    subprocess.check_call(cmd)
 
 def install_torchbench_deps():
     # torchrec_dlrm requires fbgemm_gpu, which requires gxx_linux-64=10.4.0 and sysroot_linux-64=2.17
