649e5c9dd4ac6f4092ce5c0810f6a94c9abaa858
Louis Feng <lofe@meta.com>
2023-06-21 23:25:20 -0700

    Refactor Execution Graph (EG) to Execution Trace (ET) (#1566)
    Summary:
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/1566
    
    X-link: https://github.com/facebookresearch/param/pull/72
    
    X-link: https://github.com/pytorch/pytorch/pull/99694
    
    Change execution graph to execution trace.
    See post: https://fb.workplace.com/groups/873291503156329/permalink/1529496217535851/
    
    #suppress-api-compatibility-check #suppress-bc-linter
    
    Reviewed By: chaekit
    
    Differential Revision: D44121392
    
    fbshipit-source-id: 4da704dde0e6254d3c428b62827db4022d5c7946

--- run.py
+++ run.py
@@ -196,11 +196,11 @@ def profile_one_step(func, nwarmup=WARMUP_ROUNDS):
     if args.profile_eg:
         from datetime import datetime
         import os
-        from torch.profiler import ExecutionGraphObserver
+        from torch.profiler import ExecutionTraceObserver
         start_time = datetime.now()
         timestamp = int(datetime.timestamp(start_time))
         eg_file = f"{args.model}_{timestamp}_eg.json"
-        eg = ExecutionGraphObserver()
+        eg = ExecutionTraceObserver()
         if not os.path.exists(args.profile_eg_folder):
             os.makedirs(args.profile_eg_folder)
         eg.register_callback(f"{args.profile_eg_folder}/{eg_file}")
@@ -240,7 +240,7 @@ def profile_one_step(func, nwarmup=WARMUP_ROUNDS):
     if args.profile_eg and eg:
         eg.stop()
         eg.unregister_callback()
-        print(f"Save Exeution Graph to : {args.profile_eg_folder}/{eg_file}")
+        print(f"Save Exeution Trace to : {args.profile_eg_folder}/{eg_file}")
     print(prof.key_averages(group_by_input_shape=True).table(sort_by="cpu_time_total", row_limit=30))
     print(f"Saved TensorBoard Profiler traces to {args.profile_folder}.")
 
@@ -277,9 +277,9 @@ if __name__ == "__main__":
                         help=f"Enable all profile options, including {SUPPORT_PROFILE_LIST}. Overrides --profile-options.")
     parser.add_argument("--profile-devices", type=_validate_devices,
                         help="Profile comma separated list of activities such as cpu,cuda.")
-    parser.add_argument("--profile-eg", action="store_true", help="Collect execution graph by PARAM")
+    parser.add_argument("--profile-eg", action="store_true", help="Collect execution trace by PARAM")
     parser.add_argument("--profile-eg-folder", default="./eg_logs",
-                        help="Save execution graph traces to this directory.")
+                        help="Save execution traces to this directory.")
     parser.add_argument("--cudastreams", action="store_true",
                         help="Utilization test using increasing number of cuda streams.")
     parser.add_argument("--bs", type=int, help="Specify batch size to the test.")
