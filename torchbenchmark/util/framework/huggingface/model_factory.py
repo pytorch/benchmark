794685661eb46aa07cd724b6ca17413c48bdcd3f
Peter McAughan <petermca@microsoft.com>
2023-06-30 19:58:24 +0000

    Add model_factory entry for Falcon-7b

--- torchbenchmark/util/framework/huggingface/model_factory.py
+++ torchbenchmark/util/framework/huggingface/model_factory.py
@@ -27,6 +27,7 @@ class_models = {
     'hf_Bert': (512, 512, 'BertConfig()', 'AutoModelForMaskedLM'),
     # see https://huggingface.co/bert-large-cased
     'hf_Bert_large': (512, 512, 'BertConfig(hidden_size=1024, num_hidden_layers=24, num_attention_heads=16)', 'AutoModelForMaskedLM'),
+    'hf_Falcon_7b' : (512, 512, 'AutoConfig.from_pretrained("tiiuae/falcon-7b")', 'AutoModelForCausalLM')
 }
 
 cpu_input_slice = {
