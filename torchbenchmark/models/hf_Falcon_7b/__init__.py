447efb0ff1765b75626413972b5a2bd8fb04d242
Peter McAughan <petermca@microsoft.com>
2023-07-10 21:14:00 +0000

    Revert batch_size to feasible number

--- torchbenchmark/models/hf_Falcon_7b/__init__.py
+++ torchbenchmark/models/hf_Falcon_7b/__init__.py
@@ -4,7 +4,8 @@ from torchbenchmark.util.framework.huggingface.model_factory import HuggingFaceM
 class Model(HuggingFaceModel):
     task = NLP.LANGUAGE_MODELING
     # Published training batch size is 2304: see https://huggingface.co/tiiuae/falcon-7b/blob/main/README.md
-    DEFAULT_TRAIN_BSIZE = 2304
+    # Setting to default value of 4 for feasibility
+    DEFAULT_TRAIN_BSIZE = 4
     DEFAULT_EVAL_BSIZE = 1
 
     def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
