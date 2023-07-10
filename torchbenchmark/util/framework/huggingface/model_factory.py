26b68754dc0238e5d3898f7d03111748ab50402d
Peter McAughan <petermca@microsoft.com>
2023-07-10 21:06:12 +0000

    Remove user input for trust_remote_code

--- torchbenchmark/util/framework/huggingface/model_factory.py
+++ torchbenchmark/util/framework/huggingface/model_factory.py
@@ -78,7 +78,10 @@ class HuggingFaceModel(BenchmarkModel):
             # silence "config.num_buckets is not set. Setting config.num_buckets to 128"
             config.num_buckets = 128
         class_ctor = getattr(transformers, class_models[name][3])
-        self.model = class_ctor.from_config(config).to(device)
+        kwargs = {}
+        if name == "hf_Falcon_7b":
+            kwargs["trust_remote_code"] = True
+        self.model = class_ctor.from_config(config, **kwargs).to(device)
         self.optimizer = optim.Adam(
             self.model.parameters(),
             lr=0.001,
