7d3f08618bdad69a3fcba825b8b3c3d3462e2cd9
Akshay Sonawane <asonawane@microsoft.com>
2023-07-17 23:25:18 -0700

    Add MPT-7B-instruct HuggingFace Model

--- torchbenchmark/util/framework/huggingface/model_factory.py
+++ torchbenchmark/util/framework/huggingface/model_factory.py
@@ -29,6 +29,7 @@ class_models = {
     'hf_Bert_large': (512, 512, 'BertConfig(hidden_size=1024, num_hidden_layers=24, num_attention_heads=16)', 'AutoModelForMaskedLM'),
     # default num_hidden_layers=32 but that OOMs, feel free to change this config to something more real
     'llama_v2_7b_16h' : (512,512, 'LlamaConfig(num_hidden_layers=16)', 'AutoModelForCausalLM'),
+    'hf_MPT_7b_instruct': (512, 512, 'AutoConfig.from_pretrained("mosaicml/mpt-7b-instruct", trust_remote_code=True)', 'AutoModelForCausalLM'),
 }
 
 cpu_input_slice = {
@@ -79,7 +80,10 @@ class HuggingFaceModel(BenchmarkModel):
             # silence "config.num_buckets is not set. Setting config.num_buckets to 128"
             config.num_buckets = 128
         class_ctor = getattr(transformers, class_models[name][3])
-        self.model = class_ctor.from_config(config).to(device)
+        kwargs = {}
+        if name == "hf_Falcon_7b" or name == "hf_MPT_7b_instruct":
+            kwargs["trust_remote_code"] = True
+        self.model = class_ctor.from_config(config, **kwargs).to(device)
         self.optimizer = optim.Adam(
             self.model.parameters(),
             lr=0.001,
