294fbba3c8590cf87b7e3aa38fbdd267ef3a7e3e
Mark Saroufim <marksaroufim@gmail.com>
2023-07-20 18:01:25 -0700

    Update install.py

--- torchbenchmark/models/llama_v2/install.py
+++ torchbenchmark/models/llama_v2/install.py
@@ -5,6 +5,5 @@ import os
 from torchbenchmark.util.framework.huggingface.patch_hf import patch_transformers, cache_model
 
 if __name__ == '__main__':
-    patch_transformers()
     model_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
     cache_model(model_name)
