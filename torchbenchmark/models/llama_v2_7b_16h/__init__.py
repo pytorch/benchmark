5cd94bcbf61d2f688e962704464e7cb20375f496
Mark Saroufim <marksaroufim@meta.com>
2023-07-24 07:47:45 -0700

    update

---
+++ torchbenchmark/models/llama_v2_7b_16h/__init__.py
@@ -0,0 +1,15 @@
+from torchbenchmark.tasks import NLP
+from torchbenchmark.util.framework.huggingface.model_factory import HuggingFaceModel, HuggingFaceAuthMixin
+
+class Model(HuggingFaceModel, HuggingFaceAuthMixin):
+    task = NLP.LANGUAGE_MODELING
+    DEFAULT_TRAIN_BSIZE = 1
+    DEFAULT_EVAL_BSIZE = 1
+    DEEPCOPY = False 
+
+    def __init__(self, test, device, jit=False, batch_size=None, extra_args=[]):
+        HuggingFaceAuthMixin.__init__(self)
+        super().__init__(name="llama_v2", test=test, device=device, jit=jit, batch_size=batch_size, extra_args=extra_args)
+
+    def train(self):
+        return NotImplementedError("7b LLAMA model will OOM on CI GPU machines")
