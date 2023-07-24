692e68df9c93ea101dac930bad8f7cddb2445db1
Mark Saroufim <marksaroufim@gmail.com>
2023-07-24 16:50:21 -0700

    Update __init__.py

--- torchbenchmark/models/clip/__init__.py
+++ torchbenchmark/models/clip/__init__.py
@@ -24,7 +24,7 @@ class Model(BenchmarkModel):
         super().__init__(test=test, device=device, jit=jit, batch_size=batch_size, extra_args=extra_args)
         
         self.data_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.data')
-        self.image_name = "Pizza-3007395.jpg"
+        self.image_name = "2880px-Pizza-3007395.jpg"
         self.image = Image.open(os.path.join(self.data_folder, self.image_name))
         self.text = ["pizza", "dog", "sun"]
         self.img_transform = CLIPImageTransform(is_train=False)
