3c61813148ee9e30e258c15f9f67b9ac3b197aef
Mark Saroufim <marksaroufim@meta.com>
2023-07-25 00:05:22 +0000

    ankita feedback

--- torchbenchmark/models/clip/install.py
+++ torchbenchmark/models/clip/install.py
@@ -6,6 +6,7 @@ def pip_install_requirements():
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-q', '-r', 'requirements.txt'])
 
 def download_data():
+    # CC-0 image from wikipedia page on pizza so legal to use
     subprocess.check_call(['wget', '-O', './data', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/2880px-Pizza-3007395.jpg'])
 
 if __name__ == '__main__':
