6512ae60a30db78b6529358ff9a7ec3bd71154ea
Mark Saroufim <marksaroufim@gmail.com>
2023-09-07 16:07:43 -0400

    Update __init__.py

--- torchbenchmark/models/stable_diffusion_unet/__init__.py
+++ torchbenchmark/models/stable_diffusion_unet/__init__.py
@@ -27,6 +27,7 @@ class Model(BenchmarkModel, HuggingFaceAuthMixin):
         model_id = "stabilityai/stable-diffusion-2"
         scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
         self.pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler)
+        self.example_inputs = "a photo of an astronaut riding a horse on mars"
         self.pipe.to(self.device)
 
     def enable_fp16_half(self):
