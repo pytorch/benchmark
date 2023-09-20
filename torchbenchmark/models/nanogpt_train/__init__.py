7cbee8ae5220c390a5a52ae2da51e56db5c7f5b4
An HPCaaS user for xmfan <xmfan@a100-st-p4d24xlarge-29.pytorch.hpcaas>
2023-09-20 00:23:09 +0000

    add nanogpt_train

---
+++ torchbenchmark/models/nanogpt_train/__init__.py
@@ -0,0 +1,50 @@
+from ...util.model import BenchmarkModel
+from torchbenchmark.tasks import NLP
+import torch
+from .model import GPT, GPTConfig
+
+
+class Model(BenchmarkModel):
+    task = NLP.GENERATION
+    DEFAULT_TRAIN_BSIZE = 1
+
+    def __init__(self, test, device, batch_size=None, extra_args=[]):
+        super().__init__(test=test, device=device, batch_size=batch_size, extra_args=extra_args)
+        # Use the default configs
+        # self.gpt_config = GPTConfig()
+        # self.generator_config = GPTGenerationConfig(32, 0.8, 200)
+        # self.model = SequenceGeneratorNanoGPT(GPT(self.gpt_config), self.generator_config).eval().to(self.device)
+        # self.prompt_size = 64
+        # self.example_inputs = (
+        #     torch.randint(1, self.gpt_config.vocab_size, (self.batch_size, self.prompt_size)).to(self.device),
+        # )
+
+        # from scratch
+        batch_size = 12
+
+        n_layer = 12
+        n_head = 12
+        n_embd = 768
+        block_size = 1024
+        bias = False
+        vocab_size=50304
+        dropout = 0.0
+        prompt_size = 64
+        model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=block_size,
+                  bias=bias, vocab_size=vocab_size, dropout=dropout)
+        gptconf = GPTConfig(**model_args)
+
+        self.model = GPT(gptconf).to(device)
+        self.example_inputs = (
+            torch.randint(1, gptconf.vocab_size, (batch_size, prompt_size)).to(self.device),
+        )
+
+    def get_module(self):
+        return self.model, self.example_inputs
+
+    def train(self):
+        out = self.model(*self.example_inputs)
+        return (out,)
+
+    def eval(self):
+        return NotImplementedError("Inference not supported for this model")
