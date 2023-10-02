52ac8d9202af4859274b205aa9a390052de67770
Gerson Beckenkamp <gersonmiguelbeckenkamp@gmail.com>
2023-10-02 10:50:33 +0100

    Remove extra lines

--- torchbenchmark/__init__.py
+++ torchbenchmark/__init__.py
@@ -138,7 +138,6 @@ def setup(models: List[str] = [], verbose: bool = True, continue_on_fail: bool =
     failures = {}
     models = list(map(lambda p: p.lower(), models))
     model_paths = filter(lambda p: True if not models else os.path.basename(p).lower() in models, _list_model_paths())
-    
     if allow_canary:
         canary_model_paths = filter(lambda p: True if not models else os.path.basename(p).lower() in models, _list_canary_model_paths())
         model_paths = list(model_paths)
@@ -294,7 +293,6 @@ class ModelTask(base_task.TaskBase):
 
         model_name = os.path.basename(model_path)
         model_dir = os.path.basename(os.path.dirname(model_path))
-
         diagnostic_msg = ""
         try:
             module = importlib.import_module(f'.{model_dir}.{model_name}', package=package)
