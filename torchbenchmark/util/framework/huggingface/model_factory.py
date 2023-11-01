a88756b4cdeab1977cb1679e4e48672f7fbe9088
Pranav Sharma <prs@microsoft.com>
2023-11-01 16:04:02 -0700

    Fix devices, add smaller length and fix phi dep

--- torchbenchmark/util/framework/huggingface/model_factory.py
+++ torchbenchmark/util/framework/huggingface/model_factory.py
@@ -36,7 +36,7 @@ class_models = {
     'llama_v2_70b' : (512, 512, 'AutoConfig.from_pretrained("meta-llama/Llama-2-70b-hf")', 'AutoModelForMaskedLM'),
     'phi_1_5' : (512, 512, 'AutoConfig.from_pretrained("microsoft/phi-1_5", trust_remote_code=True)', 'AutoModelForCausalLM'),
     # as per this page https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.1 trust_remote_code=True is not required
-    'mistral_7b_instruct' : (512, 512, 'AutoConfig.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")', 'AutoModelForCausalLM')
+    'mistral_7b_instruct' : (128, 128, 'AutoConfig.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")', 'AutoModelForCausalLM')
 }
 
 cpu_input_slice = {
