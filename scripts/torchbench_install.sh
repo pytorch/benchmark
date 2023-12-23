79c236aed69907988b941e730965e6bfc9fd8c21
Xu Zhao <i@xuzhao.net>
2023-12-22 19:36:48 -0800

    Move phi_1_5 to canary as it does not install on the docker build (#2095)
    Summary:
    The docker build runner will CPU OOM when installing phi_1_5. Moving the model to canary.
    
    https://github.com/pytorch/benchmark/actions/runs/7263599720
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/2095
    
    Test Plan:
    Nightly docker build:
    https://github.com/pytorch/benchmark/actions/runs/7301484235
    
    Reviewed By: aaronenyeshi
    
    Differential Revision: D52391171
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 4866098292cbca7459d632c5c05fe620e638077e

--- scripts/torchbench_install.sh
+++ scripts/torchbench_install.sh
@@ -16,4 +16,5 @@ conda activate "${CONDA_ENV}"
 parent_dir=$(dirname "$(readlink -f "$0")")/..
 cd ${parent_dir}
 
+python -c "import torch; print(torch.__version__); print(torch.version.git_version)"
 python install.py
