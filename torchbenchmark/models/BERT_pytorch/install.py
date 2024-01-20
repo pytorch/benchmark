45ce705032391eaf8e65f48a57e3800f453a105e
Xu Zhao <i@xuzhao.net>
2024-01-19 23:00:21 -0500

    Fix bert pytorch install

--- torchbenchmark/models/BERT_pytorch/install.py
+++ torchbenchmark/models/BERT_pytorch/install.py
@@ -6,4 +6,5 @@ def setup_install():
     subprocess.check_call([sys.executable, '-m', "pip", 'install', '--user', '-e', '.'])
 
 if __name__ == '__main__':
-    setup_install()
+    # setup_install()
+    pass
