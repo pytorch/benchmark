5da4e09730bdd8b74b3cb5b4984068b129bcbd6c
Xu Zhao <xzhao9@meta.com>
2024-01-22 15:55:15 -0500

    Fix installation issue.

--- torchbenchmark/models/BERT_pytorch/install.py
+++ torchbenchmark/models/BERT_pytorch/install.py
@@ -6,5 +6,4 @@ def setup_install():
     subprocess.check_call([sys.executable, '-m', "pip", 'install', '--user', '-e', '.'])
 
 if __name__ == '__main__':
-    # setup_install()
-    pass
+    setup_install()
