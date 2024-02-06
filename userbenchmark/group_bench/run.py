b7f21fb48cb7b1f4e3d07dd21bd78c823a5ab845
Xu Zhao <xzhao9@meta.com>
2024-02-05 19:47:44 -0500

    Skip runtime error

--- userbenchmark/group_bench/run.py
+++ userbenchmark/group_bench/run.py
@@ -129,8 +129,11 @@ def run_config(config: TorchBenchModelConfig, metrics: List[str], dryrun: bool=F
     except NotImplementedError:
         print(" [not_implemented]", flush=True)
         return dict.fromkeys(metrics, "not_implemented")
+    except RuntimeError as e:
+        print(" [runtime_error]", flush=True)
+        return dict.fromkeys(metrics, str(e))
     except OSError as e:
-        print(" [oserror]", flush=True)
+        print(" [os_error]", flush=True)
         return dict.fromkeys(metrics, str(e))
 
 def run_config_accuracy(config: TorchBenchModelConfig, metrics: List[str], dryrun: bool=False) -> Dict[str, str]:
@@ -146,8 +149,11 @@ def run_config_accuracy(config: TorchBenchModelConfig, metrics: List[str], dryru
     except NotImplementedError:
         print(" [not_implemented]", flush=True)
         return {"accuracy": "not_implemented"}
+    except RuntimeError as e:
+        print(" [runtime_error]", flush=True)
+        return {"accuracy": str(e)}
     except OSError as e:
-        print(" [oserror]", flush=True)
+        print(" [os_error]", flush=True)
         return {"accuracy": str(e)}
 
 def models_from_config(config) -> List[str]:
