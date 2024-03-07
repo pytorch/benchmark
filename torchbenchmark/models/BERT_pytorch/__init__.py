ac5350f778619c7c59f87438c1cec595c5047f09
Xu Zhao <xzhao9@meta.com>
2024-03-07 00:07:14 -0500

    Another fix

--- torchbenchmark/models/BERT_pytorch/__init__.py
+++ torchbenchmark/models/BERT_pytorch/__init__.py
@@ -208,7 +208,7 @@ class Model(BenchmarkModel):
         return self.model.model, self.example_inputs
 
     def set_module(self, new_model):
-        self.model.bert = new_model
+        self.model.model = new_model
 
     def eval(self) -> typing.Tuple[torch.Tensor]:
         model = self.model
