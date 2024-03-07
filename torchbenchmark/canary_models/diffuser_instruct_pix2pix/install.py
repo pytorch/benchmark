b27a351cc8d76da06df07c212da4f35b971568f8
Iason Myttas <iasonasm@gmail.com>
2024-03-07 02:31:15 +0000

    installation file fixed

--- torchbenchmark/canary_models/diffuser_instruct_pix2pix/install.py
+++ torchbenchmark/canary_models/diffuser_instruct_pix2pix/install.py
@@ -6,8 +6,8 @@ import warnings
 MODEL_NAME = "timbrooks/instruct-pix2pix"
 
 def load_model_checkpoint():
-
     from diffusers import StableDiffusionInstructPix2PixPipeline
+
     StableDiffusionInstructPix2PixPipeline.from_pretrained(MODEL_NAME, torch_dtype=torch.float16, safety_checker=None)
 
 if __name__ == '__main__':
