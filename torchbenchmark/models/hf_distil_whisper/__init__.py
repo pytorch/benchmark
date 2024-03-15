c9470a4bfd5365fa9e624f73c92d02247c574460
Xu Zhao <xzhao9@meta.com>
2024-03-15 19:04:44 -0400

    Fix hf whisper series

--- torchbenchmark/models/hf_distil_whisper/__init__.py
+++ torchbenchmark/models/hf_distil_whisper/__init__.py
@@ -17,9 +17,12 @@ class Model(HuggingFaceModel):
         self.example_inputs = {"input_features": self.input_features.to(self.device), "input_ids" : self.input_features.to(self.device)}
         self.model.to(self.device)
 
+    def get_module(self):
+        return self.model, (self.example_inputs["input_ids"], )
+
     def train(self):
         raise NotImplementedError("Training is not implemented.")
-    
+
     def eval(self):
         self.model.eval()
         with torch.no_grad():
