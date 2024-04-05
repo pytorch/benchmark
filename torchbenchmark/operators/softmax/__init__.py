Fix the interface with softmax and bmm
Summary: We updated the framework interface in D55611925, we should make corresponding changes in softmax and bmm.

Reviewed By: bertmaher

Differential Revision: D55807666

fbshipit-source-id: 323d163fc06cee9d7537b0cff7bfc007699b377b

torchbenchmark/operators/softmax/__init__.py
@@ -8,7 +8,7 @@ from torchbenchmark.util.triton_op import (
     register_benchmark,
     register_metric,
 )
-from typing import Generator
+from typing import Generator, List
 
 class Operator(BenchmarkOperator):
 
@@ -30,18 +30,18 @@ class Operator(BenchmarkOperator):
         y = torch.empty_like(x)
         # Enqueue kernel. The 1D launch grid is simple: we have one kernel instance per row o
         # f the input matrix
-        Operator.softmax_kernel[(n_rows, )](
-            y,
-            x,
-            x.stride(0),
-            y.stride(0),
-            n_cols,
-            num_warps=num_warps,
-            BLOCK_SIZE=BLOCK_SIZE,
-        )
-        if self.is_training:
-            y.sum().backward()
-        return y, x
+        def _inner():
+            Operator.softmax_kernel[(n_rows, )](
+                y,
+                x,
+                x.stride(0),
+                y.stride(0),
+                n_cols,
+                num_warps=num_warps,
+                BLOCK_SIZE=BLOCK_SIZE,
+            )
+            return y
+        return _inner
 
     @triton.jit
     def softmax_kernel(output_ptr, input_ptr, input_row_stride, output_row_stride, n_cols, BLOCK_SIZE: tl.constexpr):
@@ -72,20 +72,20 @@ class Operator(BenchmarkOperator):
         We subtract the maximum element in order to avoid overflows. Softmax is invariant to
         this shift.
         """
-        # read  MN elements ; write M  elements
-        x_max = x.max(dim=1)[0]
-        # read MN + M elements ; write MN elements
-        z = x - x_max[:, None]
-        # read  MN elements ; write MN elements
-        numerator = torch.exp(z)
-        # read  MN elements ; write M  elements
-        denominator = numerator.sum(dim=1)
-        # read MN + M elements ; write MN elements
-        ret = numerator / denominator[:, None]
-        # in total: read 5MN + 2M elements ; wrote 3MN + 2M elements
-        if self.is_training:
-            ret.sum().backward()
-        return ret, x
+        def _inner():
+            # read  MN elements ; write M  elements
+            x_max = x.max(dim=1)[0]
+            # read MN + M elements ; write MN elements
+            z = x - x_max[:, None]
+            # read  MN elements ; write MN elements
+            numerator = torch.exp(z)
+            # read  MN elements ; write M  elements
+            denominator = numerator.sum(dim=1)
+            # read MN + M elements ; write MN elements
+            ret = numerator / denominator[:, None]
+            # in total: read 5MN + 2M elements ; wrote 3MN + 2M elements
+            return ret
+        return _inner
 
     def get_input_iter(self):
         i = 1
@@ -102,7 +102,7 @@ class Operator(BenchmarkOperator):
         return float(shape[1])
 
     @register_metric()
-    def gbps(self, example_inputs, metrics: BenchmarkOperatorMetrics) -> float:
+    def gbps(self, fn_name, example_inputs, metrics: BenchmarkOperatorMetrics) -> List[float]:
         gbps = lambda ms: 2 * example_inputs[0].nelement() * example_inputs[0].element_size() * 1e-9 / (ms * 1e-3)
         return list(map(gbps, metrics.latency))
 

torchbenchmark/util/triton_op.py
@@ -87,15 +87,15 @@ class BenchmarkOperatorResult:
                 for metric in key_metrics[k]:
                     if metrics_dict["error_msg"]:
                         row.append(metrics_dict["error_msg"])
-                    elif metric in metrics_dict:
-                        if isinstance(metrics_dict[metric], list):
-                            row.append(numpy.median(metrics_dict[metric]))
-                        elif isinstance(metrics_dict[metric], bool):
-                            row.append(1.0 if metrics_dict[metric] else 0.0)
-                        else:
-                            row.append(metrics_dict[metric])
-                    elif metric in metrics_dict["extra_metrics"]:
-                        row.append(metrics_dict["extra_metrics"][metric])
+                        continue
+                    _metrics_dict = metrics_dict["extra_metrics"] \
+                        if metric in metrics_dict["extra_metrics"] else metrics_dict
+                    if isinstance(_metrics_dict[metric], list):
+                        row.append(numpy.median(_metrics_dict[metric]))
+                    elif isinstance(_metrics_dict[metric], bool):
+                        row.append(1.0 if _metrics_dict[metric] else 0.0)
+                    else:
+                        row.append(_metrics_dict[metric])
             table.append(row)
         return headers, table
 
@@ -326,14 +326,14 @@ class BenchmarkOperator():
         std = avg_length // 3  # rather arbitrary, but likely reasonable
         lengths = [random.gauss(avg_length, std) for _ in range(batch_size)]
         lengths = [int(min(max_seq_len, max(L, 0))) for L in lengths]
-    
+
         if load_factor == 1.0:
             lengths = [max_seq_len] * batch_size
-    
+
         diff = sum(lengths) - total_length
         idx_and_lengths = list(enumerate(lengths))
         random.shuffle(idx_and_lengths)
-    
+
         for i, length in idx_and_lengths:
             if diff == 0:
                 break
@@ -345,11 +345,11 @@ class BenchmarkOperator():
                 delta = min(max_seq_len - length, -diff)
                 lengths[i] += delta
                 diff += delta
-    
+
         offsets = [0]
         for length in lengths:
             offsets.append(offsets[-1] + length)
-    
+
         return torch.tensor(
             offsets,
             dtype=offsets_dtype,