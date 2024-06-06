0d3bd3e45b358455d7ef6a66f9f53b5df9b6eb98
Ashwini Rathi <arathi@habana.ai>
2024-06-06 15:03:34 +0300

    torch_multimodal_clip model get failed due to network issue Description: while running model torch_multimodal_clip getting this issue FileNotFoundError: [Errno 2] No such file or directory: 'torchbenchmark/models/torch_multimodal_clip/.data/pizza.jpg' main reason is while downloading the pizza.jpg , getting Failed to download file with status code 403 issue so this change will resolve this issue

--- torchbenchmark/models/torch_multimodal_clip/install.py
+++ torchbenchmark/models/torch_multimodal_clip/install.py
@@ -5,7 +5,10 @@ import requests
 
 def download(output_filename, uri):
     # Download the file with streaming to handle large files
-    response = requests.get(uri, stream=True)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(uri, headers=headers, stream=True)
     if response.status_code == 200:
         with open(output_filename, 'wb') as f:
             for chunk in response.iter_content(chunk_size=8192):  # Define the chunk size to be used
