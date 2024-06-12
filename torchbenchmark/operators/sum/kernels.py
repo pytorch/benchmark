3ecaae9d142151b98ca0d8d7df763afeddf55e06
Janani Sriram <jananisriram@meta.com>
2024-06-12 11:06:23 -0700

    Add support for reducing across individual dimensions for 2D matrices using the sum Triton kernel (#2295)
    Summary:
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/2295
    
    Support reducing a 2-dimensional matrix across one dimension, where the `BLOCK_SIZE` in the reduced dimension is larger than the dimension size. This kernel performs a simplified reduction which assumes that the entire reduction dimension of the tensor fits in a thread block. The implementation handles toggling between block sizes for the `M` and `N` dimensions depending on the reduction dimension. For example, this kernel will reduce across the 0-th dimension for a (M, N) = (16, 16) matrix where `BLOCK_SIZE_M >= 16` and `BLOCK_SIZE_N` is autotuned.
    
    Add a `best_config` metric to find the best `BLOCK_SIZE` for the non-reduction dimension and `num_warps` given some input size.
    
    Reviewed By: jbschlosser
    
    Differential Revision: D58261858
    
    fbshipit-source-id: 8995c91c54e9792b52f4608446e8e940027a604d

--- torchbenchmark/operators/sum/kernels.py
+++ torchbenchmark/operators/sum/kernels.py
@@ -1,12 +1,14 @@
+import itertools
+
 import torch
 import triton
 import triton.language as tl
 
 
 @triton.jit
-def triton_sum_kernel_scalar(
-    input_ptr,
-    output_ptr,
+def triton_sum_kernel_scalar_result(
+    input_ptr,  # pointer to input matrix
+    output_ptr,  # pointer to output matrix
     M,  # number of elements
     BLOCK_SIZE_M: tl.constexpr,  # number of elements per block
 ):
@@ -37,3 +39,73 @@ def triton_sum_kernel_scalar(
     tl.store(
         output_ptr + output_offsets, output
     )  # store output, where the stored pointers are in the desired output shape
+
+
+@triton.autotune(
+    configs=[
+        triton.Config(
+            {"BLOCK_SIZE_NON_REDUCE_DIM": b},
+            num_warps=w,
+        ) for b, w in itertools.product(
+            [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024],  # block sizes
+            [2, 4, 8, 16]  # number of warps
+        )
+    ],
+    key=["M", "N"],
+)
+@triton.jit
+def triton_sum_kernel_1D_result(
+    input_ptr,  # pointer to input matrix
+    output_ptr,  # pointer to output matrix
+    # matrix dimensions (input)
+    M,  # number of rows
+    N,  # number of columns
+    # block sizes (input)
+    BLOCK_SIZE_NON_REDUCE_DIM: tl.constexpr,  # number of elements in non-reduction dimension per block
+    BLOCK_SIZE_REDUCE_DIM: tl.constexpr,  # number of elements in reduction dimension per block
+    # reduction dimension
+    dim: tl.constexpr,  # dimension along which to sum
+):
+    pid = tl.program_id(axis=0)  # i-th block of input
+
+    block_start_m, block_start_n = 0, 0
+    offsets_m, offsets_n = None, None
+    if dim == 0:
+        block_start_n = pid * BLOCK_SIZE_REDUCE_DIM
+        # offsets have shape equal to input shape
+        offsets_m = block_start_m + tl.arange(
+            0, BLOCK_SIZE_REDUCE_DIM
+        )  # create 1D vector for offsets on M-th dimension
+        offsets_n = block_start_n + tl.arange(
+            0, BLOCK_SIZE_NON_REDUCE_DIM
+        )  # create 1D vector for offsets on N-th dimension
+    elif dim == 1:
+        block_start_m = pid * BLOCK_SIZE_REDUCE_DIM
+        # offsets have shape equal to input shape
+        offsets_m = block_start_m + tl.arange(
+            0, BLOCK_SIZE_NON_REDUCE_DIM
+        )  # create 1D vector for offsets on M-th dimension
+        offsets_n = block_start_n + tl.arange(
+            0, BLOCK_SIZE_REDUCE_DIM
+        )  # create 1D vector for offsets on N-th dimension
+
+    # mask has shape equal to input shape
+    mask_m = offsets_m < M
+    mask_n = offsets_n < N
+
+    # create 2D matrices of pointers and masks, using above M and N vectors
+    idxs = (offsets_m[:, None] * N) + offsets_n
+    mask = mask_m[:, None] & mask_n
+
+    # loaded pointers have shape equal to input shape
+    input = tl.load(
+        input_ptr + idxs, mask=mask, other=mask
+    )  # other=mask zeros out masked values from input
+
+    output = tl.sum(input, axis=dim)
+
+    # stored pointers have shape equal to output shape
+    if dim == 0:  # store output along N-th dimension
+        tl.store(output_ptr + offsets_n, output, mask=mask_n)
+    elif dim == 1:  # store output along M-th dimension
+        tl.store(output_ptr + offsets_m, output, mask=mask_m)
