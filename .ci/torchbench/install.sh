8ed33eab139c80dee01142910d4548f9f46c8213
Xu Zhao <xzhao9@meta.com>
2024-06-21 18:51:53 -0400

    Fix concurrent jobs

--- .ci/torchbench/install.sh
+++ .ci/torchbench/install.sh
@@ -19,7 +19,7 @@ CONDA_ENV=${BASE_CONDA_ENV} . "${SETUP_SCRIPT}"
 conda activate "${BASE_CONDA_ENV}"
 # Remove the conda env if exists
 conda remove --name "${CONDA_ENV}" -y --all || true
-conda create --name "${CONDA_ENV}" --clone "${BASE_CONDA_ENV}"
+conda create --name "${CONDA_ENV}" -y --clone "${BASE_CONDA_ENV}"
 conda activate "${CONDA_ENV}"
 
 parent_dir=$(dirname "$(readlink -f "$0")")/../..
