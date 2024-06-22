1f982392be22770de29e083b29c84f43dbbd4cbe
Xu Zhao <xzhao9@meta.com>
2024-06-22 07:20:43 -0700

    Improve the ci by separating CPU and CUDA tests (#2327)
    Summary:
    We are separating CPU and CUDA tests using different runners.
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/2327
    
    Reviewed By: huydhn
    
    Differential Revision: D58902378
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 6c2598dc2cd5c94105f1eb15862da924ae25894c

---
+++ .ci/torchbench/install.sh
@@ -0,0 +1,29 @@
+. ${HOME}/miniconda3/etc/profile.d/conda.sh
+
+if [ -z "${BASE_CONDA_ENV}" ]; then
+  echo "ERROR: BASE_CONDA_ENV is not set"
+  exit 1
+fi
+
+if [ -z "${CONDA_ENV}" ]; then
+  echo "ERROR: CONDA_ENV is not set"
+  exit 1
+fi
+
+if [ -z "${SETUP_SCRIPT}" ]; then
+  echo "ERROR: SETUP_SCRIPT is not set"
+  exit 1
+fi
+
+CONDA_ENV=${BASE_CONDA_ENV} . "${SETUP_SCRIPT}"
+conda activate "${BASE_CONDA_ENV}"
+# Remove the conda env if exists
+conda remove --name "${CONDA_ENV}" -y --all || true
+conda create --name "${CONDA_ENV}" -y --clone "${BASE_CONDA_ENV}"
+conda activate "${CONDA_ENV}"
+
+parent_dir=$(dirname "$(readlink -f "$0")")/../..
+cd ${parent_dir}
+
+python -c "import torch; print(torch.__version__); print(torch.version.git_version)"
+python install.py
