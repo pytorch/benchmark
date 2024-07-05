ac1a6cde3fa34526abbf4132666a7e997f86ee71
Xu Zhao <xzhao9@meta.com>
2024-07-04 20:26:41 -0700

    Run performance test non-alternately
    Summary:
    By default, performance tests (speedup experiments) will run the baseline and test backend alternately.
    
    However, this does not work for the torchao backend, which will change the model in-place, therefore the baseline run will also run with torchao backend since the model has already been quantized.
    
    Add a new experiment "latency_experiment" to run performance tests non-alternately (first run baseline for a few iterations, then run the test backend).
    
    other changes:
    
    need to add torch.compiler.cudagraph_mark_step_begin() to avoid the
    slowdown from             # Unable to hit fast path of CUDAGraphs because of pending, uninvoked backwards
    
    also updated the torchao APIs to the current versions
    
    Test Plan:
    
    python run_benchmark.py torchao --only AlbertForMaskedLM --quantization noquant --performance --inference --bfloat16 --inductor-compile-mode max-autotune
    python run_benchmark.py torchao --only BartForCausalLM --quantization noquant --performance --inference --bfloat16 --inductor-compile-mode max-autotune
    python run_benchmark.py torchao --only timm_efficientnet --quantization noquant --performance --inference --bfloat16 --inductor-compile-mode max-autotune
    
    (should all be ~1.0
    0.997x
    1.006x
    0.994x
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- userbenchmark/dynamo/dynamobench/common.py
+++ userbenchmark/dynamo/dynamobench/common.py
@@ -699,7 +699,6 @@ def latency_experiment(args, model_iter_fn, model, example_inputs, mark, **kwarg
     should_randomize_input = args.randomize_input
 
     import contextlib
-
     from torch._inductor.utils import maybe_profile
 
     @contextlib.contextmanager
@@ -2964,10 +2963,8 @@ class BenchmarkRunner:
         self, name, model, example_inputs, optimize_ctx, experiment, tag=None
     ):
         "Run performance test in non-alternately."
-        assert (
-            experiment.func is latency_experiment
-        ), "Must run with latency_experiment."
-
+        assert experiment.func is latency_experiment, \
+            f"Must run with latency_experiment."
         def warmup(fn, model, example_inputs, mode, niters=10):
             peak_mem = 0
             start_stats = get_dynamo_stats()
@@ -3018,7 +3015,6 @@ class BenchmarkRunner:
             if tag is not None:
                 experiment_kwargs["tag"] = tag
             results = []
-
             with maybe_snapshot_memory(
                 self.args.snapshot_memory, f"eager_{self.args.only}"
             ):
@@ -3030,9 +3026,7 @@ class BenchmarkRunner:
                         self.model_iter_fn, model, example_inputs, "eager", niters=1
                     )
 
-            baseline_timings = experiment(
-                model, example_inputs, mark="expected", **experiment_kwargs
-            )
+            baseline_timings = experiment(model, example_inputs, mark="expected", **experiment_kwargs)
 
             if self.args.export_aot_inductor:
                 t_0 = time.perf_counter()
@@ -3110,13 +3104,9 @@ class BenchmarkRunner:
                 experiment = functools.partial(
                     experiment, optimized_model_iter_fn.context.onnx_model
                 )
-            backend_timings = experiment(
-                model, example_inputs, mark="expected", **experiment_kwargs
-            )
+            backend_timings = experiment(model, example_inputs, mark="expected", **experiment_kwargs)
             timings = np.stack((baseline_timings, backend_timings), axis=1)
-            result_summary = latency_experiment_summary(
-                self.args, model, timings, **experiment_kwargs
-            )
+            result_summary = latency_experiment_summary(self.args, model, timings, **experiment_kwargs)
             if not hasattr(model, name):
                 model.name = name
             results.append(result_summary)
@@ -4430,7 +4420,13 @@ def run(runner, args, original_dir=None):
                 fullgraph=args.nopython,
                 mode=args.inductor_compile_mode,
             )
-            runner.model_iter_fn = baseline_ctx(runner.model_iter_fn)
+            model_iter_fn = baseline_ctx(runner.model_iter_fn)
+            # needed to avoid error that causes inconsistent timing due to:
+            # Unable to hit fast path of CUDAGraphs because of pending, uninvoked backwards
+            def model_iter_fn_and_mark_step(*args, **kwargs):
+                torch.compiler.cudagraph_mark_step_begin()
+                model_iter_fn(*args, **kwargs)
+            runner.model_iter_fn = model_iter_fn_and_mark_step
             optimize_ctx = torchao_optimize_ctx(args.quantization)
         else:
             optimize_ctx = torch._dynamo.optimize(args.backend, nopython=args.nopython)
