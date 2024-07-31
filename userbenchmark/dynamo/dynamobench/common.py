50db22899638387b80ae04fd8cb23924dc877c87
HDCharles <charlesdavidhernandez@gmail.com>
2024-07-30 19:17:06 -0700

    Linting
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- userbenchmark/dynamo/dynamobench/common.py
+++ userbenchmark/dynamo/dynamobench/common.py
@@ -2963,8 +2963,10 @@ class BenchmarkRunner:
         self, name, model, example_inputs, optimize_ctx, experiment, tag=None
     ):
         "Run performance test in non-alternately."
-        assert experiment.func is latency_experiment, \
-            f"Must run with latency_experiment."
+        assert (
+            experiment.func is latency_experiment
+        ), "Must run with latency_experiment."
+
         def warmup(fn, model, example_inputs, mode, niters=10):
             peak_mem = 0
             start_stats = get_dynamo_stats()
@@ -3026,7 +3028,9 @@ class BenchmarkRunner:
                         self.model_iter_fn, model, example_inputs, "eager", niters=1
                     )
 
-            baseline_timings = experiment(model, example_inputs, mark="expected", **experiment_kwargs)
+            baseline_timings = experiment(
+                model, example_inputs, mark="expected", **experiment_kwargs
+            )
 
             if self.args.export_aot_inductor:
                 t_0 = time.perf_counter()
@@ -3104,9 +3108,13 @@ class BenchmarkRunner:
                 experiment = functools.partial(
                     experiment, optimized_model_iter_fn.context.onnx_model
                 )
-            backend_timings = experiment(model, example_inputs, mark="expected", **experiment_kwargs)
+            backend_timings = experiment(
+                model, example_inputs, mark="expected", **experiment_kwargs
+            )
             timings = np.stack((baseline_timings, backend_timings), axis=1)
-            result_summary = latency_experiment_summary(self.args, model, timings, **experiment_kwargs)
+            result_summary = latency_experiment_summary(
+                self.args, model, timings, **experiment_kwargs
+            )
             if not hasattr(model, name):
                 model.name = name
             results.append(result_summary)
@@ -4421,6 +4429,7 @@ def run(runner, args, original_dir=None):
                 mode=args.inductor_compile_mode,
             )
             model_iter_fn = baseline_ctx(runner.model_iter_fn)
+
             # needed to avoid error that causes inconsistent timing due to:
             # Unable to hit fast path of CUDAGraphs because of pending, uninvoked backwards
             def model_iter_fn_and_mark_step(*args, **kwargs):
--- userbenchmark/dynamo/dynamobench/torchao_backend.py
+++ userbenchmark/dynamo/dynamobench/torchao_backend.py
@@ -5,6 +5,7 @@ import torch
 
 def setup_baseline():
     from torchao.quantization.utils import recommended_inductor_config_setter
+
     recommended_inductor_config_setter()
     torch._dynamo.config.automatic_dynamic_shapes = False
     torch._dynamo.config.cache_size_limit = 10000
@@ -14,10 +15,10 @@ def torchao_optimize_ctx(quantization: str):
     import torchao
     from torchao.quantization.quant_api import (
         autoquant,
-        quantize_,
-        int8_dynamic_activation_int8_weight,
         int4_weight_only,
+        int8_dynamic_activation_int8_weight,
         int8_weight_only,
+        quantize_,
     )
     from torchao.utils import unwrap_tensor_subclass
 
@@ -25,13 +26,17 @@ def torchao_optimize_ctx(quantization: str):
         def _torchao_apply(module: torch.nn.Module, example_inputs: Any):
             if getattr(module, "_quantized", None) is None:
                 if quantization == "int8dynamic":
-                    quantize_(module, int8_dynamic_activation_int8_weight(), set_inductor_config=False)
+                    quantize_(
+                        module,
+                        int8_dynamic_activation_int8_weight(),
+                        set_inductor_config=False,
+                    )
                 elif quantization == "int8weightonly":
                     quantize_(module, int8_weight_only(), set_inductor_config=False)
                 elif quantization == "int4weightonly":
                     quantize_(module, int4_weight_only(), set_inductor_config=False)
                 if quantization == "autoquant":
-                    torchao.autoquant(module, error_on_unseen=False, set_inductor_config=False)
+                    autoquant(module, error_on_unseen=False, set_inductor_config=False)
                     if isinstance(example_inputs, dict):
                         module(**example_inputs)
                     else:
@@ -39,7 +44,8 @@ def torchao_optimize_ctx(quantization: str):
                     from torchao.quantization.autoquant import AUTOQUANT_CACHE
 
                     if len(AUTOQUANT_CACHE) == 0:
-                        raise Exception("NotAutoquantizable"
+                        raise Exception(
+                            "NotAutoquantizable"
                             f"Found no autoquantizable layers in model {type(module)}, stopping autoquantized run"
                         )
                 else:
