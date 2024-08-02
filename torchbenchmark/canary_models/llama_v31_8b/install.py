86c8ec6cbcf92e005ef44f324257dbe133878fad
MaanavD <maanavdalal@microsoft.com>
2024-08-02 11:38:23 -0700

    Adding llama31 (#2404)
    Summary:
    Runs fine locally on CPU, pipelines will hopefully validate that
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/2404
    
    Reviewed By: kit1980
    
    Differential Revision: D60628732
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 7a5a313e871311f2d1c0eadcf125128ccd407743

---
+++ torchbenchmark/canary_models/llama_v31_8b/install.py
@@ -0,0 +1,6 @@
+import os
+from torchbenchmark.util.framework.huggingface.patch_hf import cache_model
+
+if __name__ == "__main__":
+    model_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+    cache_model(model_name)
