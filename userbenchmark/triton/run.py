1e2795c94f2842728e49fde4e2cab200c32f3aae
FindHao <yhao@meta.com>
2024-10-10 15:28:12 -0700

    remove inreachable arguments check

--- userbenchmark/triton/run.py
+++ userbenchmark/triton/run.py
@@ -146,8 +146,6 @@ def get_parser(args=None):
     args, extra_args = parser.parse_known_args(args)
     if args.op and args.ci:
         parser.error("cannot specify operator when in CI mode")
-    elif not args.op and not args.ci and not args.op_collection:
-        parser.error("must specify operator or operator collection when not in CI mode")
     if not args.op and not args.op_collection:
         print(
             "Neither operator nor operator collection is specified. Running all operators in the default collection."
