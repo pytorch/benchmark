84a2ea1c483e84a9f439076a04825b4543597c09
FindHao <yhao@meta.com>
2024-10-16 11:06:47 -0700

    we don't have oss unit_test ci

--- unit_test.py
+++
@@ -1,6 +0,0 @@
-def test_get_operators():
-    from torchbenchmark.operators_collection import list_operators_by_collection
-
-    print(list_operators_by_collection("default"))
-    print(list_operators_by_collection("all"))
-    print(list_operators_by_collection("liger"))
