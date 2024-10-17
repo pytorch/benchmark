e2f1d02279c9d42a6dc6057a421ae5dcb52aa101
Xu Zhao <xzhao9@meta.com>
2024-10-17 18:46:23 -0400

    Implement gated mlp

---
+++ torchbenchmark/operators/gated_mlp/operator.py
@@ -0,0 +1,63 @@
+from typing import Generator
+import torch
+
+try:
+    import mirage as mi
+    HAS_MIRAGE = True
+except:
+    HAS_MIRAGE = False
+
+from torchbenchmark.util.triton_op import (
+    BenchmarkOperator,
+    register_benchmark
+)
+
+class Operator(BenchmarkOperator):
+
+    DEFAULT_PRECISION = "fp16"
+
+    def __init__(self):
+        if HAS_MIRAGE:
+            self.mirage_optimized_graph = self._get_mi_optimized_graph()
+        self.baseline = self._get_baseline()
+
+
+    def _get_mi_optimized_graph(self):
+        graph = mi.new_kernel_graph()
+        X = graph.new_input(dims=(8, 4096), dtype=mi.float16)
+        W1 = graph.new_input(dims=(4096, 4096), dtype=mi.float16)
+        W2 = graph.new_input(dims=(4096, 4096), dtype=mi.float16)
+        D1 = graph.matmul(X, W1)
+        D2 = graph.matmul(X, W2)
+        O = graph.mul(graph.silu(D1, D2))
+        graph.mark_output(O)
+        optimized_graph = mi.superoptimize(graph)
+        return optimized_graph
+
+
+    @register_benchmark(baseline=True, ci=False)
+    def pt2(self, x, w1, w2):
+        def _baseline(x, w1, w2):
+            D1 = torch.matmul(x, w1)
+            D2 = torch.matmul(x, w2)
+            m = torch.nn.silu()
+            F1 = m(D1)
+            F2 = m(D2)
+            O = torch.mul(F1, F2)
+            return O
+        compiled = torch.compile(_baseline)
+        compiled(x, w1, w2)
+        return lambda: compiled(x, w1, w2)
+
+
+    @register_benchmark(ci=HAS_MIRAGE)
+    def mirage(self, x, w1, w2):
+        return lambda: self.mirage_optimized_graph([x, w1, w2])
+
+
+    def get_input_iter(self) -> Generator:
+        x = torch.randn(8, 4096, dtype=torch.float16, device=self.device)
+        w1 = torch.randn(4096, 4096, dtype=torch.float16, device=self.device)
+        w2 = torch.randn(4096, 4096, dtype=torch.float16, device=self.device)
+        yield (x, w1, w2)
+        return
