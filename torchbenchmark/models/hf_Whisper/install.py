341ad14f1b05fea0f9f174a4e037366ed6b60021
Xu Zhao <xzhao9@meta.com>
2024-11-25 11:48:26 -0800

    Migrate to AWS A100 runner (#2549)
    Summary:
    Now PyTorch provides A100 runner on AWS, we can deprecate Torchbench's own A100 testing infra and inherit the testing infra from PyTorch.
    
    Pull Request resolved: https://github.com/pytorch/benchmark/pull/2549
    
    Reviewed By: kit1980
    
    Differential Revision: D66450676
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 33b8b7547688be0b96703dcd696641c13e079f5f

--- torchbenchmark/models/hf_Whisper/install.py
+++ torchbenchmark/models/hf_Whisper/install.py
@@ -4,10 +4,8 @@ from torchbenchmark.util.framework.huggingface.patch_hf import (
     cache_model,
     patch_transformers,
 )
-from utils.python_utils import pip_install_requirements
 
 if __name__ == "__main__":
-    pip_install_requirements()
     patch_transformers()
     model_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
     cache_model(model_name)
