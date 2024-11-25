f999a9cc6d384ce5c908de80683878a3a69f1bc6
Xu Zhao <i@xuzhao.net>
2024-11-25 11:13:57 -0500

    Fix sam and sam_fast

--- torchbenchmark/models/sam_fast/__init__.py
+++ torchbenchmark/models/sam_fast/__init__.py
@@ -30,8 +30,7 @@ class Model(BenchmarkModel):
 
         self.model = sam_model_fast_registry[model_type](checkpoint=sam_checkpoint)
         self.model.to(device=device)
-        data_folder = os.path.dirname(DATA_PATH)
-        image_path = os.path.join(data_folder, "truck.jpg")
+        image_path = os.path.join(DATA_PATH, "sam_inputs", "truck.jpg")
         assert os.path.exists(image_path), f"Expected image file exists at {image_path} but not found."
         self.image = cv2.imread(image_path)
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
