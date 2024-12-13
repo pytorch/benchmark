21ade743b67c793e50c8963a74919ed8ec84a6be
Xu Zhao <xzhao9@meta.com>
2024-12-13 17:13:12 -0500

    Add linux test a10g

--- .ci/torchbench/check-ssh.sh
+++ .ci/torchbench/check-ssh.sh
@@ -4,8 +4,8 @@
 while true; do
     # Check if `who` returns non-empty output
     if who | grep -q '.'; then
-        echo "User(s) logged in. Sleeping for 100 seconds..."
-        sleep 100
+        echo "User(s) logged in. Sleeping for 1000 seconds..."
+        sleep 1000
     else
         echo "No users logged in. Exiting..."
         exit 0
