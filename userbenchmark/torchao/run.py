368caaf30f7df6e7badb0e3a5d9b8f562caf2609
Huy Do <huydhn@gmail.com>
2024-12-17 00:06:38 -0800

    Finally

--- userbenchmark/torchao/run.py
+++ userbenchmark/torchao/run.py
@@ -29,7 +29,8 @@ def _get_ci_args(
         f"--{experiment}",
         "--output",
         f"{str(OUTPUT_DIR.joinpath(output_file_name).resolve())}",
-        "--only hf_T5",  # DEBUG: TO BE REMOVED
+        "--only",  # DEBUG: TO BE REMOVED
+        "hf_T5",  # DEBUG: TO BE REMOVED
     ]
     return ci_args
 
