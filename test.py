0f4f627b8d322fd8d9d53f55c92268f486a51900
Gerson Beckenkamp <gersonb@graphcore.ai>
2024-12-17 13:00:29 +0000

    Allow all tests to be validated for both backends

--- test.py
+++ test.py
@@ -36,14 +36,17 @@ class TestBenchmark(unittest.TestCase):
         gc.collect()
 
 
-def _create_example_model_instance(task: ModelTask, device: str):
+def _create_example_model_instance(task: ModelTask, device: str, mode: str):
     skip = False
+    extra_args = ["--accuracy"]
+    if mode == "inductor":
+        extra_args.append("--inductor")
     try:
-        task.make_model_instance(test="eval", device=device, extra_args=["--accuracy"])
+        task.make_model_instance(test="eval", device=device, extra_args=extra_args)
     except NotImplementedError:
         try:
             task.make_model_instance(
-                test="train", device=device, extra_args=["--accuracy"]
+                test="train", device=device, extra_args=extra_args
             )
         except NotImplementedError:
             skip = True
@@ -70,7 +73,7 @@ def _load_test(path, device, mode):
             skip=_skip_cuda_memory_check_p(metadata), assert_equal=self.assertEqual
         ):
             try:
-                _create_example_model_instance(task, device)
+                _create_example_model_instance(task, device, mode)
                 accuracy = task.get_model_attribute("accuracy")
                 assert (
                     accuracy == "pass"
@@ -136,7 +139,7 @@ def _load_test(path, device, mode):
             skip=_skip_cuda_memory_check_p(metadata), assert_equal=self.assertEqual
         ):
             try:
-                task.make_model_instance(test="eval", device=device)
+                task.make_model_instance(test="eval", device=device, extra_args=["--inductor"] if mode == "inductor" else [])
                 task.check_device()
                 task.del_model_instance()
             except NotImplementedError as e:
@@ -149,20 +152,20 @@ def _load_test(path, device, mode):
         [example_fn, train_fn, eval_fn, check_device_fn],
         ["example", "train", "eval", "check_device"],
     ):
-        for mode in ["eager", "inductor"]:
-            # set exclude list based on metadata
-            setattr(
-                TestBenchmark,
-                f"test_{model_name}_{fn_name}_{device}" if fn_name in ["example", "check_device"] else f"test_{model_name}_{fn_name}_{device}_{mode}",
-                (
-                    unittest.skipIf(
-                        skip_by_metadata(
-                            test=fn_name, device=device, extra_args=[], metadata=metadata
-                        ),
-                        "This test is skipped by its metadata",
-                    )(fn)
-                ),
-            )
+        # set exclude list based on metadata
+        setattr(
+            TestBenchmark,
+            f"test_{model_name}_{fn_name}_{device}_{mode}",
+            (
+                unittest.skipIf(
+                    # This is expecting that models will never be skipped just based on backend, just on eval or train functions being implemented
+                    skip_by_metadata(
+                        test=fn_name, device=device, extra_args=[], metadata=metadata
+                    ),
+                    "This test is skipped by its metadata",
+                )(fn)
+            ),
+        )
 
 
 def _load_tests():
