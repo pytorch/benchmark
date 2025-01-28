f4f1e08a914de9210cff2328f8aae1a9a41cb2db
Xu Zhao <i@xuzhao.net>
2025-01-27 21:25:48 -0500

    Run release test

--- userbenchmark/release-test/run_release_test.sh
+++ userbenchmark/release-test/run_release_test.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 python -c "import torch; import time; a = torch.randn([4096, 4096]).cuda(); time.sleep(60); print('done!')"  > log.txt 2>&1 &
 
 for i in {1..120}; do
-    nvidia-smi pmon -s m -c 1 -o T 
+    nvidia-smi dmon -s m -c 1 -o T -i 0
     sleep 0.5
 done
