539a1d6f20556a9c48cae709efbca408dfc59968
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2025-03-11 11:28:14 -0700

    Add

--- torchbenchmark/models/hf_minicpm/install.py
+++ torchbenchmark/models/hf_minicpm/install.py
@@ -6,12 +6,10 @@ from torchbenchmark.util.framework.huggingface.patch_hf import (
     cache_model,
     patch_transformers,
 )
-
-def setup_install():
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "./requirements.txt"])
+from utils.python_utils import pip_install_requirements
 
 if __name__ == "__main__":
     patch_transformers()
-    setup_install()
+    pip_install_requirements()
     model_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
     cache_model(model_name)
