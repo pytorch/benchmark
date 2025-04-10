2a70af9c4c4ae5a04ed32080e8f066b9e9966c8f
Andrey Talman <atalman@fb.com>
2025-04-10 15:58:56 -0400

    Update run_release_test.sh

--- userbenchmark/release-test/run_release_test.sh
+++ userbenchmark/release-test/run_release_test.sh
@@ -8,11 +8,11 @@ EXAMPLES_DIR="${RESULT_DIR}/../examples"
 # get the directory of the current script
 CURRENT_DIR=$(dirname -- "$0")
 
-#PREFIX=""
-#if [[ ${PLATFORM_NAME} == "aws_t4_metal" ]]; then
-PREFIX="taskset -c 24-47";
-export GOMP_CPU_AFFINITY="24-47"
-#fi
+PREFIX=""
+if [[ ${PLATFORM_NAME} == "aws_t4_metal" ]]; then
+  PREFIX="taskset -c 24-47";
+  export GOMP_CPU_AFFINITY="24-47"
+fi
 
 . switch-cuda.sh "${CUDA_VERSION}"
 
@@ -22,29 +22,29 @@ sudo apt-get install bc
 sudo apt-get install --reinstall time
 which time
 # run mnist-hogwild
-#mkdir -p ${RESULT_DIR}/mnist_hogwild
-#pushd "${EXAMPLES_DIR}/mnist_hogwild"
-#export LOG_FILE=${RESULT_DIR}/mnist_hogwild/result.log
-#export MEM_FILE=${RESULT_DIR}/mnist_hogwild/result_mem.log
-#${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3
+mkdir -p ${RESULT_DIR}/mnist_hogwild
+pushd "${EXAMPLES_DIR}/mnist_hogwild"
+export LOG_FILE=${RESULT_DIR}/mnist_hogwild/result.log
+export MEM_FILE=${RESULT_DIR}/mnist_hogwild/result_mem.log
+${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3
 # run CPU WLM LSTM
-#mkdir -p ${RESULT_DIR}/wlm_cpu_lstm
-#pushd "${EXAMPLES_DIR}/word_language_model"
-#export LOG_FILE=${RESULT_DIR}/wlm_cpu_lstm/result.log
-#export MEM_FILE=${RESULT_DIR}/wlm_cpu_lstm/result_mem.log
-#${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3 --model LSTM
+mkdir -p ${RESULT_DIR}/wlm_cpu_lstm
+pushd "${EXAMPLES_DIR}/word_language_model"
+export LOG_FILE=${RESULT_DIR}/wlm_cpu_lstm/result.log
+export MEM_FILE=${RESULT_DIR}/wlm_cpu_lstm/result_mem.log
+${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3 --model LSTM
 # run GPU WLM LSTM
-# mkdir -p ${RESULT_DIR}/wlm_gpu_lstm
-# pushd "${EXAMPLES_DIR}/word_language_model"
-# export LOG_FILE=${RESULT_DIR}/wlm_gpu_lstm/result.log
-# export MEM_FILE=${RESULT_DIR}/wlm_gpu_lstm/result_mem.log
-# ${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3 --model LSTM --cuda
+mkdir -p ${RESULT_DIR}/wlm_gpu_lstm
+pushd "${EXAMPLES_DIR}/word_language_model"
+export LOG_FILE=${RESULT_DIR}/wlm_gpu_lstm/result.log
+export MEM_FILE=${RESULT_DIR}/wlm_gpu_lstm/result_mem.log
+${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3 --model LSTM --cuda
 # run CPU WLM Transformer
-#mkdir -p ${RESULT_DIR}/wlm_cpu_trans
-#pushd "${EXAMPLES_DIR}/word_language_model"
-#export LOG_FILE=${RESULT_DIR}/wlm_cpu_trans/result.log
-#export MEM_FILE=${RESULT_DIR}/wlm_cpu_trans/result_mem.log
-#${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3 --model Transformer
+mkdir -p ${RESULT_DIR}/wlm_cpu_trans
+pushd "${EXAMPLES_DIR}/word_language_model"
+export LOG_FILE=${RESULT_DIR}/wlm_cpu_trans/result.log
+export MEM_FILE=${RESULT_DIR}/wlm_cpu_trans/result_mem.log
+${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3 --model Transformer
 # run mnist
 mkdir -p ${RESULT_DIR}/mnist
 pushd "${EXAMPLES_DIR}/mnist"
@@ -52,8 +52,8 @@ export LOG_FILE=${RESULT_DIR}/mnist/result.log
 export MEM_FILE=${RESULT_DIR}/mnist/result_mem.log
 ${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3
 # run GPU WLM Transformer
-# mkdir -p ${RESULT_DIR}/wlm_gpu_trans
-# pushd "${EXAMPLES_DIR}/word_language_model"
-# export LOG_FILE=${RESULT_DIR}/wlm_gpu_trans/result.log
-# export MEM_FILE=${RESULT_DIR}/wlm_gpu_trans/result_mem.log
-# ${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3 --model Transformer --cuda
+mkdir -p ${RESULT_DIR}/wlm_gpu_trans
+pushd "${EXAMPLES_DIR}/word_language_model"
+export LOG_FILE=${RESULT_DIR}/wlm_gpu_trans/result.log
+export MEM_FILE=${RESULT_DIR}/wlm_gpu_trans/result_mem.log
+${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 3 --model Transformer --cuda
