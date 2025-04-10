bf0d42ebaada5558544e205649fd18d3fb3d5ec2
Andrey Talman <atalman@fb.com>
2025-04-10 19:50:29 -0400

    Update run_release_test.sh

--- userbenchmark/release-test/run_release_test.sh
+++ userbenchmark/release-test/run_release_test.sh
@@ -26,7 +26,7 @@ mkdir -p ${RESULT_DIR}/mnist_hogwild
 pushd "${EXAMPLES_DIR}/mnist_hogwild"
 export LOG_FILE=${RESULT_DIR}/mnist_hogwild/result.log
 export MEM_FILE=${RESULT_DIR}/mnist_hogwild/result_mem.log
-${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 10
+bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 10
 # run CPU WLM LSTM
 #mkdir -p ${RESULT_DIR}/wlm_cpu_lstm
 #pushd "${EXAMPLES_DIR}/word_language_model"
@@ -50,7 +50,7 @@ mkdir -p ${RESULT_DIR}/mnist
 pushd "${EXAMPLES_DIR}/mnist"
 export LOG_FILE=${RESULT_DIR}/mnist/result.log
 export MEM_FILE=${RESULT_DIR}/mnist/result_mem.log
-${PREFIX} bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 10
+bash "${CURRENT_DIR}/monitor_proc.sh" python main.py --epochs 10
 # run GPU WLM Transformer
 #mkdir -p ${RESULT_DIR}/wlm_gpu_trans
 #pushd "${EXAMPLES_DIR}/word_language_model"
