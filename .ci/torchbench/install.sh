7137956104e2bdadf63add021a73f92cf4b3233b
Huy Do <huydhn@gmail.com>
2025-08-21 17:49:25 -0700

    Debug
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- .ci/torchbench/install.sh
+++ .ci/torchbench/install.sh
@@ -9,5 +9,7 @@ whoami
 ls -lah /usr/local/lib/python3.12/
 ls -lah /usr/local/lib/python3.12/dist-packages
 
+sleep 3600
+
 python3 -c "import torch; print(torch.__version__); print(torch.version.git_version)"
 python3 install.py
