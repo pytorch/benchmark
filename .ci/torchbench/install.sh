ce0cfbb9da4d87249853c137143eed90bcfdcb19
Huy Do <huydhn@gmail.com>
2025-08-21 19:18:41 -0700

    Remove debug
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- .ci/torchbench/install.sh
+++ .ci/torchbench/install.sh
@@ -2,14 +2,5 @@
 
 set -eux
 
-python3 --version
-python3 -m pip --version
-
-whoami
-ls -lah /usr/local/lib/python3.12/
-ls -lah /usr/local/lib/python3.12/dist-packages
-
-sleep 3600
-
 python3 -c "import torch; print(torch.__version__); print(torch.version.git_version)"
 python3 install.py
