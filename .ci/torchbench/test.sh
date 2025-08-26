979cb9fbaf98275eaeee17fbb97a409d422768b4
Huy Do <huydhn@gmail.com>
2025-08-25 22:25:52 -0700

    Restore TorchBench PR tests (#2636)
    Summary:
    With the Docker image back https://github.com/pytorch/benchmark/pull/2635, we can properly run tests now.  All failures are fixed with the exception of the memory leak issue when training `moco` which I'm not sure and will keep it as a TODO
    
    
    Reviewed By: malfet
    
    Differential Revision: D80882141
    
    Pulled By: huydhn

--- .ci/torchbench/test.sh
+++ .ci/torchbench/test.sh
@@ -1,31 +1,15 @@
-. ${HOME}/miniconda3/etc/profile.d/conda.sh
+#!/bin/bash
 
-if [ -z "${CONDA_ENV}" ]; then
-  echo "ERROR: CONDA_ENV is not set"
-  exit 1
-fi
+set -eux
 
-if [ -z "${TEST_CONFIG}" ]; then
-  echo "ERROR: TEST_CONFIG is not set"
-  exit 1
-fi
+pip install expecttest
 
-if [[ -n "${SETUP_SCRIPT}" && -e "${SETUP_SCRIPT}" ]]; then
-  . "${SETUP_SCRIPT}"
-fi
-
-. ${HOME}/miniconda3/etc/profile.d/conda.sh
-
-conda activate "${CONDA_ENV}"
-
-parent_dir=$(dirname "$(readlink -f "$0")")/../..
-cd ${parent_dir}
-
-# Test subprocess worker
-if [[ "$TEST_CONFIG" == 'cpu' ]]; then
-  python -m torchbenchmark._components.test.test_subprocess
-  python -m torchbenchmark._components.test.test_worker
+# TODO (huydhn): Not sure why the worker swallows up SIGSEGV when running
+# on CPU
+if [[ "$TEST_CONFIG" == "cuda" ]]; then
+  python3 -m torchbenchmark._components.test.test_subprocess
+  python3 -m torchbenchmark._components.test.test_worker
 fi
 
 # Test models
-python test.py -v -k "$TEST_CONFIG"
+python3 test.py -v -k "$TEST_CONFIG"
--- torchbenchmark/models/maml_omniglot/__init__.py
+++ torchbenchmark/models/maml_omniglot/__init__.py
@@ -97,7 +97,9 @@ class Model(BenchmarkModel):
                 ),
             ]
         ):
-            self.meta_inputs = torch.load(f"{root}/batch.pt", weights_only=True)
+            self.meta_inputs = torch.load(
+                f"{root}/batch-20250825.pt", weights_only=True
+            )
         self.meta_inputs = tuple(
             [torch.from_numpy(i).to(self.device) for i in self.meta_inputs]
         )
