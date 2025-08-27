d65f14cb1c9e47167142918cd32b030712068b90
Huy Do <huydhn@gmail.com>
2025-08-27 02:57:33 -0700

    Use 2 versions of maml_omniglot for numpy 1x and 2x
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- torchbenchmark/models/functorch_maml_omniglot/__init__.py
+++ torchbenchmark/models/functorch_maml_omniglot/__init__.py
@@ -96,8 +96,9 @@ class Model(BenchmarkModel):
                 ),
             ]
         ):
+            filename = "batch-np-2x.pt" if np.__version__ >= "2.0.0" else "batch-np-1x.pt"
             self.meta_inputs = torch.load(
-                f"{root}/maml_omniglot/batch-20250825.pt", weights_only=True
+                f"{root}/maml_omniglot/{filename}", weights_only=True
             )
         self.meta_inputs = tuple(
             [torch.from_numpy(i).to(self.device) for i in self.meta_inputs]
--- torchbenchmark/models/maml_omniglot/install.py
+++ torchbenchmark/models/maml_omniglot/install.py
@@ -3,6 +3,7 @@ from utils.python_utils import pip_install_requirements
 
 if __name__ == "__main__":
     pip_install_requirements()
-    s3_utils.checkout_s3_data(
-        "MODEL_PKLS", "maml_omniglot/batch-20250825.pt", decompress=False
-    )
+    for filename in ["batch-np-1x.pt", "batch-np-2x.pt"]:
+        s3_utils.checkout_s3_data(
+            "MODEL_PKLS", f"maml_omniglot/{filename}", decompress=False
+        )
