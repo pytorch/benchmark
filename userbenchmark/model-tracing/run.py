fa09c87f2142ee50a6b56bbf32334b9ece313981
Xu Zhao <xzhao9@fb.com>
2025-09-04 08:57:07 -0700

    trace the model

--- userbenchmark/model-tracing/run.py
+++ userbenchmark/model-tracing/run.py
@@ -4,7 +4,25 @@ Example trace:
 """
 
 import argparse
+import itertools
 from typing import List
+
+from torchbenchmark.util.experiment.instantiator import (
+    list_models,
+    load_model_isolated,
+    TorchBenchModelConfig,
+)
+
+
+def generate_model_config(model_name: str, mode: str) -> TorchBenchModelConfig:
+    return TorchBenchModelConfig(
+        name=model_name,
+        device="cuda",
+        test=mode,
+        batch_size=None,
+        extra_args=[],
+        extra_env=None,
+    )
 
 
 class ModelTrace:
@@ -14,23 +32,28 @@ class ModelTrace:
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--model", type=str, default=None, help="Model to trace, split by comma."
+        "--models", type=str, default=None, help="Model to trace, split by comma."
     )
     parser.add_argument(
         "--mode",
         type=str,
-        default="train,eval",
-        help="Mode to trace, default is train,eval.",
+        default="train",
+        help="Mode to trace, default is train.",
     )
     parser.add_argument("--output", type=str, help="Output directory.")
     return parser
 
 
-def trace_model():
-    pass
+def trace_model(cfg: TorchBenchModelConfig) -> ModelTrace:
+    task = load_model_isolated(cfg)
+    return ModelTrace()
 
 
 def run(args: List[str]):
     parser = get_parser()
-    args = parser.parse_args(args)
-    run_traces(extra_args=args)
+    args: argparse.Namespace = parser.parse_args(args)
+    models = args.models.split(",") if args.models else list_models()
+    modes = args.mode.split(",") if args.mode else ["train"]
+    model_args = list(itertools.product(models, modes))
+    cfgs = [generate_model_config(model_name, mode) for model_name, mode in model_args]
+    traces = [trace_model(cfg) for cfg in cfgs]
